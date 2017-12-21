d20f922795efb9b853a0d5d182c0186354c25621
Gabriela Aznar <aznarsig@wcr-mac-13.local>
2017-12-21 18:14:52 +0100

    Add properties in entity class.

--- climada/entity/entity.py
+++ climada/entity/entity.py
@@ -1,58 +1,119 @@
 """
-=====================
-entity
-=====================
-
 Define Entity Class.
 """
-# Author: Gabriela Aznar Siguan (gabriela.aznar@usys.ethz.ch)
-# Created on Fri Nov 10 10:00:03 2017
 
-#    Copyright (C) 2017 by
-#    David N. Bresch, david.bresch@gmail.com
-#    Gabriela Aznar Siguan (g.aznar.siguan@gmail.com)
-#    All rights reserved.
-
+from climada.entity.impact_funcs.base  import ImpactFuncs
 from climada.entity.impact_funcs.source_excel  import ImpactFuncsExcel
+from climada.entity.discounts.base import Discounts
 from climada.entity.discounts.source_excel import DiscountsExcel
+from climada.entity.measures.base import Measures
 from climada.entity.measures.source_excel import MeasuresExcel
+from climada.entity.exposures.base import Exposures
 from climada.entity.exposures.source_excel import ExposuresExcel
-from climada.util.config import entity_default
+from climada.util.config import ENT_DEF_XLS
 
 class Entity(object):
-    """Contains the definition of one entity"""
+    """Collects exposures, impact functions, measures and discount rates.
+
+    Attributes
+    ----------
+        exposures (subclass Exposures): exposures
+        impact_funcs (subclass ImpactFucs): vulnerability functions
+        measures (subclass Measures): measures
+        discounts (subclass Discounts): discount rates
+        def_file (str): name of the xls file used as default source data
+    """
+
+    def_file = ENT_DEF_XLS
 
     def __init__(self, exposures=None, impact_funcs=None, measures=None,
                  discounts=None):
+        """Initialization. Default values are set for the inputs not provided.
 
+        Parameters
+        ----------
+            exposures (subclass Exposures, optional)
+            impact_funcs (subclass ImpactFucs, optional)
+            measures (subclass Measures, optional)
+            discounts (subclass Discounts, optional)
+
+        Raises
+        ------
+            ValueError
+
+        Examples
+        ---------
+            >>> Entity()
+            Builds an Entity with the values obtained from ENT_DEF_XLS file.
+            >>> Entity(exposures=myexposures)
+            Builds an Entity with impact function, measures and discount rates
+            from ENT_DEF_XLS file, and the given exposures.
+            >>> Entity(impact_funcs=myimpact_funcs, measures=mymeasures)
+            Builds an Entity with exposures and discount rates from
+            ENT_DEF_XLS file, and the given impact functions and measures.
+        """
         if exposures is not None:
             self.exposures = exposures
         else:
-            self.exposures = ExposuresExcel(entity_default)
+            self.exposures = ExposuresExcel(self.def_file)
 
         if impact_funcs is not None:
             self.impact_funcs = impact_funcs
         else:
-            self.impact_funcs = ImpactFuncsExcel(entity_default)
+            self.impact_funcs = ImpactFuncsExcel(self.def_file)
 
         if measures is not None:
             self.measures = measures
         else:
-            self.measures = MeasuresExcel(entity_default)
+            self.measures = MeasuresExcel(self.def_file)
 
         if discounts is not None:
             self.discounts = discounts
         else:
-            self.discounts = DiscountsExcel(entity_default)
-
-    #def interpolate(self, grid, conf):
-    #    """ Interpolate the entity assets to a grid using the configuration"""
-        #self.assets.interpolate(grid, conf)
-
-    #def calc_future(self, conf):
-    #    """ Compute the future assets following the configuration """
+            self.discounts = DiscountsExcel(self.def_file)
 
     def tags(self):
-        """Obtain the entity tag"""
-        return {self.exposures.tag, self.impact_funcs.tag,
-                self.measures.tag, self.discounts.tag}
+        """Return entity tag constructed from its attributes tags."""
+        return {self._exposures.tag, self._impact_funcs.tag,
+                self._measures.tag, self._discounts.tag}
+
+    @property
+    def exposures(self):
+        return self._exposures
+
+    @exposures.setter
+    def exposures(self, value):
+        if not isinstance(value, Exposures):
+            raise ValueError("Input value is not subclass of Exposures ABC.")
+        self._exposures = value
+
+    @property
+    def impact_funcs(self):
+        return self._impact_funcs
+
+    @impact_funcs.setter
+    def impact_funcs(self, value):
+        if not isinstance(value, ImpactFuncs):
+            raise ValueError("Input value is not subclass of ImpactFuncs \
+                             ABC.")
+        self._impact_funcs = value
+
+    @property
+    def measures(self):
+        return self._measures
+
+    @measures.setter
+    def measures(self, value):
+        if not isinstance(value, Measures):
+            raise ValueError("Input value is not subclass of Measures ABC.")
+        self._measures = value
+
+    @property
+    def discounts(self):
+        return self._discounts
+
+    @discounts.setter
+    def discounts(self, value):
+        if not isinstance(value, Discounts):
+            raise ValueError("Input value is not subclass of Discounts ABC.")
+        self._discounts = value
