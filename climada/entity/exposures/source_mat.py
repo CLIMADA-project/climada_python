4180b10124375bbd97cc841f46fc5cf1cc7bd71c
Gabriela Aznar <aznarsig@wcr-mac-13.local>
2018-01-16 16:38:05 +0100

    Add imported exposures from mat file. Corrections in excel import of exposures.

---
+++ climada/entity/exposures/source_mat.py
@@ -0,0 +1,140 @@
+"""
+Define ExposuresMat class.
+"""
+
+import numpy as np
+
+from climada.entity.exposures.base import Exposures
+from climada.entity.tag import Tag
+import climada.util.hdf5_handler as hdf5
+
+class ExposuresMat(Exposures):
+    """Exposures class loaded from a mat file produced by climada.
+
+    Attributes
+    ----------
+        sup_field_name (str): name of the enclosing variable, if present
+        field_name (str): name of variable containing the data
+        var (dict): name of the variables in field_name
+    """
+
+    def __init__(self, file_name=None, description=None):
+        """Extend Exposures __init__ method.
+
+        Parameters
+        ----------
+            file_name (str, optional): name of the source file
+            description (str, optional): description of the source data
+
+        Examples
+        --------
+            >>> ExposuresMat()
+            Initializes empty attributes.
+            >>> ExposuresMat('filename')
+            Loads data from the provided file.
+            >>> ExposuresMat('filename', 'description of file')
+            Loads data from the provided file and stores provided description.
+        """
+        self.sup_field_name = 'entity'
+        self.field_name = 'assets'
+        self.var = {'lat' : 'lat',
+                    'lon' : 'lon',
+                    'val' : 'Value',
+                    'ded' : 'Deductible',
+                    'cov' : 'Cover',
+                    'imp' : 'DamageFunID',
+                    'cat' : 'Category_ID',
+                    'reg' : 'Region_ID',
+                    'uni' : 'Value_unit',
+                    'ass' : 'centroid_index',
+                    'ref' : 'reference_year'
+                   }
+        # Initialize
+        Exposures.__init__(self, file_name, description)
+
+    def read(self, file_name, description=None):
+        """Override read Loader method."""
+       # append the file name and description into the instance class
+        self.tag = Tag(file_name, description)
+
+        # Load mat data
+        expo = hdf5.read(file_name)
+        try:
+            expo = expo[self.sup_field_name]
+        except KeyError:
+            pass
+        expo = expo[self.field_name]
+
+        # Fill variables
+        self._read_obligatory(expo)
+        self._read_default(expo)
+        self._read_optional(expo, file_name)
+
+    def _read_obligatory(self, expo):
+        """Fill obligatory variables."""
+        self.value = expo[self.var['val']]. \
+        reshape(len(expo[self.var['val']]),)
+
+        coord_lat = expo[self.var['lat']]
+        coord_lon = expo[self.var['lon']]
+        self.coord = np.concatenate((coord_lat, coord_lon), axis=1)
+
+        self.impact_id = expo[self.var['imp']]. \
+        reshape(len(expo[self.var['imp']]),)
+        self.impact_id = self.impact_id.astype(int)
+
+        # set exposures id according to appearance order
+        num_exp = len(self.value)
+        self.id = np.linspace(self.id.size, self.id.size + \
+                              num_exp - 1, num_exp, dtype=int)
+
+    def _read_default(self, expo):
+        """Fill optional variables. Set default values."""
+        num_exp = len(expo[self.var['val']])
+        # get the exposures deductibles as np.array float 64
+        # if not provided set default zero values
+        self.deductible = self._parse_default(expo, self.var['ded'], \
+                                               np.zeros(num_exp))
+        # get the exposures coverages as np.array float 64
+        # if not provided set default exposure values
+        self.cover = self._parse_default(expo, self.var['cov'], self.value)
+
+    def _read_optional(self, expo, file_name):
+        """Fill optional parameters."""
+        self.ref_year = self._parse_optional(expo, self.ref_year, \
+                                             self.var['ref'])
+        if not isinstance(self.ref_year, int):
+            self.ref_year = int(self.ref_year[0])
+
+        self.category_id = self._parse_optional(expo, self.category_id, \
+                                                self.var['cat'])
+        self.category_id = self.category_id.astype(int)
+        self.region_id = self._parse_optional(expo, self.region_id, \
+                                                self.var['reg'])
+        self.region_id = self.region_id.astype(int)
+        self.assigned = self._parse_optional(expo, self.assigned, \
+                                                self.var['ass'])
+        self.assigned = self.assigned.astype(int)
+        try:
+            self.value_unit = hdf5.get_string_from_ref(file_name, \
+                                                  expo[self.var['uni']][0][0])
+        except KeyError:
+            pass
+
+    @staticmethod
+    def _parse_optional(hdf, var, var_name):
+        """Retrieve optional variable, leave its original value if fail."""
+        try:
+            var = hdf[var_name].reshape(len(hdf[var_name]),)
+        except KeyError:
+            pass
+        return var
+
+    @staticmethod
+    def _parse_default(hdf, var_name, def_val):
+        """Retrieve optional variable, set default value if fail."""
+        try:
+            res = hdf[var_name].reshape(len(hdf[var_name]),)
+        except KeyError:
+            res = def_val
+        return res
---
+++ climada/entity/exposures/test/test_mat.py
@@ -0,0 +1,226 @@
+"""
+Test ExposuresMat class.
+"""
+
+import warnings
+import unittest
+import numpy as np
+
+import climada.util.hdf5_handler as hdf5
+from climada.entity.exposures.source_mat import ExposuresMat
+from climada.util.constants import ENT_DEMO_MAT
+from climada.util.config import config
+
+class TestReader(unittest.TestCase):
+    """Test reader functionality of the ExposuresMat class"""
+
+    def test_read_demo_pass(self):
+        """ Read one single excel file"""
+        # Read demo excel file
+        expo = ExposuresMat()
+        description = 'One single file.'
+        expo.read(ENT_DEMO_MAT, description)
+
+        # Check results
+        n_expos = 50
+
+        self.assertEqual(type(expo.id[0]), np.int64)
+        self.assertEqual(expo.id.shape, (n_expos,))
+        self.assertEqual(expo.id[0], 0)
+        self.assertEqual(expo.id[n_expos-1], n_expos-1)
+
+        self.assertEqual(expo.value.shape, (n_expos,))
+        self.assertEqual(expo.value[0], 13927504367.680632)
+        self.assertEqual(expo.value[n_expos-1], 12624818493.687229)
+
+        self.assertEqual(expo.deductible.shape, (n_expos,))
+        self.assertEqual(expo.deductible[0], 0)
+        self.assertEqual(expo.deductible[n_expos-1], 0)
+
+        self.assertEqual(expo.cover.shape, (n_expos,))
+        self.assertEqual(expo.cover[0], 13927504367.680632)
+        self.assertEqual(expo.cover[n_expos-1], 12624818493.687229)
+
+        self.assertEqual(type(expo.impact_id[0]), np.int64)
+        self.assertEqual(expo.impact_id.shape, (n_expos,))
+        self.assertEqual(expo.impact_id[0], 1)
+        self.assertEqual(expo.impact_id[n_expos-1], 1)
+
+        self.assertEqual(type(expo.category_id[0]), np.int64)
+        self.assertEqual(expo.category_id.shape, (n_expos,))
+        self.assertEqual(expo.category_id[0], 1)
+        self.assertEqual(expo.category_id[n_expos-1], 1)
+
+        self.assertEqual(type(expo.assigned[0]), np.int64)
+        self.assertEqual(expo.assigned.shape, (n_expos,))
+        self.assertEqual(expo.assigned[0], 47)
+        self.assertEqual(expo.assigned[n_expos-1], 46)
+
+        self.assertEqual(expo.region_id.shape, (0,))
+
+        self.assertEqual(expo.coord.shape, (n_expos, 2))
+        self.assertEqual(expo.coord[0][0], 26.93389900000)
+        self.assertEqual(expo.coord[n_expos-1][0], 26.34795700000)
+        self.assertEqual(expo.coord[0][1], -80.12879900000)
+        self.assertEqual(expo.coord[n_expos-1][1], -80.15885500000)
+
+        self.assertEqual(expo.ref_year, config["present_ref_year"])
+        self.assertEqual(expo.value_unit, 'USD')
+        self.assertEqual(expo.tag.file_name, ENT_DEMO_MAT)
+        self.assertEqual(expo.tag.description, description)
+
+    def test_check_demo_warning(self):
+        """Check warning centroids when demo read."""
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always")
+            ExposuresMat(ENT_DEMO_MAT)
+            # Verify warnings thrown
+            self.assertIn("Exposures.region_id not set.", \
+                          str(w[0].message))
+
+class TestObligatories(unittest.TestCase):
+    """Test reading exposures obligatory values."""
+
+    def test_no_value_fail(self):
+        """Error if no values."""
+        expo = ExposuresMat()
+        expo.var['val'] = 'no valid value'
+        with self.assertRaises(KeyError):
+            expo.read(ENT_DEMO_MAT)
+
+    def test_no_impact_fail(self):
+        """Error if no impact ids."""
+        expo = ExposuresMat()
+        expo.var['imp'] = 'no valid impact'
+        with self.assertRaises(KeyError):
+            expo.read(ENT_DEMO_MAT)
+
+    def test_no_coord_fail(self):
+        """Error if no coordinates."""
+        expo = ExposuresMat()
+        expo.var['lat'] = 'no valid Latitude'
+        with self.assertRaises(KeyError):
+            expo.read(ENT_DEMO_MAT)
+
+        expo.var['lat'] = 'Latitude'
+        expo.var['lon'] = 'no valid Longitude'
+        with self.assertRaises(KeyError):
+            expo.read(ENT_DEMO_MAT)
+
+class TestOptionals(unittest.TestCase):
+    """Test reading exposures optional values."""
+
+    def test_no_category_pass(self):
+        """Not error if no category id."""
+        expo = ExposuresMat()
+        expo.var['cat'] = 'no valid category'
+        expo.read(ENT_DEMO_MAT)
+
+        # Check results
+        self.assertEqual(0, expo.category_id.size)
+
+    def test_no_region_pass(self):
+        """Not error if no region id."""
+        expo = ExposuresMat()
+        expo.var['reg'] = 'no valid region'
+        expo.read(ENT_DEMO_MAT)
+
+        # Check results
+        self.assertEqual(0, expo.region_id.size)
+
+    def test_no_unit_pass(self):
+        """Not error if no value unit."""
+        expo = ExposuresMat()
+        expo.var['uni'] = 'no valid value unit'
+        expo.read(ENT_DEMO_MAT)
+
+        # Check results
+        self.assertEqual('NA', expo.value_unit)
+
+    def test_no_assigned_pass(self):
+        """Not error if no value unit."""
+        expo = ExposuresMat()
+        expo.var['ass'] = 'no valid assign'
+        expo.read(ENT_DEMO_MAT)
+
+        # Check results
+        self.assertEqual(0, expo.assigned.size)
+
+    def test_no_refyear_pass(self):
+        """Not error if no value unit."""
+        expo = ExposuresMat()
+        expo.var['ref'] = 'no valid ref'
+        expo.read(ENT_DEMO_MAT)
+
+        # Check results
+        self.assertEqual(config["present_ref_year"], expo.ref_year)
+
+class TestDefaults(unittest.TestCase):
+    """Test reading exposures default values."""
+
+    def test_no_cover_pass(self):
+        """Check default values for excel file with no cover."""
+        # Read demo excel file
+        expo = ExposuresMat()
+        # Change cover column name to simulate no present column
+        expo.var['cov'] = 'Dummy'
+        expo.read(ENT_DEMO_MAT)
+
+        # Check results
+        self.assertEqual(True, np.array_equal(expo.value, expo.cover))
+
+    def test_no_deductible_pass(self):
+        """Check default values for excel file with no deductible."""
+        # Read demo excel file
+        expo = ExposuresMat()
+        # Change deductible column name to simulate no present column
+        expo.var['ded'] = 'Dummy'
+        expo.read(ENT_DEMO_MAT)
+
+        # Check results
+        self.assertEqual(True, np.array_equal(np.zeros(len(expo.value)), \
+                                              expo.deductible))
+
+class TestParsers(unittest.TestCase):
+    """Test parser auxiliary functions"""
+
+    def setUp(self):
+        self.expo = hdf5.read(ENT_DEMO_MAT)
+        self.expo = self.expo[ExposuresMat().sup_field_name]
+        self.expo = self.expo[ExposuresMat().field_name]
+
+    def test_parse_optional_exist_pass(self):
+        """Check variable read if present."""
+        var_ini = 0
+        var = ExposuresMat._parse_optional(self.expo, var_ini, 'lat')
+        self.assertEqual(50, len(var))
+
+    def test_parse_optional_not_exist_pass(self):
+        """Check pass if variable not present and initial value kept."""
+        var_ini = 0
+        var = ExposuresMat._parse_optional(self.expo, var_ini, 'Not Present')
+        self.assertEqual(var_ini, var)
+
+    def test_parse_default_exist_pass(self):
+        """Check variable read if present."""
+        def_val = 5
+        var = ExposuresMat._parse_default(self.expo, 'lat', def_val)
+        self.assertEqual(50, len(var))
+
+    def test_parse_default_not_exist_pass(self):
+        """Check pass if variable not present and default value is set."""
+        def_val = 5
+        var = ExposuresMat._parse_default(self.expo, 'Not Present', def_val)
+        self.assertEqual(def_val, var)
+        
+# Execute TestReader
+suite = unittest.TestLoader().loadTestsFromTestCase(TestReader)
+suite.addTests(
+    unittest.TestLoader().loadTestsFromTestCase(TestParsers))
+suite.addTests(
+    unittest.TestLoader().loadTestsFromTestCase(TestDefaults))
+suite.addTests(
+    unittest.TestLoader().loadTestsFromTestCase(TestOptionals))
+suite.addTests(
+    unittest.TestLoader().loadTestsFromTestCase(TestObligatories))
+unittest.TextTestRunner(verbosity=2).run(suite)
