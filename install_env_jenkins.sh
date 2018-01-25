b51289292875fc6297da5e25567a65a3f6ccd84d
Gabriela Aznar <aznarsig@wcr-mac-13.local>
2018-01-25 09:54:40 +0100

    Add python.app installation in script for mac os x.

--- install_env_jenkins.sh
+++ install_env_jenkins.sh
@@ -11,6 +11,9 @@ conda create --name climada_env python=3.6
 source activate climada_env
 python setup.py develop
 
+# for mac os x: install python.app: Proxy on os x letting python libraries hook into the gui event loop
+conda install python.app
+
 echo "Installation completed in environment climada_env. Execute tests to check installation."
 PYTHONPATH=. python3 unit_tests.py
 PYTHONPATH=. python3 integ_tests.py
