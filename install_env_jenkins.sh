1c955f771473a5a1a36dea6f31700e2019e9908a
Gabriela Aznar <aznarsig@wcr-mac-13.local>
2018-01-25 11:07:04 +0100

    work around for mac os installation.

--- install_env_jenkins.sh
+++ install_env_jenkins.sh
@@ -11,11 +11,10 @@ conda create --name climada_env python=3.6
 source activate climada_env
 python setup.py develop
 
-# for mac os x: install python.app: Proxy on os x letting python libraries hook into the gui event loop
-conda install python.app
+# for mac os x: install matplotlib with condas defined dependencies
+conda install matplotlib
 
 echo "Installation completed in environment climada_env. Execute tests to check installation."
 PYTHONPATH=. python3 unit_tests.py
-PYTHONPATH=. python3 integ_tests.py
 
 source deactivate
