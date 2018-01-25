7bfa1cf2830be0f1b76b8f2a6dc5f544859894c1
Gabriela Aznar <aznarsig@wcr-mac-13.local>
2018-01-25 10:00:26 +0100

    Add integration tests for execution.

--- run_tests_jenkins.sh
+++ run_tests_jenkins.sh
@@ -5,7 +5,7 @@ set -e
 source ../../../.conda/envs/climada_env/bin/activate climada_env
 
 # run all climada tests
-PYTHONPATH=. python -m coverage run unit_tests.py
+PYTHONPATH=. python -m coverage run unit_tests.py integ_tests.py
 
 # analize coverage
 python -m coverage xml -o coverage.xml
