81b781da9d9a6ba30f1982d5a8bf255b9bdbc730
gabriela <g.aznar.siguan@gmail.com>
2018-01-31 18:35:38 +0100

    pylint configuration file.

--- run_tests_jenkins.sh
+++ run_tests_jenkins.sh
@@ -13,5 +13,5 @@ python -m coverage xml -o coverage.xml
 python -m coverage html -d coverage
 
 # run static code analysis
-pylint -ry --load-plugins=pylint.extensions.mccabe --extension-pkg-whitelist=numpy --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" climada > pylint.log || true
+pylint -ry --rcfile=./pylint_conf.rc climada > pylint.log || true
 
