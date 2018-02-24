31cbca26ba56084373ce0e2ffed0f6580deab5d3
gabriela <g.aznar.siguan@gmail.com>
2018-02-24 12:22:12 +0100

    add append method. change input None to empty string.

--- climada/entity/tag.py
+++ climada/entity/tag.py
@@ -13,7 +13,7 @@ class Tag(object):
         description (str): description of the data
     """
 
-    def __init__(self, file_name=None, description=None):
+    def __init__(self, file_name='', description=''):
         """Initialize values.
 
         Parameters
@@ -21,13 +21,20 @@ class Tag(object):
             file_name (str, optional): file name to read
             description (str, optional): description of the data
         """
-        if file_name is None:
-            self.file_name = ''
-        else:
-            self.file_name = file_name
-        if description is None:
-            self.description = ''
-        else:
-            self.description = description
-        #self._next = 'NA'
-        #self._prev = 'NA'
+        self.file_name = file_name
+        self.description = description        
+
+    def append(self, tag):
+        """Append input Tag instance information to current Tag."""
+        # add file name if not present in tag
+        if self.file_name == '':
+            self.file_name = tag.file_name  
+            self.description = tag.description
+        elif tag.file_name not in self.file_name:
+            if not isinstance(self.file_name, list):
+                self.file_name = [self.file_name]
+            self.file_name.append(tag.file_name)
+    
+            if not isinstance(self.description, list):
+                self.description = [self.description]
+            self.description.append(tag.description)
