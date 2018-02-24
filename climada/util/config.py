1369aa3576446fd1a36511deef8ddc1f3fb2e30a
gabriela <g.aznar.siguan@gmail.com>
2018-02-24 12:24:45 +0100

    add repository folder variable.

--- climada/util/config.py
+++ climada/util/config.py
@@ -4,6 +4,9 @@ Define configuration parameters.
 
 from climada.util.constants import DATA_DIR, ENT_TEMPLATE_XLS
 
+# Define folder containg repository data
+REPO_DIR = DATA_DIR
+
 HAZ_DEF_XLS = DATA_DIR + 'demo/Excel_hazard.xlsx'
 HAZ_DEF_MAT = DATA_DIR + 'demo/atl_prob.mat'
 ENT_DEF_XLS = ENT_TEMPLATE_XLS
