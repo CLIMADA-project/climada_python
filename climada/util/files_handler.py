07fac3e4c922375b15447ce101d491d7a636efaa
gabriela <g.aznar.siguan@gmail.com>
2018-02-24 12:26:56 +0100

    add module to handle with file names.

---
+++ climada/util/files_handler.py
@@ -0,0 +1,69 @@
+"""
+Functions to deal with files.
+"""
+
+__all__ = ['to_str_list',
+           'get_file_names'
+          ]
+
+import os
+
+def to_str_list(num_exp, values, val_name):
+    """Check size and transform to list if necessary. If size is one, build
+    a list with num_exp repeated values.
+    
+    Parameters
+    ----------
+        num_exp (int): number of expect list elements
+        values (object or list(object)): values to check and transform
+        val_name (str): name of the variable values
+
+    Returns
+    -------
+        list
+    """
+    if isinstance(values, list):
+        if len(values) == num_exp:
+            val_list = values
+        elif len(values) == 1:
+            val_list = list()
+            val_list += num_exp * [values[0]]
+        else:
+            raise ValueError('Provide one or %s %s.' % \
+                             (num_exp, val_name))
+    else:
+        val_list = list()
+        val_list += num_exp * [values] 
+    return val_list
+
+
+def get_file_names(file_name):
+    """Return list of files contained.
+    
+    Parameters
+    ----------
+        file_name (str or list(str)): file name, or list of file names or name
+            of the folder containing the files
+
+    Returns
+    -------
+        list
+    """
+    file_list = list()
+    if isinstance(file_name, list):
+        for file in file_name:
+            _process_one_file_name(file, file_list)
+    else:
+        _process_one_file_name(file_name, file_list)
+    return file_list
+
+def _process_one_file_name(name, file_list):
+    """Apend to input list the file contained in name"""
+    if os.path.splitext(name)[1] == '': 
+        tmp_files = os.listdir(name)
+        # append only files, not folders
+        for file in tmp_files:
+            if os.path.splitext(file)[1] != '': 
+                file_list.append(file)
+    else:
+        file_list.append(name)
---
+++ climada/util/test/test_files.py
@@ -0,0 +1,74 @@
+"""
+Test files_handler module.
+"""
+
+import os
+import unittest
+
+from climada.util.files_handler import to_str_list, get_file_names
+from climada.util.constants import DATA_DIR
+
+class TestToStrList(unittest.TestCase):
+    """Test to_str_list function"""
+    def test_identity_pass(self):
+        """Returns the same list if its length is correct."""
+        num_exp = 3
+        values = ['hi', 'ho', 'ha']
+        val_name = 'values'
+        out = to_str_list(num_exp, values, val_name)
+        self.assertEqual(values, out)
+        
+    def test_one_to_list(self):
+        """When input is a string or list with one element, it returns a list 
+        with the expected number of elments repeated"""
+        num_exp = 3
+        values = 'hi'
+        val_name = 'values'
+        out = to_str_list(num_exp, values, val_name)
+        self.assertEqual(['hi', 'hi', 'hi'], out)
+        
+        values = ['ha']
+        out = to_str_list(num_exp, values, val_name)
+        self.assertEqual(['ha', 'ha', 'ha'], out)
+
+    def test_list_wrong_length_fail(self):
+        """When input is list of neither expected size nor one, fail."""
+        num_exp = 3
+        values = ['1', '2']
+        val_name = 'values'
+        
+        with self.assertRaises(ValueError) as error:
+            to_str_list(num_exp, values, val_name)
+        self.assertEqual("Provide one or 3 values.", str(error.exception))
+        
+class TestGetFileNames(unittest.TestCase):
+    """Test get_file_names function"""
+    def test_one_file_copy(self):
+        """If input is one file name, return a list with this file name"""
+        file_name = "test.mat"
+        out = get_file_names(file_name)
+        self.assertEqual([file_name], out)
+
+    def test_several_file_copy(self):
+        """If input is a list with several file names, return the same list"""
+        file_name = ["test1.mat", "test2.mat", "test3.mat", "test4.mat"]
+        out = get_file_names(file_name)
+        self.assertEqual(file_name, out)
+
+    def test_folder_contents(self):
+        """If input is one folder name, return a list with containg files. 
+        Folder names are not contained."""
+        file_name = DATA_DIR + "demo/"
+        out = get_file_names(file_name)
+        for file in out:
+            self.assertEqual('.', os.path.splitext(file)[1][0])
+        
+        file_name = DATA_DIR
+        out = get_file_names(file_name)
+        for file in out:
+            self.assertNotEqual('', os.path.splitext(file)[1])
+
+# Execute Tests
+TESTS = unittest.TestLoader().loadTestsFromTestCase(TestToStrList)
+TESTS.addTests(unittest.TestLoader().loadTestsFromTestCase(TestGetFileNames))
+unittest.TextTestRunner(verbosity=2).run(TESTS)
