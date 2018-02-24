886200d0b8e17ffa468b2c551a10a41fa995da3c
gabriela <g.aznar.siguan@gmail.com>
2018-02-24 20:47:42 +0100

    add coverage with multiprocessing. this makes the execution slower.

--- run_tests_jenkins.sh
+++ run_tests_jenkins.sh
@@ -5,8 +5,9 @@ set -e
 source ../../../.conda/envs/climada_env/bin/activate climada_env
 
 # run all climada tests
-PYTHONPATH=. python -m coverage run -a unit_tests.py
-PYTHONPATH=. python -m coverage run -a integ_tests.py
+PYTHONPATH=. python -m coverage run -a --parallel-mode --concurrency=multiprocessing unit_tests.py
+PYTHONPATH=. python -m coverage run -a --parallel-mode --concurrency=multiprocessing integ_tests.py
+PYTHONPATH=. python -m coverage combine
 
 # analize coverage
 python -m coverage xml -o coverage.xml
