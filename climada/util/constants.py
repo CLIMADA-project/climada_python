096ff2b6b4487d10e2ad5442d920241f075f8121
gabriela <g.aznar.siguan@gmail.com>
2018-02-24 12:25:00 +0100

    add a hazard file without event names to use it in unit tests (HAZ_TEST_MAT). the reading of this file is faster.

--- climada/util/constants.py
+++ climada/util/constants.py
@@ -16,12 +16,13 @@ import os
 
 # working directory
 WORKING_DIR = os.path.dirname(__file__) + '/../../'
-# folder containing the demo data
+# folder containing the data
 DATA_DIR = WORKING_DIR + 'data/'
 
 # Files containg data used as demo, e.g. for unit testing
 HAZ_DEMO_XLS = DATA_DIR + 'demo/Excel_hazard.xlsx'
 HAZ_DEMO_MAT = DATA_DIR + 'demo/atl_prob.mat'
+HAZ_TEST_MAT = DATA_DIR + 'demo/atl_prob_no_name.mat'
 ENT_DEMO_XLS = DATA_DIR + 'demo/demo_today.xlsx'
 ENT_TEMPLATE_XLS = DATA_DIR + 'demo/entity_template.xlsx'
 ENT_DEMO_MAT = DATA_DIR + 'demo/demo_today.mat'
