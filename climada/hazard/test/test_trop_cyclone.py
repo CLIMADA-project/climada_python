d9a556cafb71aaff6f1a1f133f9c0f0211d6a206
gabriela <g.aznar.siguan@gmail.com>
2018-03-24 13:48:29 +0100

    Filling tropical cyclones up to holland method.

--- climada/hazard/test/test_trop_cyclone.py
+++ climada/hazard/test/test_trop_cyclone.py
@@ -3,18 +3,103 @@ Test TropCyclone class
 """
 
 import unittest
+import numpy as np
 
-from climada.util.constants import HAZ_TEST_MAT
-from climada.hazard.trop_cyclone import TropCyclone
+from climada.hazard.trop_cyclone import TropCyclone, read_ibtracs, set_category, missing_pressure
+from climada.util.config import CONFIG
 
-class TestLoader(unittest.TestCase):
+TEST_TRACK = CONFIG['local_data']['repository'] + "/tc_tracks/ibtracs/ibtracs_global_intp-None_1951239N12334.csv"
+
+class TestReader(unittest.TestCase):
     """Test loading funcions from the TropCyclone class"""
 
-    def test_read_fail(self):
-        """Read a tropical cyclone."""
+    def test_read_one_pass(self):
+        """Read and fill one tropical cyclone."""
+        centroids = None
+        trop = TropCyclone()
         with self.assertRaises(NotImplementedError): 
-            TropCyclone(HAZ_TEST_MAT)
+            trop._read_one(TEST_TRACK, 'TC', '', centroids)
+
+    def test_read_fail(self):
+        """Read a tropical cyclone from constructor."""
+        with self.assertRaises(NotImplementedError): 
+            TropCyclone(TEST_TRACK)
+
+class TestIBTracs(unittest.TestCase):
+    """Test loading funcions from the TropCyclone class"""
+
+    def test_category_pass(self):
+        """Test category computation."""
+        max_sus_wind = np.array([25, 30, 35, 40, 45, 45, 45, 45, 35, 25])
+        max_sus_wind_unit = 'kn'
+        cat = set_category(max_sus_wind, max_sus_wind_unit)
+        self.assertEqual(0, cat)
+
+        max_sus_wind = np.array([25, 25, 25, 30, 30, 30, 30, 30, 25, 25, 20])
+        max_sus_wind_unit = 'kn'
+        cat = set_category(max_sus_wind, max_sus_wind_unit)
+        self.assertEqual(-1, cat)
+
+        max_sus_wind = np.array([80, 90, 100, 115, 120, 125, 130, 120, 110, 80, 75, 80, 65])
+        max_sus_wind_unit = 'kn'
+        cat = set_category(max_sus_wind, max_sus_wind_unit)
+        self.assertEqual(4, cat)
+
+        max_sus_wind = np.array([28.769475, 34.52337, 40.277265, 46.03116, 51.785055, \
+        51.785055, 51.785055, 51.785055, 40.277265, 28.769475])
+        max_sus_wind_unit = 'mph'
+        cat = set_category(max_sus_wind, max_sus_wind_unit)
+        self.assertEqual(0, cat)
+        
+        max_sus_wind = np.array([12.86111437, 12.86111437, 12.86111437, 15.43333724, \
+        15.43333724, 15.43333724, 15.43333724, 15.43333724, 12.86111437, 12.86111437, 10.2888915])
+        max_sus_wind_unit = 'm/s'
+        cat = set_category(max_sus_wind, max_sus_wind_unit)
+        self.assertEqual(-1, cat)
+
+        max_sus_wind = np.array([148.16, 166.68, 185.2, 212.98, 222.24, 231.5, 240.76, \
+        222.24, 203.72, 148.16, 138.9, 148.16, 120.38])
+        max_sus_wind_unit = 'km/h'
+        cat = set_category(max_sus_wind, max_sus_wind_unit)
+        self.assertEqual(4, cat)
+
+    def test_missing_pres_pass(self):
+        """Test central pressure function."""
+        cen_pres = np.array([-999, -999, -999, -999, -999, -999, -999, -999, -999, 992, -999, -999, 993, -999, -999, 1004])
+        v_max = np.array([45, 50, 50, 55, 60, 65, 70, 80, 75, 70, 70, 70, 70, 65, 55, 45]) 
+        lat = np.array([13.8, 13.9, 14, 14.1, 14.1, 14.1, 14.1, 14.2, 14.2, 14.3, 14.4, 14.6, 14.8, 15, 15.1, 15.1])
+        lon = np.array([-51.1, -52.8, -54.4, -56, -57.3, -58.4, -59.7, -61.1, -62.7, -64.3, -65.8, -67.4, -69.4, -71.4, -73, -74.2])
+        out_pres = missing_pressure(cen_pres, v_max, lat, lon)
+
+        ref_res = np.array([989.7085, 985.6725, 985.7236, 981.6847, 977.6324, 973.5743, 969.522, 961.3873, 965.5237, 969.6648, 969.713, 969.7688, 969.8362, 973.9936, 982.2247, 990.4395])
+        np.testing.assert_array_almost_equal(ref_res, out_pres)
+
+    def test_read_pass(self):
+        """Read a tropical cyclone."""
+        tc_track = read_ibtracs(TEST_TRACK)
+        
+        self.assertEqual(tc_track['lon'][11], -39.60)
+        self.assertEqual(tc_track['lat'][23], 14.10)
+        self.assertEqual(tc_track['time_step'][7], 6)
+        self.assertEqual(np.max(tc_track['radius_max_wind']), 0)
+        self.assertEqual(np.min(tc_track['radius_max_wind']), 0)
+        self.assertEqual(tc_track['max_sustained_wind'][21], 55)
+        self.assertEqual(tc_track['central_pressure'][29], 969.76880)
+        self.assertEqual(np.max(tc_track['environmental_pressure']), 1010)
+        self.assertEqual(np.min(tc_track['environmental_pressure']), 1010)
+        self.assertEqual(tc_track['time.year'][13], 1951)
+        self.assertEqual(tc_track['time.month'][26], 9)
+        self.assertEqual(tc_track['time.day'][7], 29)
+        self.assertEqual(tc_track.attrs['max_sustained_wind_unit'], 'kn')
+        self.assertEqual(tc_track.attrs['central_pressure_unit'], 'kn')
+        self.assertEqual(tc_track.attrs['orig_event_flag'], 1)
+        self.assertEqual(tc_track.attrs['name'], '1951239N12334')
+        self.assertEqual(tc_track.attrs['data_provider'], 'hurdat_atl')
+        self.assertTrue(np.isnan(tc_track.attrs['basin']))
+        self.assertEqual(tc_track.attrs['id_no'], 1951239012334)
+        self.assertEqual(tc_track.attrs['category'], 1)
 
 # Execute Tests
-TESTS = unittest.TestLoader().loadTestsFromTestCase(TestLoader)
+TESTS = unittest.TestLoader().loadTestsFromTestCase(TestReader)
+TESTS.addTests(unittest.TestLoader().loadTestsFromTestCase(TestIBTracs))
 unittest.TextTestRunner(verbosity=2).run(TESTS)
--- climada/hazard/trop_cyclone.py
+++ climada/hazard/trop_cyclone.py
@@ -2,13 +2,24 @@
 Define Tropical Cyclone.
 """
 
-__all__ = ['TropCyclone']
+__all__ = ['TropCyclone', 'read_ibtracs']
 
 import logging
+import datetime as dt
+import pandas as pd
+import xarray as xr
+import numpy as np
+from pint import UnitRegistry
 
 from climada.hazard.base import Hazard
+from climada.hazard.tag import Tag as TagHazard
+from climada.hazard.centroids.base import Centroids
+from climada.util.constants import GLB_CENTROIDS
+import climada.util.checker as check
 
 LOGGER = logging.getLogger(__name__)
+
+SAFFIR_SIM_CAT = [34, 64, 83, 96, 113, 135, 1000]
 
 class TropCyclone(Hazard):
     """Contains events of Tropical Cyclones.
@@ -24,6 +35,7 @@ class TropCyclone(Hazard):
         intensity (sparse.csr_matrix): intensity of the events at centroids
         fraction (sparse.csr_matrix): fraction of affected exposures for each
             event at each centroid
+        track (list(xarray.Dataset)): list of tropical cyclone tracks
     """
 
     def __init__(self, file_name='', description='', centroids=None):
@@ -43,10 +55,11 @@ class TropCyclone(Hazard):
             ValueError
         """
         self.haz_type = 'TC'
+        self.track = [] # [xr.Dataset()]
         Hazard.__init__(self, file_name, self.haz_type, description, centroids)
     
     @staticmethod
-    def _read_one(file_name, haz_type, description, centroids, var_names):
+    def _read_one(file_name, haz_type, description, centroids, var_names=None):
         """ Read input file. If centroids are not provided, they are read
         from file_name.
 
@@ -56,14 +69,144 @@ class TropCyclone(Hazard):
             haz_type (str): acronym of the hazard type (e.g. 'TC')
             description (str): description of the source data
             centroids (Centroids, optional): Centroids instance
-            var_names (dict): name of the variables in the file (e.g. 
-                      DEF_VAR_NAME defined in the source modules)
-            file_name (str): name of the source file
 
         Raises
         ------
             ValueError, KeyError
         """
-#        new_haz = TropCyclone()
+        if centroids is None:
+            centroids = Centroids(GLB_CENTROIDS, 'Global Nat centroids')
+        new_haz = TropCyclone()
+        new_haz.tag = TagHazard(file_name, haz_type, description)
+        try:
+            track = read_ibtracs(file_name)
+        except pd.errors.ParserError:
+            LOGGER.error('Provide a IBTraCS file in csv format containing one'\
+                         + ' TC track.')
+            raise ValueError
+        new_haz._event_from_track(track, centroids)
+        new_haz.track.append(track)
+        return new_haz
+    
+    def _event_from_track(self, track, centroids):
+        """ """
+        # TODO: coastal_centroids + climada_tc_equal_timestep + isimip_windfield_holland
         raise NotImplementedError
-#        return new_haz
+    
+    def append(self, tc_haz):
+        """Check and append variables of input TropCyclone to current. 
+        Repeated events and centroids will be overwritten. Tracks are appended.
+        
+        Parameters
+        ----------
+            tc_haz (TropCyclone): TropCyclone instance to append to current
+
+        Raises
+        ------
+            ValueError
+        """
+        super(TropCyclone, self).append(tc_haz)
+        self.track.extend(tc_haz.track)
+        
+    def track_info(self):
+        """Get information about tracks"""
+        # TODO infos track
+        
+    def plot_track(self):
+        """Plot track """
+        # TODO plot tracks
+        
+    def check(self):
+        """Check if the attributes contain consistent data.
+
+        Raises
+        ------
+            ValueError
+        """
+        super(TropCyclone, self).check()
+        check.size(len(self.event_id), self.track, 'track')
+
+def read_ibtracs(file_name):
+    """Read IBTrACS track file.
+
+        Parameters
+        ----------
+            file_name (str): file name containing one IBTrACS track to read
+        
+        Returns
+        -------
+            xarray.Dataset     
+    """
+    dfr = pd.read_csv(file_name)
+    name = dfr['ibtracsID'].values[0]
+    
+    datetimes = list()
+    for time in dfr['isotime'].values:
+        year = np.fix(time/1e6)
+        time = time - year*1e6
+        month = np.fix(time/1e4)
+        time = time - month*1e4
+        day = np.fix(time/1e2)
+        hour = time - day*1e2 
+        datetimes.append(dt.datetime(int(year), int(month), int(day), \
+                                     int(hour)))
+    
+    lat = dfr['cgps_lat'].values
+    lon = dfr['cgps_lon'].values
+    cen_pres = dfr['pcen'].values
+    cen_pres_unit = 'kn'
+    max_sus_wind = dfr['vmax'].values
+    max_sus_wind_unit = 'kn'
+    cen_pres = missing_pressure(cen_pres, max_sus_wind, lat, lon)
+    category = set_category(max_sus_wind, max_sus_wind_unit)
+    
+    return xr.Dataset( \
+        {'lon': ('time', lon), \
+         'lat': ('time', lat), \
+         'time_step': ('time', dfr['tint'].values), \
+         'radius_max_wind': ('time', dfr['rmax'].values), \
+         'max_sustained_wind': ('time', max_sus_wind), \
+         'central_pressure': ('time', cen_pres), \
+         'environmental_pressure': ('time', dfr['penv'].values)}, \
+         coords={'time': datetimes}, \
+         attrs={'max_sustained_wind_unit': max_sus_wind_unit, \
+         'central_pressure_unit': cen_pres_unit, \
+         'orig_event_flag': dfr['original_data'].values[0], \
+         'name': name, \
+         'data_provider': dfr['data_provider'].values[0], \
+         'basin': dfr['gen_basin'].values[0], \
+         'id_no': float(name.replace('N', '0').replace('S', '1')), \
+         'category': category \
+        })
+    
+def missing_pressure(cen_pres, v_max, lat, lon):
+    """Deal with missing central pressures."""
+    if np.argwhere(cen_pres < 0).size > 0:
+        cen_pres = 1024.388 + 0.047*lat - 0.029*lon - 0.818*v_max
+    return cen_pres
+
+def set_category(max_sus_wind, max_sus_wind_unit):
+    """Add storm category according to saffir-simpson hurricane scale
+   -1 tropical depression
+    0 tropical storm
+    1 Hurrican category 1
+    2 Hurrican category 2
+    3 Hurrican category 3
+    4 Hurrican category 4
+    5 Hurrican category 5
+    """
+    ureg = UnitRegistry()
+    if (max_sus_wind_unit == 'kn') or (max_sus_wind_unit == 'kt'):
+        unit = ureg.knot
+    elif max_sus_wind_unit == 'mph':
+        unit = ureg.mile / ureg.hour
+    elif max_sus_wind_unit == 'm/s':
+        unit = ureg.meter / ureg.second
+    elif max_sus_wind_unit == 'km/h':
+        unit = ureg.kilometer / ureg.hour
+    else:
+        LOGGER.error('Wind not recorded in kn, conversion to kn needed.')
+        raise ValueError
+    max_wind_kn = (np.max(max_sus_wind) * unit).to(ureg.knot).magnitude
+    
+    return (np.argwhere(max_wind_kn < SAFFIR_SIM_CAT) - 1)[0][0]
