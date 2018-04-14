624baafb703b2136cce8aeb1fab6eed9be3c5658
gabriela <g.aznar.siguan@gmail.com>
2018-04-14 20:05:22 +0200

    Add __str__ method

--- climada/hazard/centroids/tag.py
+++ climada/hazard/centroids/tag.py
@@ -5,7 +5,7 @@ Define Tag class.
 __all__ = ['Tag']
 
 class Tag(object):
-    """Definition of one Exposures, DiscRates, ImpactFuncSet or Measures tag.
+    """Source data tag for Centroids.
 
     Attributes:
         file_name (str): name of the source file
@@ -20,19 +20,25 @@ class Tag(object):
             description (str, optional): description of the data
         """
         self.file_name = file_name
-        self.description = description        
+        self.description = description
 
     def append(self, tag):
         """Append input Tag instance information to current Tag."""
         # add file name if not present in tag
         if self.file_name == '':
-            self.file_name = tag.file_name  
+            self.file_name = tag.file_name
             self.description = tag.description
         elif tag.file_name not in self.file_name:
             if not isinstance(self.file_name, list):
                 self.file_name = [self.file_name]
             self.file_name.append(tag.file_name)
-    
+
             if not isinstance(self.description, list):
                 self.description = [self.description]
             self.description.append(tag.description)
+
+    def __str__(self):
+        return ' File: ' + self.file_name + '\n Description: ' + \
+            self.description
+
+    __repr__ = __str__
