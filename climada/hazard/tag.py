5516dbc4eeb85e9a72e03e8fc20d08648fc8a440
gabriela <g.aznar.siguan@gmail.com>
2018-04-14 20:07:01 +0200

    Add __str__ method

--- climada/hazard/tag.py
+++ climada/hazard/tag.py
@@ -9,7 +9,7 @@ __all__ = ['Tag']
 LOGGER = logging.getLogger(__name__)
 
 class Tag(object):
-    """Definition of one hazard tag.
+    """Contain information used to tag a Hazard.
 
     Attributes:
         file_name (str or list(str)): name of the source file(s)
@@ -17,17 +17,17 @@ class Tag(object):
         description (str or list(str)): description(s) of the data
     """
 
-    def __init__(self, file_name='', haz_type='NA', description=''):
+    def __init__(self, haz_type='NA', file_name='', description=''):
         """Initialize values.
 
         Parameters:
+            haz_type (str, optional): acronym of the hazard type (e.g. 'TC').
             file_name (str or list(str), optional): file name(s) to read
-            haz_type (str, optional): acronym of the hazard type (e.g. 'TC')
             description (str or list(str), optional): description of the data
-            
+
         """
-        self.file_name = file_name
         self.haz_type = haz_type
+        self.file_name = file_name
         self.description = description
 
     def append(self, tag):
@@ -41,14 +41,19 @@ class Tag(object):
 
         # add file name if not present in tag
         if self.file_name == '':
-            self.file_name = tag.file_name  
+            self.file_name = tag.file_name
             self.description = tag.description
         elif tag.file_name not in self.file_name:
             if not isinstance(self.file_name, list):
-                self.file_name = [self.file_name]        
+                self.file_name = [self.file_name]
             self.file_name.append(tag.file_name)
-    
+
             if not isinstance(self.description, list):
                 self.description = [self.description]
             self.description.append(tag.description)
-        
+
+    def __str__(self):
+        return ' Type: ' + self.haz_type + '\n File: ' + self.file_name +\
+            '\n Description: ' + self.description
+
+    __repr__ = __str__
