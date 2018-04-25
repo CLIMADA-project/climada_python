7419336a1d096c829fc16aedbae7b090a8918ce3
gabriela <g.aznar.siguan@gmail.com>
2018-04-25 10:21:56 +0200

    Improve documentation

--- climada/util/coordinates.py
+++ climada/util/coordinates.py
@@ -19,8 +19,19 @@ class Coordinates(object):
         return (0, 2)
 
     def resample(self, coord, method=interp.METHOD[0], \
-                    distance=interp.DIST_DEF[0]):
-        """Get indexes of resampled input coordinates."""
+                    distance=interp.DIST_DEF[1]):
+        """ Input coordinates are resampled to current grid.
+
+        Parameters:
+            coord (2d array): First column contains latitude, second
+                column contains longitude. Each row is a geographic point.
+            method (str, optional): interpolation method. Default: nearest
+                neighbor.
+            distance (str, optional): metric to use. Default: haversine
+
+        Returns:
+            np.array
+        """
         raise NotImplementedError
 
 class IrregularGrid(np.ndarray, Coordinates):
@@ -28,21 +39,27 @@ class IrregularGrid(np.ndarray, Coordinates):
     coordinates for one exposure. The first column is for latitudes and the
     second for longitudes (in degrees)."""
     def __new__(cls, input_array):
-        # Input array is an already formed ndarray instance
-        # We first cast to be our class type
         obj = np.asarray(input_array).view(cls)
-        # add the new attribute to the created instance
-#        obj.info = info
         return obj
 
     def __array_finalize__(self, obj):
-        # see InfoArray.__array_finalize__ for comments
         if obj is None:
             return
-#        self.info = getattr(obj, 'info', None)
 
     def resample(self, coord, method=interp.METHOD[0], \
                     distance=interp.DIST_DEF[0]):
+        """ Input coordinates are resampled to current grid.
+
+        Parameters:
+            coord (2d array): First column contains latitude, second
+                column contains longitude. Each row is a geographic point.
+            method (str, optional): interpolation method. Default: nearest
+                neighbor.
+            distance (str, optional): metric to use. Default: haversine
+
+        Returns:
+            np.array
+        """
         return interp.interpol_index(self, coord, method, distance)
 
 class RegularGrid(Coordinates):
