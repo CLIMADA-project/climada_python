b588e8fa116db7eeac9b64a210c9b2e0b85795d1
gabriela <g.aznar.siguan@gmail.com>
2018-07-02 11:16:20 +0200

    Add setting functions.

--- climada/entity/impact_funcs/tc_emanuel.py
+++ climada/entity/impact_funcs/tc_emanuel.py
@@ -4,9 +4,12 @@ Define impact function for tropical cyclnes using the formula of Emanuele 2011.
 
 __all__ = ['IFEmanuele']
 
+import logging
 import numpy as np
 
 from climada.entity.impact_funcs.base import ImpactFunc
+
+LOGGER = logging.getLogger(__name__)
 
 class IFEmanuele(ImpactFunc):
     """Impact function for tropical cyclones according to Emanuele 2011."""
@@ -33,6 +36,35 @@ class IFEmanuele(ImpactFunc):
         self.intensity_unit = 'm/s'
         self.intensity = intensity
         self.paa = np.ones(intensity.shape)
+        self.set_shape(v_thresh, v_half)
+        self.set_scale(scale)
+
+    def set_shape(self, v_thresh, v_half=74.7):
+        """ Check values and set vulnerability curve with according shape.
+
+        Parameters:
+            v_thresh (float): first shape parameter, wind speed in
+                m/s below which there is no damage. Default: 25.7(Emanuel 2011)
+            v_half (float): second shape parameter, wind speed in m/s
+                at which 50% of max. damage is expected. Default:
+                v_threshold + 49 m/s (mean value of Sealy & Strobl 2017)
+        """
+        if v_half <= v_thresh:
+            LOGGER.error('Shape parameters out of range: v_half <= v_thresh.')
+        elif  v_thresh < 0 or v_half < 0:
+            LOGGER.error('Negative shape parameter.')
+
         v_temp = (self.intensity - v_thresh) / (v_half - v_thresh)
         v_temp[v_temp < 0] = 0
-        self.mdd = scale * v_temp**3 / (1 + v_temp**3)
+        self.mdd = v_temp**3 / (1 + v_temp**3)
+
+    def set_scale(self, scale):
+        """ Multiply vulnerability curve by scale.
+
+        Parameters:
+            scale (float): scale parameter, linear scaling of MDD.
+                0<=scale<=1. Default: 1.0
+        """
+        if scale > 1 or scale <= 0:
+            LOGGER.error('Scale parameter out of range.')
+        self.mdd *= scale
--- climada/entity/impact_funcs/test/test_tc_emanuel.py
+++ climada/entity/impact_funcs/test/test_tc_emanuel.py
@@ -7,7 +7,7 @@ import numpy as np
 
 from climada.entity.impact_funcs.tc_emanuel import IFEmanuele
 
-class TestInterpolation(unittest.TestCase):
+class TestEmanuelFormula(unittest.TestCase):
     """Impact function interpolation test"""
 
     def test_default_values_pass(self):
@@ -21,13 +21,13 @@ class TestInterpolation(unittest.TestCase):
         self.assertTrue(np.array_equal(imp_fun.paa, np.ones((25,))))
         self.assertTrue(np.array_equal(imp_fun.mdd[0:6], np.zeros((6,))))
         self.assertTrue(np.array_equal(imp_fun.mdd[6:10], 
-                         np.array([0.0006753419543492556, 0.006790495604105169, 0.02425254393374475, 0.05758706257339458])))
+                        np.array([0.0006753419543492556, 0.006790495604105169, 0.02425254393374475, 0.05758706257339458])))
         self.assertTrue(np.array_equal(imp_fun.mdd[10:15], 
-                         np.array([0.10870556455111065, 0.1761433569521351, 0.2553983618763961, 0.34033822528795565, 0.4249447743109498])))
+                        np.array([0.10870556455111065, 0.1761433569521351, 0.2553983618763961, 0.34033822528795565, 0.4249447743109498])))
         self.assertTrue(np.array_equal(imp_fun.mdd[15:20], 
-                         np.array([0.5045777092933046, 0.576424302849412, 0.6393091739184916, 0.6932203123193963, 0.7388256596555696])))
+                        np.array([0.5045777092933046, 0.576424302849412, 0.6393091739184916, 0.6932203123193963, 0.7388256596555696])))
         self.assertTrue(np.array_equal(imp_fun.mdd[20:25], 
-                         np.array([0.777104531116526, 0.8091124649261859, 0.8358522190681132, 0.8582150905529946, 0.8769633232141456])))
+                        np.array([0.777104531116526, 0.8091124649261859, 0.8358522190681132, 0.8582150905529946, 0.8769633232141456])))
 
     def test_values_pass(self):
         """Compute mdr interpolating values."""
@@ -41,8 +41,45 @@ class TestInterpolation(unittest.TestCase):
         self.assertTrue(np.array_equal(imp_fun.paa, np.ones((6,))))
         self.assertTrue(np.array_equal(imp_fun.mdd[0:3], np.zeros((3,))))
         self.assertTrue(np.array_equal(imp_fun.mdd[3:], 
-                         np.array([0.017857142857142853, 0.11428571428571425, 0.250000000000000])))
+                        np.array([0.017857142857142853, 0.11428571428571425, 0.250000000000000])))
+
+    def test_set_scale(self):
+        """Set scale parameter."""
+        imp_fun = IFEmanuele()
+        scale = 0.5
+        imp_fun.set_scale(scale)
+        self.assertEqual(imp_fun.name, 'Emanuel 2011')
+        self.assertEqual(imp_fun.haz_type, 'TC')
+        self.assertEqual(imp_fun.id, 1)
+        self.assertEqual(imp_fun.intensity_unit, 'm/s')
+        self.assertTrue(np.array_equal(imp_fun.intensity, np.arange(0, 121, 5)))
+        self.assertTrue(np.array_equal(imp_fun.paa, np.ones((25,))))
+        self.assertTrue(np.array_equal(imp_fun.mdd[0:6], np.zeros((6,))))
+        self.assertTrue(np.array_equal(imp_fun.mdd[6:10], 
+                        scale * np.array([0.0006753419543492556, 0.006790495604105169, 0.02425254393374475, 0.05758706257339458])))
+        self.assertTrue(np.array_equal(imp_fun.mdd[10:15], 
+                        scale * np.array([0.10870556455111065, 0.1761433569521351, 0.2553983618763961, 0.34033822528795565, 0.4249447743109498])))
+        self.assertTrue(np.array_equal(imp_fun.mdd[15:20], 
+                        scale * np.array([0.5045777092933046, 0.576424302849412, 0.6393091739184916, 0.6932203123193963, 0.7388256596555696])))
+        self.assertTrue(np.array_equal(imp_fun.mdd[20:25], 
+                        scale * np.array([0.777104531116526, 0.8091124649261859, 0.8358522190681132, 0.8582150905529946, 0.8769633232141456])))
         
+    def test_set_shape(self):
+        """Set shape parameters."""
+        imp_fun = IFEmanuele(if_id=5, intensity=np.arange(0,6,1))
+        v_thresh = 2
+        v_half = 5
+        imp_fun.set_shape(v_thresh, v_half)
+        self.assertEqual(imp_fun.name, 'Emanuel 2011')
+        self.assertEqual(imp_fun.haz_type, 'TC')
+        self.assertEqual(imp_fun.id, 5)
+        self.assertEqual(imp_fun.intensity_unit, 'm/s')
+        self.assertTrue(np.array_equal(imp_fun.intensity, np.arange(0, 6, 1)))
+        self.assertTrue(np.array_equal(imp_fun.paa, np.ones((6,))))
+        self.assertTrue(np.array_equal(imp_fun.mdd[0:3], np.zeros((3,))))
+        self.assertTrue(np.array_equal(imp_fun.mdd[3:], 
+                        2 * np.array([0.017857142857142853, 0.11428571428571425, 0.250000000000000])))
+
 # Execute Tests
-TESTS = unittest.TestLoader().loadTestsFromTestCase(TestInterpolation)
+TESTS = unittest.TestLoader().loadTestsFromTestCase(TestEmanuelFormula)
 unittest.TextTestRunner(verbosity=2).run(TESTS)
