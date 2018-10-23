e28f051642b092b716d4100b291942eb1b9534ac
gabriela <g.aznar.siguan@gmail.com>
2018-10-23 17:35:01 +0200

    remove initial imports

--- climada/__init__.py
+++ climada/__init__.py
@@ -7,8 +7,3 @@ from .util.config import setup_conf_user
 from .util.config import setup_logging
 setup_conf_user()
 setup_logging(CONFIG['log_level'])
-
-from .entity import *
-from .hazard import *
-from .engine import *
-from .util import *
