8a13fd57f6fac945d63ae5e4375f2722a5be5b25
gabriela <g.aznar.siguan@gmail.com>
2018-10-26 16:30:09 +0200

    add default hazard type for impact_id

--- climada/entity/exposures/black_marble.py
+++ climada/entity/exposures/black_marble.py
@@ -59,6 +59,9 @@ INCOME_GRP_NE_TABLE = {5: 1, # Low income
                       }
 """ Meaning of values of natural earth's income groups. """
 
+DEF_HAZ_TYPE = 'TC'
+""" Default hazard type used in impact functions id."""
+
 class BlackMarble(Exposures):
     """Defines exposures from night light intensity, GDP and income group.
     Attribute region_id is defined as:
@@ -166,8 +169,7 @@ class BlackMarble(Exposures):
 
         exp_bkmrb.id = np.arange(1, exp_bkmrb.value.size+1)
         exp_bkmrb.region_id = np.ones(exp_bkmrb.value.shape, int)*cntry_info[0]
-        # hazard type TC as default
-        exp_bkmrb.impact_id = {'TC': np.ones(exp_bkmrb.value.size, int)}
+        exp_bkmrb.impact_id = {DEF_HAZ_TYPE: np.ones(exp_bkmrb.value.size, int)}
         exp_bkmrb.ref_year = cntry_info[3]
         exp_bkmrb.tag.description = ("{} {:d} GDP: {:.3e} income group: {:d}"+\
             "\n").format(cntry_info[1], cntry_info[3], \
@@ -346,7 +348,7 @@ def add_sea(exp, sea_res):
     exp.id = np.arange(1, exp.value.size+1)
     exp.region_id = np.append(exp.region_id, lat_mgrid[on_land].astype(int)*0
                               - 1)
-    exp.impact_id = {'TC': np.ones(exp.value.size, int)}
+    exp.impact_id = {DEF_HAZ_TYPE: np.ones(exp.value.size, int)}
 
 def _fill_admin1_geom(iso3, admin1_rec, prov_list):
     """Get admin1 polygons for each input province of country iso3.
