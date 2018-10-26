7cd42adf21516324cd6a4b91f0d7dabb7a846082
gabriela <g.aznar.siguan@gmail.com>
2018-10-26 16:24:12 +0200

    Create global section

--- climada/util/config.py
+++ climada/util/config.py
@@ -97,19 +97,16 @@ def setup_conf_user():
         if 'local_data' in userconfig.keys():
             CONFIG['local_data'].update(userconfig['local_data'])
 
+        if 'global' in userconfig.keys():
+            CONFIG['global'] = userconfig['global']
+
         if 'entity' in userconfig.keys():
             CONFIG['entity'].update(userconfig['entity'])
 
         if 'trop_cyclone' in userconfig.keys():
             CONFIG['trop_cyclone'].update(userconfig['trop_cyclone'])
 
-        if 'log_level' in userconfig.keys():
-            CONFIG['log_level'] = userconfig['log_level']
-
-        if 'impact' in userconfig.keys():
-            CONFIG['impact'] = userconfig['impact']
-
-        if 'adaptation' in userconfig.keys():
-            CONFIG['adaptation'] = userconfig['adaptation']
+        if 'cost_benefit' in userconfig.keys():
+            CONFIG['cost_benefit'] = userconfig['cost_benefit']
 
         check_conf()
