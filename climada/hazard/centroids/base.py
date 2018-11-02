8f40bb7556ef39ca80d811e4eb7eeea18f9cdcfe
Jan Hartman <jhartman@student.ethz.ch>
2018-11-02 22:52:04 +0100

    fix area calculation (np.tile vs np.repeat confusion)

--- climada/hazard/centroids/base.py
+++ climada/hazard/centroids/base.py
@@ -226,6 +226,7 @@ class Centroids(object):
         lon_unique_n = len(np.unique(self.lon))
         area_per_lat = lat_res_km * lon_res_km
         self.area_per_centroid = np.tile(area_per_lat, lon_unique_n)
+        self.area_per_centroid = np.repeat(area_per_lat, lon_unique_n)
 
     def set_on_land(self):
         """ Add the _on_land attribute, i.e. if a centroid is on land
