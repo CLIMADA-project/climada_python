b316620679690be49dfc1a6aec8818ffaa1b853e
Marine Perus <Marine@uwis-cx-dock-11-404.ethz.ch>
2018-12-18 11:23:21 +0100

    Bush fire hazard set from FIRMS repository.

--- climada/hazard/bush_fire.py
+++ climada/hazard/bush_fire.py
@@ -1,44 +1,335 @@
-"""
-This file is part of CLIMADA.
-
-Copyright (C) 2017 CLIMADA contributors listed in AUTHORS.
-
-CLIMADA is free software: you can redistribute it and/or modify it under the
-terms of the GNU Lesser General Public License as published by the Free
-Software Foundation, version 3.
-
-CLIMADA is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License along
-with CLIMADA. If not, see <https://www.gnu.org/licenses/>.
-
----
-
-Define BushFire class.
-"""
+"""Define BushFire class."""
 
 __all__ = ['BushFire']
 
 import logging
+import numpy as np
+import pandas as pd
+from scipy import sparse
+from datetime import datetime
+from sklearn.cluster import DBSCAN
 
+from climada.hazard.centroids.base import Centroids
+from climada.util.interpolation import interpol_index
 from climada.hazard.base import Hazard
+from climada.hazard.tag import Tag as TagHazard
+from random import randint
 
 LOGGER = logging.getLogger(__name__)
 
 HAZ_TYPE = 'BF'
 """ Hazard type acronym for Bush Fire """
 
-DIST_INTER_FIRE = 50 #km
-""" Distance between two different fires/events (in km)"""
+THRESH_INTERP = 2.0
+""" Distance threshold in km over which no neighbor will be found """
+
+EPS = 0.5
+""" maximum distance (in °) between two samples for them to be considered
+as in the same neighborhood"""
+
+MIN_SAMPLES = 1
+"""number of samples (or total weight) in a neighborhood for a point to
+be considered as a core point. This includes the point itself."""
 
 class BushFire(Hazard):
-    """Contains bush fire events.
-
-    Attributes:
-    """
+    """Contains bush fire events."""
 
     def __init__(self):
         """Empty constructor. """
         Hazard.__init__(self, HAZ_TYPE)
+
+    def set_bush_fire(self, csv_firms, centroids, description=''):
+        #Identify separate events:
+        #1 event = 1 fire up to its maximal extension
+        #Firms data clustered by consecutive dates
+        #Then pixel clustered geographically
+        #Then event_id defined for each pixel
+        """ Fill in the hazard file
+
+        Parameters:
+            csv_firms: csv file of the FIRMS data (obtained here:https://firms.modaps.eosdis.nasa.gov/download/)
+            centroids (Centroids, optional): Centroids where to model BF.
+                Default: global centroids.
+            description (str, optional): description of the events
+
+        Raises:
+            ValueError
+
+        """
+        LOGGER.info('Setting up Hazard file.')
+
+        firms, csv_firms, description = self._read_firms_csv(csv_firms)
+        #firms, description = self._read_firms_synth()
+        # add cons_id
+        firms = self._firms_cons_days(firms)
+        # add clus_id and event_id
+        firms = self._firms_clustering(firms)
+        firms = self._firms_event(firms)
+        centroids = self._centroids_creation(firms)
+        brightness = self._calc_brightness(firms, centroids)
+
+        self.tag = TagHazard(HAZ_TYPE, csv_firms, description)
+        self.units = 'K' # Kelvin units brightness
+        self.centroids = centroids
+        # following values are defined for each event
+        self.event_id = np.array(np.unique(firms['event_id'].values))
+        nb_years = (max(firms['datenum'].values) - min(firms['datenum'].values))/365
+        freq = nb_years/(max(firms['event_id'].values))
+        self.frequency = np.tile(freq, len(np.unique(firms['event_id'])))
+        self.event_name = np.array(np.unique(firms['event_id']))
+        event_uni, _ = np.unique(firms['event_id'].values, return_inverse=True, axis=0)
+        date = np.zeros(len(np.unique(firms['event_id'].values)))
+        for ev_id, event in enumerate(event_uni):
+            temp = firms.loc[firms['event_id'] == event, 'datenum']
+            temp = temp.reset_index()
+            date[ev_id] = temp.at[0, 'datenum']
+        self.date = date
+        self.orig = np.tile(True, len(np.unique(firms['event_id'])))
+
+        # following values are defined for each event and centroid
+        self.intensity = brightness
+        self.fraction = self.intensity.copy()
+        self.fraction.data.fill(1.0)
+
+    #Historical event (from FIRMS data)
+    @staticmethod
+    def _read_firms_csv(csv_firms, description=''):
+        """Read csv files from FIRMS data.
+
+        Parameters:
+            csv_firms: csv file of the FIRMS data
+            description (str, optional): description of the events
+
+        Returns:
+            firms, csv_firms, description
+        """
+        # open and read the file
+        firms = pd.read_csv(csv_firms)
+
+        for index, acq_date in enumerate(firms['acq_date'].values):
+            datenum = datetime.strptime(acq_date, '%Y-%M-%d').toordinal()
+            firms.at[index, 'datenum'] = datenum
+        return firms, csv_firms, description
+
+    @staticmethod
+    def _firms_cons_days(firms):
+        """Compute clusters of consecutive days.
+
+        Parameters:
+            firms (dataframe): dataset obtained from FIRMS data
+
+        Returns:
+            firms
+        """
+
+        LOGGER.info('Computing clusters of consecutive days.')
+        #Clustering by consecutive dates
+        cons_id = [0]
+        firms['cons_id'] = pd.Series(cons_id)
+        for index, val in list(map(tuple, firms['datenum'].items()))[1:]:
+            if abs(firms.at[index, 'datenum'] - firms.at[(index-1), 'datenum']) > 1:
+                firms.at[index, 'cons_id'] = firms.at[(index-1), 'cons_id']+1
+            else:
+                firms.at[index, 'cons_id'] = firms.at[(index-1), 'cons_id']
+        print(max(firms['cons_id'].values))
+        return firms
+
+
+    @staticmethod
+    def _firms_clustering(firms):
+        """Compute geographic clusters and sort firms with ascending clus_id for each cons_id.
+
+        Parameters:
+            firms (dataframe): dataset obtained from FIRMS data
+
+        Returns:
+            firms
+        """
+
+        LOGGER.info('Computing geographic clusters in consecutive events.')
+        #Creation of an identifier for geographical clustering
+        cluster_id = np.zeros((firms.index.size,))-1
+        for cons_id in np.unique(firms['cons_id'].values):
+            cc = np.argwhere(firms['cons_id'].values == cons_id).reshape(-1,)
+            lat_lon = firms.loc[cc, ['latitude', 'longitude']].values
+            lat_lon_uni, lat_lon_cpy = np.unique(lat_lon, return_inverse=True, axis=0)
+            cluster_uni = DBSCAN(eps=EPS, min_samples=MIN_SAMPLES).fit(lat_lon_uni).labels_
+            cluster_cpy = cluster_uni[lat_lon_cpy]
+            cluster_id[cc] = cluster_cpy
+        firms['clus_id'] = pd.Series(cluster_id)
+        print(max(firms['clus_id'].values))
+
+        LOGGER.info('Sorting of firms data')
+        #Re-order FIRMS file to have clus_id in ascending order inside each cons_id
+        cons_id_gp = firms.groupby('cons_id')
+        firms_sort = cons_id_gp.get_group(0).sort_values('clus_id')
+        for val, group in cons_id_gp:
+            if val > 0:
+                sort_clus = cons_id_gp.get_group(val).sort_values('clus_id')
+                firms_sort = firms_sort.append(sort_clus)
+        firms = firms_sort
+        firms = firms.reset_index()
+        return firms
+
+    @staticmethod
+    def _firms_event(firms):
+        """Creation of event_id for each dataset point.
+
+        Parameters:
+            firms (dataframe): dataset obtained from FIRMS data
+
+        Returns:
+            firms
+        """
+
+        LOGGER.info('Creation of event_id.')
+        #New event = change in cons_id and clus_id
+        event_id = np.zeros((firms.index.size,))+1
+        firms['event_id'] = pd.Series(event_id)
+        for index, val in list(map(tuple, firms['clus_id'].items()))[1:]:
+            if ((firms.at[index, 'clus_id'] - firms.at[index-1, 'clus_id']) == 0) & ((firms.at[index, 'cons_id'] - firms.at[index-1, 'cons_id']) == 0):
+                firms.at[index, 'event_id'] = int(firms.at[index-1, 'event_id'])
+            else:
+                firms.at[index, 'event_id'] = int(firms.at[index-1, 'event_id']+1)
+        print(max(firms['event_id'].values))
+        return firms
+
+    @staticmethod
+    def _centroids_creation(firms):
+        """Compute centroids for the firms dataset.
+
+        Parameters:
+            firms (dataframe): dataset obtained from FIRMS data
+
+        Returns:
+            centroids
+        """
+
+        LOGGER.info('Computing centroids.')
+        min_lat = firms['latitude'].min()
+        max_lat = firms['latitude'].max()
+        min_lon = firms['longitude'].min()
+        max_lon = firms['longitude'].max()
+        centroids = Centroids()
+        centroids.coord = (np.mgrid[min_lat : max_lat : complex(0, 500),
+                                    min_lon : max_lon : complex(0, 500)]).reshape(2, 250000).transpose()
+        centroids.id = np.arange(len(centroids.coord)) + 1
+        return centroids
+
+    @staticmethod
+    def _calc_brightness(firms, centroids):
+        """Fill in the intensity matrix with the maximum brightness at each centroid.
+
+        Parameters:
+            firms (dataframe): dataset obtained from FIRMS data
+            centroids (Centroids): centroids for the dataset
+
+        Returns:
+            brightness (matrix)
+        """
+
+        LOGGER.info('Identifying closest (lat,lon) points from firms dataframe for each centroid.')
+        # Identifies the unique (lat,lon) points of the firms dataframe -> lat_lon_uni
+        # Set the same index value for each duplicate (lat,lon) points -> lat_lon_cpy
+        # This firms['index'] = lat_lon_cpy is used after to fill up the intensity matrix
+        lat_lon = np.array(firms[['latitude', 'longitude']].values)
+        lat_lon_uni, lat_lon_cpy = np.unique(lat_lon, return_inverse=True, axis=0)
+        firms['index'] = lat_lon_cpy
+
+        # Index of the closest (lat,lon) points of firms dataframe for each centroids
+        # If latlon = -1 -> no (lat,lon) points close enought (compare to threshold)
+        # from the centroid
+        latlon = interpol_index(lat_lon_uni, centroids.coord, threshold=THRESH_INTERP)
+
+        LOGGER.info('Computing brightness matrix.')
+        num_events = int(max(firms['event_id'].values))
+        num_centroids = int(len(centroids.id))
+        brightness = sparse.lil_matrix(np.zeros((num_events, num_centroids)))
+
+        LOGGER.debug('Filling up the matrix.')
+        # Matrix intensity: events x centroids
+        # Creation a of temporary firms dataframe (temp_firms)
+        # containing index and brightness for one event
+        # To take into account (lat,lon) points with redudndant coordinates:
+        # identification of the unique values of temp_firms['index'] -> index_uni
+        # set the same index value for each duplicate (lat,lon) points -> index_cpy
+        # For one event, if more than one points of firms dataframe have the same coordinates
+        # take the maximum brightness value of these points (maximal damages).
+        # Fill the matrix
+
+        for ev_idx, ev_id in enumerate(np.unique(firms['event_id'].values)):
+            temp_firms = firms.loc[firms['event_id'] == ev_id, ['index', 'brightness']]
+            index_uni, index_cpy = np.unique(temp_firms['index'].values, return_inverse=True, axis=0)
+            bright = np.zeros(index_uni.size)
+            for index_idx, index in enumerate(index_uni):
+                bright[index_idx] = np.max(temp_firms['brightness'][index_uni[index_cpy] == index])
+            print(temp_firms)
+            for idx, val in enumerate(index_uni):
+                centr_idx = np.argwhere(latlon == val)
+                brightness[ev_idx, centr_idx] = bright[idx]
+        brightness = brightness.tocsr()
+        return brightness
+    
+    #Probabilistic event
+    @staticmethod
+    def _random_ignition(centroids):
+        """Select randomly a centroid as ignition starting point.
+
+        Parameters:
+            centroids (Centroids): Centroids instance. Use global
+                centroids if not provided.
+            
+
+        Returns: ignition_start (int): centroids.id of the centroid randomly selected
+            as ignition starting point. 
+            
+        """
+        ignition_start = randint(0, centroids.id.size)
+        
+        return ignition_start
+    
+    @staticmethod
+    def _propagation (centroids, ignition_start):
+        """Propagate the fire from the ignition point with a cellular automat.
+        
+        Parameters:
+            centroids (Centroids): Centroids instance.
+            ignition_start (int): id of the centroid randomly selected as 
+                ignition starting point.
+                
+        Returns: 
+             
+            
+        """
+        
+        fire = centroids.id.reshape(-1,1)
+        fire = pd.DataFrame.from_dict(fire)
+        fire.columns = ['centr_id']
+        fire['val'] = np.zeros(centroids.id.size)
+        
+        #step 0: change in val only for the ignition starting point
+        for raw, centr_id in enumerate(fire['centr_id'].values):
+            if fire.at[raw, 'centr_id'] == ignition_start:
+                fire.at[raw, 'val'] = 1
+                
+        #next steps: 
+        lat_uni = list(np.unique(centroids.lat))
+        dlat = []
+        lon_uni = np.unique(centroids.lon)
+        dlon = []
+        
+        for x in iter(lat_uni):
+            d_lat = x - next(iter(lat_uni))
+            dlat.append(d_lat)
+        for x in iter(lon_uni):
+            d_lon = x - next(iter(lon_uni))
+            dlon.append(d_lon)
+        dlat = dlat[1]
+        dlon = dlon[1]
+        
+        
+        
+        
+        
+        
+        
---
+++ climada/hazard/test/test_bush_fire.py
@@ -0,0 +1,217 @@
+"""
+Test Bush fire class
+"""
+
+import os
+import unittest
+
+
+from climada.hazard.bush_fire import BushFire
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
+TEST_FIRMS = os.path.join(DATA_DIR, "Victoria_firms.csv")
+#TEST_FIRMS = os.path.join(DATA_DIR, "Spain_2013-2018_firms.csv")
+TEST_SYNTH = os.path.join(DATA_DIR, "Firms_synthetique.csv")
+description = ''
+
+class TestReaderFirms(unittest.TestCase):
+    """Test loading functions from the BushFire class"""
+
+    def test_read_one_pass(self):
+         bf = BushFire()
+         firms, csv_firms, description = bf._read_firms_csv(TEST_FIRMS)
+
+         self.assertEqual(firms['latitude'][0], -38.104)
+         self.assertEqual(firms['longitude'][0], 146.388)
+         self.assertEqual(firms['latitude'].iloc[-1], -36.016)
+         self.assertEqual(firms['longitude'].iloc[-1], 146.97)
+         self.assertFalse(firms['datenum'][0] == firms['datenum'][29])
+
+    def test_cons_days(self):
+        bf = BushFire()
+        firms, csv_firms, description = bf._read_firms_csv(TEST_FIRMS)
+        firms = bf._firms_cons_days(firms)
+
+        self.assertEqual(firms['cons_id'][27], 0)
+        self.assertEqual(firms['cons_id'][28], 1)
+        self.assertEqual(firms['cons_id'][194], 1)
+        self.assertEqual(firms['cons_id'][195], 2)
+        self.assertEqual(firms['cons_id'][197], 2)
+        self.assertEqual(firms['cons_id'][198], 3)
+        self.assertEqual(firms['cons_id'][2136], 3)
+        self.assertEqual(firms['cons_id'][2137], 4)
+
+    def test_clustering(self):
+         bf = BushFire()
+         firms, csv_firms, description = bf._read_firms_csv(TEST_FIRMS)
+         #add cons_id
+         firms = bf._firms_cons_days(firms)
+         #add clus_id
+         firms = bf._firms_clustering(firms)
+
+         self.assertEqual(max((firms['clus_id'][:28]).values), 2)
+         self.assertEqual(max((firms['clus_id'][195:198]).values), 1)
+
+    def test_event_one_pass(self):
+        bf = BushFire()
+        firms, csv_firms, description = bf._read_firms_csv(TEST_FIRMS)
+        firms = bf._firms_cons_days(firms)
+        firms = bf._firms_clustering(firms)
+        #add event_id
+        firms = bf._firms_event(firms)
+        self.assertEqual(max((firms['event_id'][:28]).values), 3)
+        self.assertEqual(max((firms['event_id'][28:195]).values), 10)
+
+    def test_brightness_one_pass(self):
+        bf = BushFire()
+        firms, csv_firms, description = bf._read_firms_csv(TEST_FIRMS)
+        firms = bf._firms_cons_days(firms)
+        firms = bf._firms_clustering(firms)
+        firms = bf._firms_event(firms)
+        centroids = bf._centroids_creation(firms)
+
+
+        brightness = bf._calc_brightness(firms, centroids)
+
+        # fill in Hazard file
+#    def test_bush_fire_one_pass(self):
+#         bf = BushFire()
+#
+#         bf.set_bush_fire (TEST_FIRMS, description)
+#         bf.check()
+#
+#         bf.plot_intensity(event=0)
+#         bf.plot_intensity(event=1)
+#         bf.plot_intensity(event=2)
+#         bf.plot_intensity(event=3)
+#         bf.plot_intensity(event=4)
+#         bf.plot_intensity(event=10)
+#         bf.plot_intensity(event=12)
+#         bf.plot_intensity(event=13)
+
+
+
+# Execute Tests
+TESTS = unittest.TestLoader().loadTestsFromTestCase(TestReaderFirms)
+#TESTS.addTests(unittest.TestLoader().loadTestsFromTestCase(TestReader))
+#unittest.TextTestRunner(verbosity=2).run(TESTS)
+
+class TestReaderDF(unittest.TestCase):
+    """Test loading functions from the BushFire class for synthetic data"""
+    
+    @staticmethod
+    def _read_firms_synth(firms, description=''):
+        """Read synthetic files from bushfire data.
+
+        Parameters:
+            firms: dataframe with latitude, longitude, acquisition date (acq_date)
+            description (str, optional): description of the events
+
+        Returns:
+            firms, description
+        """
+
+        for index, acq_date in enumerate(firms['acq_date'].values):
+            datenum = datetime.strptime(acq_date, '%Y-%M-%d').toordinal()
+            firms.at[index, 'datenum'] = datenum
+        return firms, description
+
+    def test_read_one_pass(self):
+        bf = BushFire()
+        firms, description = bf._read_firms_synth(firms)
+
+        self.assertEqual(firms['latitude'][0], -38.104)
+        self.assertEqual(firms['longitude'][0], 146.388)
+        self.assertEqual(firms['latitude'].iloc[-1], -38.104)
+        self.assertEqual(firms['longitude'].iloc[-1], 146.388)
+        self.assertFalse(firms['datenum'][0] == firms['datenum'][9])
+
+    def test_cons_days(self):
+        bf = BushFire()
+        firms, description = bf._read_firms_synth(firms)
+        # add cons_id
+        firms = bf._firms_cons_days(firms)
+
+        self.assertEqual(firms['cons_id'][0], 0)
+        self.assertEqual(firms['cons_id'][7], 0)
+        self.assertEqual(firms['cons_id'][8], 0)
+        self.assertEqual(firms['cons_id'][9], 0)
+        self.assertEqual(firms['cons_id'][10], 0)
+        self.assertEqual(firms['cons_id'][11], 1)
+        self.assertEqual(firms['cons_id'][12], 1)
+        self.assertEqual(firms['cons_id'][13], 2)
+
+    def clustering(self):
+         bf = BushFire()
+         firms, description = bf._read_firms_synth(firms)
+         firms = bf._firms_cons_days(firms)
+         #add clus_id
+         firms = bf._firms_clustering(firms)
+
+         self.assertEqual(max((firms['clus_id'][:10]).values), 2)
+         self.assertEqual(max((firms['clus_id'][11:13]).values), 0)
+
+    def test_event_one_pass(self):
+        bf = BushFire()
+        firms, description = bf._read_firms_synth(firms)
+        firms = bf._firms_cons_days(firms)
+        firms = bf._firms_clustering(firms)
+        #add event_id
+        firms = bf._firms_event(firms)
+        centroids = bf._centroids_creation(firms)
+
+        self.assertEqual(max((firms['event_id'][:10]).values), 3)
+        self.assertEqual(firms['event_id'][11], 4)
+        self.assertEqual(firms['event_id'][12], 4)
+        self.assertEqual(firms['event_id'][13], 5)
+
+    def test_brightness_one_pass(self):
+        bf = BushFire()
+
+        firms = {'latitude' : [-38.104, -38.104, -38.104, -38.093, -38.095,
+             -37.433, -37.421, -37.423, -37.45, -38.104, -38.104, -38.104,
+             -38.095, -37.45 ],
+        'longitude' : [146.388, 146.388, 146.388, 146.397, 146.386, 142.43,
+             142.442, 142.428, 145.361, 146.388, 146.388, 146.388,
+             146.397, 145.361],
+        'brightness' : [316.5, 150, 500, 312.6, 312.7, 324.4, 373.6,
+             359.6, 312.9, 100, 400, 500, 300, 250],
+        'acq_date' : ['2006-01-24', '2006-01-24', '2006-01-24', '2006-01-24', '2006-01-24',
+             '2006-01-24', '2006-01-24', '2006-01-24', '2006-01-24', '2006-01-25', '2006-01-25',
+             '2006-01-28', '2006-01-28', '2006-01-30']}
+        firms = pd.DataFrame.from_dict(firms)
+
+        firms, description = bf._read_firms_synth(firms)
+        firms = bf._firms_cons_days(firms)
+        firms = bf._firms_clustering(firms)
+        firms = bf._firms_event(firms)
+        centroids = bf._centroids_creation(firms)
+#        # add brightness matrix
+#        brightness = bf._calc_brightness(firms)
+#
+#        self.assertEqual(brightness.get_shape(),(5,10000))
+#        self.assertEqual(brightness[0,98], 500)
+#        self.assertEqual(brightness[0,198], 312.7)
+#        self.assertEqual(brightness[0,298], 312.6)
+#        self.assertEqual(brightness[1,9572], 312.9)
+#        self.assertEqual(brightness[2,9699], 324.4)
+#        self.assertEqual(brightness[2,9899], 373.6)
+#        self.assertEqual(brightness[3,98], 500)
+#        self.assertEqual(brightness[3,198], 300)
+#        self.assertEqual(brightness[4,9572], 250)
+
+#
+        # fill in Hazard file
+#    def test_bush_fire_one_pass(self):
+#         bf = BushFire()
+#
+#         bf.set_bush_fire (TEST_FIRMS, description)
+#         bf.check()
+#
+#         bf.plot_intensity(event=1)
+
+
+
+# Execute Tests
+TESTS = unittest.TestLoader().loadTestsFromTestCase(TestReaderDF)
+unittest.TextTestRunner(verbosity=2).run(TESTS)
