f7c5c718d0c0c8886772cd9e381ca8eb41d1a618
gabriela <g.aznar.siguan@gmail.com>
2019-03-29 19:01:11 +0100

    rename append and extend methodsgit add climada/entity/measures/test/test_meas_set.py

--- climada/entity/measures/test/test_meas_set.py
+++ climada/entity/measures/test/test_meas_set.py
@@ -54,14 +54,13 @@ class TestContainer(unittest.TestCase):
         """Test error is raised when wrong ImpactFunc provided."""
         meas = MeasureSet()
         act_1 = Measure()
-        with self.assertLogs('climada.entity.measures.measure_set', level='ERROR') as cm:
-            with self.assertRaises(ValueError):
-                meas.add_measure(act_1)
-        self.assertIn("Input Measure's name not set.", cm.output[0])
+        with self.assertLogs('climada.entity.measures.measure_set', level='WARNING') as cm:
+            meas.append(act_1)
+        self.assertIn("Input Measure's hazard type not set.", cm.output[0])
 
         with self.assertLogs('climada.entity.measures.measure_set', level='ERROR') as cm:
             with self.assertRaises(ValueError):
-                meas.add_measure(45)
+                meas.append(45)
         self.assertIn("Input value is not of type Measure.", cm.output[0])
 
     def test_remove_measure_pass(self):
@@ -69,18 +68,20 @@ class TestContainer(unittest.TestCase):
         meas = MeasureSet()
         act_1 = Measure()
         act_1.name = 'Mangrove'
-        meas.add_measure(act_1)
-        meas.remove_measure('Mangrove')
-        self.assertEqual(0, len(meas._data))
+        act_1.haz_type = 'FL'
+        meas.append(act_1)
+        meas.remove_measure(name='Mangrove')
+        self.assertEqual(0, meas.size())
 
     def test_remove_wrong_error(self):
         """Test error is raised when invalid inputs."""
         meas = MeasureSet()
         act_1 = Measure()
         act_1.name = 'Mangrove'
-        meas.add_measure(act_1)
-        with self.assertLogs('climada.entity.measures.measure_set', level='WARNING') as cm:
-            meas.remove_measure('Seawall')
+        act_1.haz_type = 'FL'
+        meas.append(act_1)
+        with self.assertLogs('climada.entity.measures.measure_set', level='INFO') as cm:
+            meas.remove_measure(name='Seawall')
         self.assertIn('No Measure with name Seawall.', cm.output[0])
 
     def test_get_names_pass(self):
@@ -88,55 +89,62 @@ class TestContainer(unittest.TestCase):
         meas = MeasureSet()
         act_1 = Measure()
         act_1.name = 'Mangrove'
-        meas.add_measure(act_1)
+        act_1.haz_type = 'FL'
+        meas.append(act_1)
         self.assertEqual(1, len(meas.get_names()))
-        self.assertEqual(['Mangrove'], meas.get_names())
+        self.assertEqual({'FL': ['Mangrove']}, meas.get_names())
 
         act_2 = Measure()
         act_2.name = 'Seawall'
-        meas.add_measure(act_2)
-        self.assertEqual(2, len(meas.get_names()))
-        self.assertIn('Mangrove', meas.get_names())
-        self.assertIn('Seawall', meas.get_names())
+        act_2.haz_type = 'FL'
+        meas.append(act_2)
+        self.assertEqual(2, len(meas.get_names('FL')))
+        self.assertIn('Mangrove', meas.get_names('FL'))
+        self.assertIn('Seawall', meas.get_names('FL'))
 
     def test_get_measure_pass(self):
         """Test normal functionality of get_measure method."""
         meas = MeasureSet()
         act_1 = Measure()
         act_1.name = 'Mangrove'
-        meas.add_measure(act_1)
-        self.assertIs(act_1, meas.get_measure('Mangrove'))
+        act_1.haz_type = 'FL'
+        meas.append(act_1)
+        self.assertIs(act_1, meas.get_measure(name='Mangrove')[0])
 
         act_2 = Measure()
         act_2.name = 'Seawall'
-        meas.add_measure(act_2)
-        self.assertIs(act_1, meas.get_measure('Mangrove'))
-        self.assertIs(act_2, meas.get_measure('Seawall'))
-        self.assertEqual(2, len(meas.get_measure()))
+        act_2.haz_type = 'FL'
+        meas.append(act_2)
+        self.assertIs(act_1, meas.get_measure(name='Mangrove')[0])
+        self.assertIs(act_2, meas.get_measure(name='Seawall')[0])
+        self.assertEqual(2, len(meas.get_measure('FL')))
 
     def test_get_measure_wrong_error(self):
         """Test get_measure method with wrong inputs."""
         meas = MeasureSet()
         act_1 = Measure()
         act_1.name = 'Seawall'
-        meas.add_measure(act_1)
+        act_1.haz_type = 'FL'
+        meas.append(act_1)
         self.assertEqual([], meas.get_measure('Mangrove'))
 
     def test_num_measures_pass(self):
         """Test num_measures function."""
         meas = MeasureSet()
-        self.assertEqual(0, meas.num_measures())
+        self.assertEqual(0, meas.size())
         act_1 = Measure()
         act_1.name = 'Mangrove'
-        meas.add_measure(act_1)
-        self.assertEqual(1, meas.num_measures())
-        meas.add_measure(act_1)
-        self.assertEqual(1, meas.num_measures())
+        act_1.haz_type = 'FL'
+        meas.append(act_1)
+        self.assertEqual(1, meas.size())
+        meas.append(act_1)
+        self.assertEqual(1, meas.size())
 
         act_2 = Measure()
         act_2.name = 'Seawall'
-        meas.add_measure(act_2)
-        self.assertEqual(2, meas.num_measures())
+        act_2.haz_type = 'FL'
+        meas.append(act_2)
+        self.assertEqual(2, meas.size())
 
 class TestChecker(unittest.TestCase):
     """Test check functionality of the MeasureSet class"""
@@ -145,12 +153,13 @@ class TestChecker(unittest.TestCase):
         """Wrong intensity definition"""
         meas = MeasureSet()
         act_1 = Measure()
+        act_1.haz_type = 'TC'
         act_1.name = 'Mangrove'
         act_1.hazard_inten_imp = (1, 2, 3)
         act_1.color_rgb = np.array([1, 1, 1])
         act_1.mdd_impact = (1, 2)
         act_1.paa_impact = (1, 2)
-        meas.add_measure(act_1)
+        meas.append(act_1)
 
         with self.assertLogs('climada.util.checker', level='ERROR') as cm:
             with self.assertRaises(ValueError):
@@ -163,11 +172,12 @@ class TestChecker(unittest.TestCase):
         meas = MeasureSet()
         act_1 = Measure()
         act_1.name = 'Mangrove'
+        act_1.haz_type = 'DR'
         act_1.color_rgb = (1, 2)
         act_1.mdd_impact = (1, 2)
         act_1.paa_impact = (1, 2)
         act_1.hazard_inten_imp = (1, 2)
-        meas.add_measure(act_1)
+        meas.append(act_1)
 
         with self.assertLogs('climada.util.checker', level='ERROR') as cm:
             with self.assertRaises(ValueError):
@@ -179,11 +189,12 @@ class TestChecker(unittest.TestCase):
         meas = MeasureSet()
         act_1 = Measure()
         act_1.name = 'Mangrove'
+        act_1.haz_type = 'DR'
         act_1.color_rgb = np.array([1, 1, 1])
         act_1.mdd_impact = (1)
         act_1.paa_impact = (1, 2)
         act_1.hazard_inten_imp = (1, 2)
-        meas.add_measure(act_1)
+        meas.append(act_1)
 
         with self.assertLogs('climada.util.checker', level='ERROR') as cm:
             with self.assertRaises(ValueError):
@@ -195,11 +206,12 @@ class TestChecker(unittest.TestCase):
         meas = MeasureSet()
         act_1 = Measure()
         act_1.name = 'Mangrove'
+        act_1.haz_type = 'TC'
         act_1.color_rgb = np.array([1, 1, 1])
         act_1.mdd_impact = (1, 2)
         act_1.paa_impact = (1, 2, 3, 4)
         act_1.hazard_inten_imp = (1, 2)
-        meas.add_measure(act_1)
+        meas.append(act_1)
 
         with self.assertLogs('climada.util.checker', level='ERROR') as cm:
             with self.assertRaises(ValueError):
@@ -211,56 +223,80 @@ class TestChecker(unittest.TestCase):
         meas = MeasureSet()
         act_1 = Measure()
         act_1.name = 'LaLa'
-        meas._data['LoLo'] = act_1
-        with self.assertRaises(ValueError) as error:
-            meas.check()
-        self.assertEqual('Wrong Measure.name: LoLo != LaLa', \
-                         str(error.exception))
+        act_1.haz_type = 'FL'
+        meas._data['FL'] = dict()
+        meas._data['FL']['LoLo'] = act_1
 
-class TestAppend(unittest.TestCase):
-    """Check append function"""
-    def test_append_to_empty_same(self):
-        """Append MeasureSet to empty one."""
+        with self.assertLogs('climada.entity.measures.measure_set', level='ERROR') as cm:
+            with self.assertRaises(ValueError):
+                meas.check()
+        self.assertIn('Wrong Measure.name: LoLo != LaLa', cm.output[0])
+
+    def test_def_color(self):
+        """ Test default grey scale used when no color set """
+        meas = MeasureSet()
+        act_1 = Measure()
+        act_1.name = 'LaLa'
+        act_1.haz_type = 'FL'
+        meas.append(act_1)
+
+        act_2 = Measure()
+        act_2.name = 'LoLo'
+        act_2.haz_type = 'FL'
+        meas.append(act_2)
+
+        meas.check()
+        self.assertTrue(np.array_equal(meas.get_measure('FL', 'LaLa').color_rgb, np.ones(4)))
+        self.assertTrue(np.allclose(meas.get_measure('FL', 'LoLo').color_rgb,
+            np.array([0., 0., 0., 1.0])))
+
+class TestExtend(unittest.TestCase):
+    """Check extend function"""
+    def test_extend_to_empty_same(self):
+        """Extend MeasureSet to empty one."""
         meas = MeasureSet()
         meas_add = MeasureSet()
         act_1 = Measure()
         act_1.name = 'Mangrove'
+        act_1.haz_type = 'TC'
         act_1.color_rgb = np.array([1, 1, 1])
         act_1.mdd_impact = (1, 2)
         act_1.paa_impact = (1, 2)
         act_1.hazard_inten_imp = (1, 2)
-        meas_add.add_measure(act_1)
+        meas_add.append(act_1)
 
-        meas.append(meas_add)
+        meas.extend(meas_add)
         meas.check()
 
-        self.assertEqual(meas.num_measures(), 1)
-        self.assertEqual(meas.get_names(), [act_1.name])
+        self.assertEqual(meas.size(), 1)
+        self.assertEqual(meas.get_names(), {'TC': ['Mangrove']})
 
-    def test_append_equal_same(self):
-        """Append the same MeasureSet. The inital MeasureSet is obtained."""
+    def test_extend_equal_same(self):
+        """Extend the same MeasureSet. The inital MeasureSet is obtained."""
         meas = MeasureSet()
         meas_add = MeasureSet()
         act_1 = Measure()
         act_1.name = 'Mangrove'
+        act_1.haz_type = 'TC'
         act_1.color_rgb = np.array([1, 1, 1])
         act_1.mdd_impact = (1, 2)
         act_1.paa_impact = (1, 2)
         act_1.hazard_inten_imp = (1, 2)
-        meas.add_measure(act_1)
-        meas_add.add_measure(act_1)
+        meas.append(act_1)
+        meas_add.append(act_1)
 
-        meas.append(meas_add)
+        meas.extend(meas_add)
         meas.check()
 
-        self.assertEqual(meas.num_measures(), 1)
-        self.assertEqual(meas.get_names(), [act_1.name])
+        self.assertEqual(meas.size(), 1)
+        self.assertEqual(meas.get_names(), {'TC': ['Mangrove']})
 
-    def test_append_different_append(self):
-        """Append MeasureSet with same and new values. The actions
+    def test_extend_different_extend(self):
+        """Extend MeasureSet with same and new values. The actions
         with repeated name are overwritten."""
         act_1 = Measure()
         act_1.name = 'Mangrove'
+        act_1.haz_type = 'TC'
         act_1.color_rgb = np.array([1, 1, 1])
         act_1.mdd_impact = (1, 2)
         act_1.paa_impact = (1, 2)
@@ -268,6 +304,7 @@ class TestAppend(unittest.TestCase):
 
         act_11 = Measure()
         act_11.name = 'Mangrove'
+        act_11.haz_type = 'TC'
         act_11.color_rgb = np.array([1, 1, 1])
         act_11.mdd_impact = (1, 2)
         act_11.paa_impact = (1, 3)
@@ -275,23 +312,24 @@ class TestAppend(unittest.TestCase):
 
         act_2 = Measure()
         act_2.name = 'Anything'
+        act_2.haz_type = 'TC'
         act_2.color_rgb = np.array([1, 1, 1])
         act_2.mdd_impact = (1, 2)
         act_2.paa_impact = (1, 2)
         act_2.hazard_inten_imp = (1, 2)
 
         meas = MeasureSet()
-        meas.add_measure(act_1)
+        meas.append(act_1)
         meas_add = MeasureSet()
-        meas_add.add_measure(act_11)
-        meas_add.add_measure(act_2)
+        meas_add.append(act_11)
+        meas_add.append(act_2)
 
-        meas.append(meas_add)
+        meas.extend(meas_add)
         meas.check()
 
-        self.assertEqual(meas.num_measures(), 2)
-        self.assertEqual(meas.get_names(), [act_1.name, act_2.name])
-        self.assertEqual(meas.get_measure(act_1.name).paa_impact, act_11.paa_impact)
+        self.assertEqual(meas.size(), 2)
+        self.assertEqual(meas.get_names(), {'TC': ['Mangrove', 'Anything']} )
+        self.assertEqual(meas.get_measure(name=act_1.name)[0].paa_impact, act_11.paa_impact)
 
 class TestReaderExcel(unittest.TestCase):
     """Test reader functionality of the MeasuresExcel class"""
@@ -305,9 +343,9 @@ class TestReaderExcel(unittest.TestCase):
         # Check results
         n_meas = 4
 
-        self.assertEqual(len(meas.get_measure()), n_meas)
+        self.assertEqual(meas.size(), n_meas)
 
-        act_man = meas.get_measure('Mangroves')
+        act_man = meas.get_measure(name='Mangroves')[0]
         self.assertEqual(act_man.name, 'Mangroves')
         self.assertEqual(type(act_man.color_rgb), np.ndarray)
         self.assertEqual(len(act_man.color_rgb), 3)
@@ -322,7 +360,7 @@ class TestReaderExcel(unittest.TestCase):
         self.assertEqual(act_man.risk_transf_attach, 0)
         self.assertEqual(act_man.risk_transf_cover, 0)
 
-        act_buil = meas.get_measure('Building code')
+        act_buil = meas.get_measure(name='Building code')[0]
         self.assertEqual(act_buil.name, 'Building code')
         self.assertEqual(type(act_buil.color_rgb), np.ndarray)
         self.assertEqual(len(act_buil.color_rgb), 3)
@@ -345,10 +383,10 @@ class TestReaderExcel(unittest.TestCase):
         meas = MeasureSet()
         meas.read_excel(ENT_TEMPLATE_XLS)
 
-        self.assertEqual(len(meas.get_measure()), 7)
+        self.assertEqual(meas.size(), 7)
 
         name = 'elevate existing buildings'
-        act_buil = meas.get_measure(name)
+        act_buil = meas.get_measure(name=name)[0]
         self.assertEqual(act_buil.name, name)
         self.assertEqual(act_buil.haz_type, 'TS')
         self.assertTrue(np.array_equal(act_buil.color_rgb, np.array([0.84, 0.89, 0.70])))
@@ -369,7 +407,7 @@ class TestReaderExcel(unittest.TestCase):
         self.assertEqual(act_buil.risk_transf_cover, 0)
 
         name = 'vegetation management'
-        act_buil = meas.get_measure(name)
+        act_buil = meas.get_measure(name=name)[0]
         self.assertEqual(act_buil.name, name)
         self.assertEqual(act_buil.haz_type, 'TC')
         self.assertTrue(np.array_equal(act_buil.color_rgb, np.array([0.76, 0.84, 0.60])))
@@ -389,7 +427,7 @@ class TestReaderExcel(unittest.TestCase):
         self.assertEqual(act_buil.risk_transf_attach, 0)
         self.assertEqual(act_buil.risk_transf_cover, 0)
 
-        self.assertEqual(meas.get_measure('enforce building code').imp_fun_map, '1to3')
+        self.assertEqual(meas.get_measure(name='enforce building code')[0].imp_fun_map, '1to3')
 
         self.assertEqual(meas.tag.file_name, ENT_TEMPLATE_XLS)
         self.assertEqual(meas.tag.description, '')
@@ -406,9 +444,9 @@ class TestReaderMat(unittest.TestCase):
         # Check results
         n_meas = 4
 
-        self.assertEqual(len(meas.get_measure()), n_meas)
+        self.assertEqual(meas.size(), n_meas)
 
-        act_man = meas.get_measure('Mangroves')
+        act_man = meas.get_measure(name='Mangroves')[0]
         self.assertEqual(act_man.name, 'Mangroves')
         self.assertEqual(act_man.haz_type, 'XX')
         self.assertEqual(type(act_man.color_rgb), np.ndarray)
@@ -433,7 +471,7 @@ class TestReaderMat(unittest.TestCase):
         self.assertEqual(act_man.risk_transf_cover, 0)
 
 
-        act_buil = meas.get_measure('Building code')
+        act_buil = meas.get_measure(name='Building code')[0]
         self.assertEqual(act_buil.name, 'Building code')
         self.assertEqual(act_buil.haz_type, 'XX')
         self.assertEqual(type(act_buil.color_rgb), np.ndarray)
@@ -468,6 +506,7 @@ class TestWriter(unittest.TestCase):
 
         act_1 = Measure()
         act_1.name = 'Mangrove'
+        act_1.haz_type = 'TC'
         act_1.color_rgb = np.array([1, 1, 1])
         act_1.cost = 10
         act_1.mdd_impact = (1, 2)
@@ -477,6 +516,7 @@ class TestWriter(unittest.TestCase):
 
         act_11 = Measure()
         act_11.name = 'Something'
+        act_11.haz_type = 'TC'
         act_11.color_rgb = np.array([1, 1, 1])
         act_11.mdd_impact = (1, 2)
         act_11.paa_impact = (1, 3)
@@ -485,7 +525,7 @@ class TestWriter(unittest.TestCase):
 
         act_2 = Measure()
         act_2.name = 'Anything'
-        act_2.haz_type = 'Fl'
+        act_2.haz_type = 'FL'
         act_2.color_rgb = np.array([1, 1, 1])
         act_2.mdd_impact = (1, 2)
         act_2.paa_impact = (1, 2)
@@ -494,9 +534,9 @@ class TestWriter(unittest.TestCase):
         act_2.imp_fun_map = 'map'
 
         meas_set = MeasureSet()
-        meas_set.add_measure(act_1)
-        meas_set.add_measure(act_11)
-        meas_set.add_measure(act_2)
+        meas_set.append(act_1)
+        meas_set.append(act_11)
+        meas_set.append(act_2)
 
         file_name = 'test_meas.xlsx'
         meas_set.write_excel(file_name)
@@ -507,7 +547,10 @@ class TestWriter(unittest.TestCase):
         self.assertEqual(meas_read.tag.file_name, file_name)
         self.assertEqual(meas_read.tag.description, 'test')
 
-        for meas in meas_read.get_measure():
+        meas_list = meas_read.get_measure('TC')
+        meas_list.extend(meas_read.get_measure('FL'))
+
+        for meas in meas_list:
             if meas.name == 'Mangrove':
                 meas_ref = act_1
             elif meas.name == 'Something':
@@ -533,7 +576,7 @@ class TestWriter(unittest.TestCase):
 # Execute Tests
 TESTS = unittest.TestLoader().loadTestsFromTestCase(TestContainer)
 TESTS.addTests(unittest.TestLoader().loadTestsFromTestCase(TestChecker))
-TESTS.addTests(unittest.TestLoader().loadTestsFromTestCase(TestAppend))
+TESTS.addTests(unittest.TestLoader().loadTestsFromTestCase(TestExtend))
 TESTS.addTests(unittest.TestLoader().loadTestsFromTestCase(TestReaderExcel))
 TESTS.addTests(unittest.TestLoader().loadTestsFromTestCase(TestWriter))
 TESTS.addTests(unittest.TestLoader().loadTestsFromTestCase(TestReaderMat))
