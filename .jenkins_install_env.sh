bdf72f3ccb928188e629ba3cd94c9d4ce1b86650
gabriela <g.aznar.siguan@gmail.com>
2019-04-29 07:57:38 +0200

    add xmlrunner package to climada

--- .jenkins_install_env.sh
+++ .jenkins_install_env.sh
@@ -4,5 +4,5 @@ conda remove --name climada_env --all
 conda env create -f requirements/env_climada.yml --name climada_env
 
 source activate climada_env
-python3 tests_install.py report
+make install_test
 conda deactivate
