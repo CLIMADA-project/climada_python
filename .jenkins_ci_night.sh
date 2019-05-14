f38e07654ca9b24779e24a5c1168f8d84585baa1
gabriela <g.aznar.siguan@gmail.com>
2019-05-14 22:50:57 +0200

    remove env lists

--- .jenkins_ci_night.sh
+++ .jenkins_ci_night.sh
@@ -1,14 +1,7 @@
 #!/bin/bash -e
 
 source activate climada_env
-
-pip list
-conda list
-
 conda env update --file requirements/env_developer.yml
-
-pip list
-conda list
 
 make lint
 make test
--- .jenkins_install_env.sh
+++ .jenkins_install_env.sh
@@ -4,7 +4,5 @@ conda remove --name climada_env --all
 conda env create -f requirements/env_climada.yml --name climada_env
 
 source activate climada_env
-pip list
-conda list
 make install_test
 conda deactivate
