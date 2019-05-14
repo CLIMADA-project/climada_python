60dc5b49155f5420a0c224b3a870b26b9cc24ddb
gabriela <g.aznar.siguan@gmail.com>
2019-05-14 21:01:02 +0200

    show installed packages

--- .jenkins_install_env.sh
+++ .jenkins_install_env.sh
@@ -4,5 +4,7 @@ conda remove --name climada_env --all
 conda env create -f requirements/env_climada.yml --name climada_env
 
 source activate climada_env
+pip list
+conda list
 make install_test
 conda deactivate
