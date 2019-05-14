f7763d73e3441b25a312bb671d45d88d1d854672
gabriela <g.aznar.siguan@gmail.com>
2019-05-14 21:05:31 +0200

    show packages

--- .jenkins_ci_night.sh
+++ .jenkins_ci_night.sh
@@ -1,7 +1,15 @@
 #!/bin/bash -e
 
 source activate climada_env
+
+pip list
+conda list
+
 conda env update --file requirements/env_developer.yml
+
+pip list
+conda list
+
 make lint
 make test
 conda deactivate
