d8b237235d57f52bf71a1cf18b14289c311ba5d3
gabriela <g.aznar.siguan@gmail.com>
2019-09-17 19:18:33 +0200

    add reader and writer of TCTracks in netcdf (slow and big compared to pickle in this case).

--- climada/hazard/tc_tracks.py
+++ climada/hazard/tc_tracks.py
@@ -406,6 +406,32 @@ class TCTracks():
         axis.legend(leg_lines, leg_names, loc=0)
         return axis
 
+    def write_netcdf(self, folder_name):
+        """ Write a netcdf file per track with track.sid name in given folder.
+
+        Parameter:
+            folder_name (str): folder name where to write files
+        """
+        list_path = [os.path.join(folder_name, track.sid+'.nc') for track in self.data]
+        LOGGER.info('Writting %s files.', self.size)
+        for track in self.data:
+            track.attrs['orig_event_flag'] = int(track.orig_event_flag)
+        xr.save_mfdataset(self.data, list_path)
+
+    def read_netcdf(self, folder_name):
+        """ Read all netcdf files contained in folder and fill a track per file.
+
+        Parameters:
+            folder_name (str): folder name where to write files
+        """
+        file_tr = get_file_names(folder_name)
+        LOGGER.info('Reading %s files.', len(file_tr))
+        self.data = list()
+        for file in file_tr:
+            track = xr.open_dataset(file)
+            track.attrs['orig_event_flag'] = bool(track.orig_event_flag)
+            self.data.append(track)
+
     @staticmethod
     @jit(parallel=True)
     def _one_rnd_walk(track, ens_size, ens_amp0, ens_amp, max_angle, rnd_vec):
