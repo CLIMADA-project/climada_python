312d330209b83ac4100512ac74d792684d489ea4
Thomas Vogt <thomas.vogt@pik-potsdam.de>
2020-06-23 16:08:54 +0200

    Rename NAT_REG_ID to RIVER_FLOOD_REGIONS_CSV

--- climada/util/constants.py
+++ climada/util/constants.py
@@ -34,7 +34,7 @@ __all__ = ['SOURCE_DIR',
            'NATEARTH_CENTROIDS_150AS',
            'NATEARTH_CENTROIDS_360AS',
            'DEMO_GDP2ASSET',
-           'NAT_REG_ID',
+           'RIVER_FLOOD_REGIONS_CSV',
            'TC_ANDREW_FL',
            'HAZ_DEMO_H5',
            'EXP_DEMO_H5',
@@ -58,7 +58,7 @@ ISIMIP_GPWV3_NATID_150AS = os.path.join(SYSTEM_DIR, 'NatID_grid_0150as.nc')
 """ Compressed version of National Identifier Grid in 150 arc-seconds from
     ISIMIP project, based on GPWv3:
 
-        ISIMIP2a/InputData/landuse_humaninfluences/population/ID_GRID/Nat_id_grid_ISIMIP.nc
+    ISIMIP2a/InputData/landuse_humaninfluences/population/ID_GRID/Nat_id_grid_ISIMIP.nc
 
     More references:
 
@@ -85,26 +85,30 @@ ENT_TEMPLATE_XLS = os.path.join(SYSTEM_DIR, 'entity_template.xlsx')
 
 HAZ_TEMPLATE_XLS = os.path.join(SYSTEM_DIR, 'hazard_template.xlsx')
 """ Hazard template in xls format."""
-NAT_REG_ID = os.path.join(SYSTEM_DIR, 'NatRegIDs.csv')
-""" Look-up table ISO3 codes"""
 
+RIVER_FLOOD_REGIONS_CSV = os.path.join(SYSTEM_DIR, 'NatRegIDs.csv')
+""" Look-up table for river flood module """
 
 HAZ_DEMO_FL = os.path.join(DATA_DIR, 'demo', 'SC22000_VE__M1.grd.gz')
 """ Raster file of flood over Venezuela. Model from GAR2015"""
 
-
-HAZ_DEMO_FLDDPH = os.path.join(DATA_DIR, 'demo', 'flddph_WaterGAP2_miroc5_historical_flopros_gev_picontrol_2000_0.1.nc')
+HAZ_DEMO_FLDDPH = os.path.join(DATA_DIR, 'demo',
+    'flddph_WaterGAP2_miroc5_historical_flopros_gev_picontrol_2000_0.1.nc')
 """ NetCDF4 Flood depth from isimip simulations"""
 
-
-HAZ_DEMO_FLDFRC = os.path.join(DATA_DIR, 'demo', 'fldfrc_WaterGAP2_miroc5_historical_flopros_gev_picontrol_2000_0.1.nc')
+HAZ_DEMO_FLDFRC = os.path.join(DATA_DIR, 'demo',
+    'fldfrc_WaterGAP2_miroc5_historical_flopros_gev_picontrol_2000_0.1.nc')
 """ NetCDF4 Flood fraction from isimip simulations"""
 
 HAZ_DEMO_MAT = os.path.join(DATA_DIR, 'demo', 'atl_prob.mat')
-""" Hazard demo from climada in MATLAB: hurricanes from 1851 to 2011 over Florida with 100 centroids."""
+""" Hazard demo from climada in MATLAB: hurricanes from 1851 to 2011 over
+    Florida with 100 centroids.
+"""
 
 HAZ_DEMO_H5 = os.path.join(DATA_DIR, 'demo', 'tc_fl_1975_2011.h5')
-""" Hazard demo in h5 format: ibtracs from 1975 to 2011 over Florida with 2500 centroids."""
+""" Hazard demo in h5 format: ibtracs from 1975 to 2011 over Florida with
+    2500 centroids.
+"""
 
 DEMO_GDP2ASSET = os.path.join(DATA_DIR, 'demo', 'gdp2asset_CHE_exposure.nc')
 """Exposure demo file for GDP2Asset"""
@@ -112,8 +116,10 @@ DEMO_GDP2ASSET = os.path.join(DATA_DIR, 'demo', 'gdp2asset_CHE_exposure.nc')
 WS_DEMO_NC = [os.path.join(DATA_DIR, 'demo', 'fp_lothar_crop-test.nc'),
               os.path.join(DATA_DIR, 'demo', 'fp_xynthia_crop-test.nc')]
 """ Winter storm in Europe files. These test files have been generated using
-the netCDF kitchen sink: ncks -d latitude,50.5,54.0 -d longitude,3.0,7.5
-./file_in.nc ./file_out.nc """
+   the netCDF kitchen sink:
+
+    ncks -d latitude,50.5,54.0 -d longitude,3.0,7.5 ./file_in.nc ./file_out.nc
+"""
 
 
 ENT_DEMO_TODAY = os.path.join(DATA_DIR, 'demo', 'demo_today.xlsx')
@@ -158,6 +164,9 @@ ISIMIP_NATID_TO_ISO = [
 
 
 RIVER_GEO_REGIONS = {
+    'NO_REGION': [
+        'CYP', 'MAC', 'MCO', 'MUS', 'PLW', 'REU', 'SMR', 'SPM', 'SYC'
+    ],
     # Southern Africa
     'SAF': [
         'AGO', 'BWA', 'LSO', 'NAM', 'SHN', 'SWZ', 'ZAF'
