fdc1edc1e15c55b73877cebbbf72e7b390736700
wjan262 <janwue@gmx.ch>
2020-07-13 15:29:13 +0200

    plot intensity landslide.py
    use plot function from base module

--- climada/hazard/landslide.py
+++ climada/hazard/landslide.py
@@ -454,13 +454,15 @@ class Landslide(Hazard):
                 self.check()
 
             if check_plots == 1:
-                fig1, ax1 = plt.subplots(nrows=1, ncols=1)
-                self.plot_raw() 
-                fig1.suptitle('Raw data: Occurrence prob of LS per year', fontsize=14)
-
-                fig2, ax2 = plt.subplots(nrows=1, ncols=1)
-                self.plot_events()
-                fig2.suptitle('Prob. LS Hazard Set n_years = %i' %n_years, fontsize=14)
+#                fig1, ax1 = plt.subplots(nrows=1, ncols=1)
+#                self.plot_raw() 
+#                fig1.suptitle('Raw data: Occurrence prob of LS per year', fontsize=14)
+#
+#                fig2, ax2 = plt.subplots(nrows=1, ncols=1)
+#                self.plot_events()
+#                fig2.suptitle('Prob. LS Hazard Set n_years = %i' %n_years, fontsize=14)
+                
+                self.plot_intensity(0)
 
             return self
 
