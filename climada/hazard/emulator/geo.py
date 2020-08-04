e6e8ba5b25ad56e31524a566ff156c4f3a4be93e
Thomas Vogt <thomas.vogt@pik-potsdam.de>
2020-08-04 12:07:07 +0200

    climada.hazard.emulator: implementation and tutorial

---
+++ climada/hazard/emulator/geo.py
@@ -0,0 +1,176 @@
+"""
+This file is part of CLIMADA.
+
+Copyright (C) 2017 ETH Zurich, CLIMADA contributors listed in AUTHORS.
+
+CLIMADA is free software: you can redistribute it and/or modify it under the
+terms of the GNU Lesser General Public License as published by the Free
+Software Foundation, version 3.
+
+CLIMADA is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along
+with CLIMADA. If not, see <https://www.gnu.org/licenses/>.
+
+---
+
+Georegion objects for the hazard event emulator.
+"""
+
+import logging
+import numpy as np
+import geopandas as gpd
+import shapely.ops
+import shapely.vectorized
+from shapely.geometry import Polygon
+
+from climada.hazard import Centroids
+from climada.util.coordinates import get_country_geometries, NE_CRS
+import climada.hazard.emulator.const as const
+
+LOGGER = logging.getLogger(__name__)
+
+
+class HazRegion():
+    """Hazard region for given geo information"""
+
+    def __init__(self, extent=None, geometry=None, country=None, season=(1, 12)):
+        """Initialize HazRegion
+
+        If several arguments are passed, the spatial intersection is taken.
+
+        Parameters
+        ----------
+        extent : tuple (min_lon, max_lon, min_lat, max_lat)
+        geometry : GeoPandas DataFrame
+        country :  str or list of str
+            Countries are represented by their ISO 3166-1 alpha-3 identifiers.
+        season : pair of int
+            First and last month of hazard-specific season within this region
+        """
+        self._set_geometry(extent=extent, geometry=geometry, country=country)
+        self.geometry['const'] = 0
+        self.shape = self.geometry.dissolve(by='const').geometry[0]
+        self.season = season
+
+
+    def _set_geometry(self, extent=None, geometry=None, country=None):
+        self.meta = {}
+
+        if extent is not None:
+            self.meta['extent'] = extent
+
+        if country is not None:
+            self.meta['country'] = country
+            if type(country) is not list:
+                country = [country]
+
+        self.geometry = get_country_geometries(country_names=country, extent=extent)
+
+        if geometry is not None:
+            self.meta['geometry'] = repr(geometry)
+            self.geometry = gpd.overlay(self.geometry, geometry, how="intersection")
+
+
+    def centroids(self, latlon=None):
+        """Return centroids in this region
+
+        Parameters
+        ----------
+        latlon : pair (lat, lon)
+            Latitude and longitude of centroids.
+            If not given, values are taken from CLIMADA's 150 arc-second base grid.
+
+        Returns
+        -------
+        centroids : climada.hazard.Centroids object
+        """
+        if latlon is None:
+            centroids = Centroids.from_base_grid(res_as=150)
+            centroids.set_meta_to_lat_lon()
+            latlon = centroids.lat, centroids.lon
+        lat, lon = latlon
+        msk = shapely.vectorized.contains(self.shape, lon, lat)
+        centroids = Centroids()
+        centroids.set_lat_lon(lat[msk], lon[msk])
+        centroids.id = np.arange(centroids.lon.shape[0])
+        return centroids
+
+
+class TCRegion(HazRegion):
+    """Hazard region with support for TC ocean basins"""
+
+    def __init__(self, tc_basin=None, season=None, **kwargs):
+        """Initialize TCRegion
+
+        The given geo information must be such that everything is contained in a single
+        TC ocean basin.
+
+        Parameters
+        ----------
+        tc_basin : str
+            TC (sub-)basin abbreviated name, such as "SIW". If not given, automatically determined
+            from geometry and basin bounds.
+        **kwargs : see HazRegion.__init__
+        """
+        self._set_geometry(**kwargs)
+
+        if tc_basin is not None:
+            df2 = get_tc_basin_geometry(tc_basin)
+            self.geometry = gpd.overlay(self.geometry, df2, how="intersection")
+            self.meta['tc_basin'] = tc_basin
+            self.tc_basin = tc_basin
+
+        self.geometry['const'] = 0
+        self.shape = self.geometry.dissolve(by='const').geometry[0]
+
+        if self.tc_basin is None:
+            self._determine_tc_basin()
+
+        if season is None:
+            season = const.TC_BASIN_SEASONS[self.tc_basin[:2]]
+        self.season = season
+
+
+    def _determine_tc_basin(self):
+        for basin in const.TC_SUBBASINS.keys():
+            TC_BASIN_GEOM = get_tc_basin_geometry(basin)
+            if all(TC_BASIN_GEOM.contains(self.shape)):
+                self.tc_basin = basin
+                break
+        if self.tc_basin is None:
+            raise ValueError("Region is not contained in a single basin!")
+        for tc_basin in const.TC_SUBBASINS[self.tc_basin]:
+            tc_basin_geom = get_tc_basin_geometry(tc_basin)
+            if all(tc_basin_geom.contains(self.shape)):
+                self.tc_basin = tc_basin
+                break
+        LOGGER.info(f"Automatically determined TC basin: {self.tc_basin}")
+
+
+def get_tc_basin_geometry(tc_basin):
+    """Get TC (sub-)basin geometry
+
+    Parameters
+    ----------
+    tc_basin : str
+        TC (sub-)basin abbreviated name, such as "SIW" or "NA".
+
+    Returns
+    -------
+    df : GeoPandas DataFrame
+    """
+    polygons = []
+    for rect in const.TC_BASIN_GEOM[tc_basin]:
+        lonmin, lonmax, latmin, latmax = rect
+        polygons.append(Polygon([
+            (lonmin, latmin),
+            (lonmin, latmax),
+            (lonmax, latmax),
+            (lonmax, latmin)
+        ]))
+    polygons = shapely.ops.unary_union(polygons)
+    polygons = gpd.GeoSeries(polygons, crs=NE_CRS)
+    return gpd.GeoDataFrame({'geometry': polygons}, crs=NE_CRS)
---
+++ climada/hazard/emulator/stats.py
@@ -0,0 +1,204 @@
+"""
+This file is part of CLIMADA.
+
+Copyright (C) 2017 ETH Zurich, CLIMADA contributors listed in AUTHORS.
+
+CLIMADA is free software: you can redistribute it and/or modify it under the
+terms of the GNU Lesser General Public License as published by the Free
+Software Foundation, version 3.
+
+CLIMADA is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along
+with CLIMADA. If not, see <https://www.gnu.org/licenses/>.
+
+---
+
+Statistical tools for the hazard event emulator.
+"""
+
+import datetime
+import logging
+
+import numpy as np
+import pandas as pd
+
+import climada.hazard.emulator.const as const
+
+LOGGER = logging.getLogger(__name__)
+
+def seasonal_average(data, season):
+    """Compute seasonal average from monthly-time series.
+
+    For seasons that are across newyear, the months after June are attributed to the following
+    year's season. For example: The 6-month season from November 1980 till April 1981 is attributed
+    to the year 1981.
+
+    The two seasons that are truncated at the beginning/end of the dataset's time period are
+    discarded. When the input data is 1980-2010, the output data will be 1981-2010, where 2010
+    corresponds to the 2009/2010 season and 1981 corresponds to the 1980/1981 season.
+
+    Parameters
+    ----------
+    data : DataFrame { year, month, ... }
+        All further columns will be averaged over.
+    season : pair of ints
+        Start/end month of season.
+
+    Returns
+    -------
+    averaged_data : DataFrame { year, ... }
+        Same format as input, but with month column removed.
+    """
+    start, end = season
+
+    if data['month'].unique().size == 1:
+        data = data.drop('month', 1)
+
+    if "month" not in data.columns:
+        return data.iloc[1:] if start > end else data
+
+    if start > end:
+        msk = (data['month'] >= start) | (data['month'] <= end)
+    else:
+        msk = (data['month'] >= start) & (data['month'] <= end)
+    data = data[msk]
+    if start > end:
+        data['year'][data['month'] > 6] += 1
+        data = data[data['year'] != data['year'].min()]
+        data = data[data['year'] != data['year'].max()]
+    data = data.reset_index(drop=True)
+    data = data.groupby('year').mean().reset_index()
+    return data.drop('month', 1)
+
+
+def seasonal_statistics(events, season):
+    """Compute seasonal statistics from given hazard event data
+
+    Parameters
+    ----------
+    events : DataFrame { year, month, intensity, ... }
+        Events outside of the given season are ignored.
+    season : pair of ints
+        Start/end month of season.
+
+    Returns
+    -------
+    haz_stats : DataFrame { year, events, intensity_mean, intensity_std, intensity_max }
+        For seasons that are across newyear, this might cover one year less than the input data
+        since truncated seasons are discarded.
+    """
+    events = events.reindex(columns=['year', 'month', 'eventcount', 'intensity'])
+    events['eventcount'] = 1
+    sea_start, sea_end = season
+    if sea_end < sea_start:
+        events['year'][events['month'] > 6] += 1
+    events = events.drop(labels=['month'], axis=1)
+
+    new_cols = ['eventcount', 'intensity_mean', 'intensity_std', 'intensity_max']
+    def collapse(group, new_cols=new_cols):
+        new_vals = [group['eventcount'].sum(),
+                    group['intensity'].mean(),
+                    group['intensity'].std(ddof=0),
+                    group['intensity'].max()]
+        return pd.Series(new_vals, index=new_cols)
+    haz_stats = events.groupby(['year']).apply(collapse).reset_index()
+
+    if sea_end < sea_start:
+        # drop first and last years as they are incomplete
+        haz_stats = haz_stats.iloc[1:-1, :].reset_index(drop=True)
+
+    return haz_stats
+
+
+def haz_max_events(hazard, min_thresh=0):
+    """Table of max intensity events for given hazard
+
+    Parameters
+    ----------
+    hazard : climada.hazard.Hazard object
+    min_thresh : float
+        Minimum intensity for event to be registered.
+
+    Returns
+    -------
+    events : DataFrame { id, name, year, month, day, lat, lon, intensity }
+        The integer value in column `id` refers to the internal order of events in the given
+        `hazard` object. `lat`, `lon` and `intensity` specify location and intensity of the maximum
+        intensity registered.
+    """
+    inten = hazard.intensity
+    exp_hazards = (inten >= min_thresh).todense()
+    exp_hazards = np.where(np.any(exp_hazards, axis=1))[0]
+    LOGGER.info(f"Condensing {inten.shape[0]} hazards to {exp_hazards.size} max events ...")
+    inten = inten[exp_hazards]
+    inten_max_ids = np.asarray(inten.argmax(axis=1)).ravel()
+    inten_max = inten[range(inten.shape[0]),inten_max_ids].todense()
+    dates = hazard.date[exp_hazards]
+    dates = [datetime.date.fromordinal(d) for d in dates]
+    return pd.DataFrame({
+        'id': exp_hazards,
+        'name': [hazard.event_name[s] for s in exp_hazards],
+        'year': np.int64([d.year for d in dates]),
+        'month': np.int64([d.month for d in dates]),
+        'day': np.int64([d.day for d in dates]),
+        'lat': hazard.centroids.lat[inten_max_ids],
+        'lon': hazard.centroids.lon[inten_max_ids],
+        'intensity': np.asarray(inten_max).ravel(),
+    })
+
+
+def normalize_seasonal_statistics(events, events_obs, region, freq_norm):
+    """Bias-corrected annual hazard statistics
+
+    Parameters
+    ----------
+    events : DataFrame { ... }
+        See `haz_max_events`.
+    events_obs : DataFrame { ... }
+        See `haz_max_events`.
+    region : HazRegion object
+        The geographical region for which to determine statistics.
+    freq_norm : DataFrame { year, freq }
+        Information about the relative surplus of hazard events per year, i.e.,
+        if `freq_norm` specifies the value 0.2 in some year, then it is
+        assumed that the number of events given for that year is 5 times as
+        large as it is predicted to be.
+
+    Returns
+    -------
+    statistics : DataFrame { year, intensity_max, intensity_mean, eventcount,
+                             intensity_max_obs, intensity_mean_obs, eventcount_obs }
+        Normalized and observed hazard statistics.
+    """
+    norm_period = [events_obs['year'].min(), events_obs['year'].max()]
+    haz_stats = {
+        'obs': seasonal_statistics(events_obs, region.season),
+        'cal': seasonal_statistics(events, region.season),
+    }
+
+    # Merge observed into modelled statistics for comparison
+    haz_stats = pd.merge(haz_stats['cal'], haz_stats['obs'], suffixes=('', '_obs'),
+                         on="year", how="left", sort=True)
+
+    # Normalize `eventcount` according to simulated frequency.
+    # In case of season across newyear, this normalizes by the year with most of
+    # hazard season, ignoring the fractional contribution from the year before.
+    haz_stats = pd.merge(haz_stats, freq_norm, on='year', how='left', sort=True)
+    haz_stats['eventcount'] *= haz_stats['freq']
+    haz_stats = haz_stats.drop(labels=['freq'], axis=1)
+
+    # Bias-correct intensity and frequency to observations in norm period
+    for col in ['eventcount', 'intensity_mean', 'intensity_std', 'intensity_max']:
+        idx = haz_stats.index[(haz_stats['year'] >= norm_period[0]) \
+                            & (haz_stats['year'] <= norm_period[1])]
+        col_data = haz_stats.loc[idx, col]
+        col_data_obs = haz_stats.loc[idx, f"{col}_obs"].fillna(0)
+        if col == 'eventcount':
+            fact = col_data_obs.sum() / col_data.sum()
+        else:
+            fact = col_data_obs.mean() / col_data.mean()
+        haz_stats[col] *= fact
+    return haz_stats
