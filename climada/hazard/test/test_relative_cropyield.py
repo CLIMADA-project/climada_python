Bugfix in historical mean calculation in relative_cropyield hazard

climada/hazard/test/test_relative_cropyield.py
@@ -66,10 +66,11 @@ class TestRelativeCropyield(unittest.TestCase):
         haz.set_rel_yield_to_int(hist_mean)
         self.assertEqual(haz.intensity_def, 'Relative Yield')
 
-        self.assertAlmostEqual(np.max(hist_mean), 8.098413, places=5)
+        self.assertEqual(np.shape(hist_mean), (1092,))
+        self.assertAlmostEqual(np.max(hist_mean), 8.397826, places=5)
         self.assertEqual(haz.intensity.shape, (5, 1092))
-        self.assertAlmostEqual(np.nanmax(haz.intensity.toarray()), 3.0, places=5)
-        self.assertAlmostEqual(haz.intensity.max(), 3.0, places=5)
+        self.assertAlmostEqual(np.nanmax(haz.intensity.toarray()), 4.0, places=5)
+        self.assertAlmostEqual(haz.intensity.max(), 4.0, places=5)
         self.assertAlmostEqual(haz.intensity.min(), -1.0, places=5)
 
 # Execute Tests