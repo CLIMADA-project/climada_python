7a777e13c93a1b450622bb4f753562369f88c5d1
schmide <emanuel.schmid@id.ethz.ch>
2020-11-02 14:05:53 +0100

    climada.util.config.Config refactoring renamed from Object index handling elaborated

--- climada/util/config.py
+++ climada/util/config.py
@@ -127,9 +127,9 @@ def setup_conf_user():
         check_conf()
 
 
-class Object(object):
+class Config(object):
     """Convenience Class.
-    An Object is a slow json object like dictonary who's values can be accessed by their names right away.
+    A Config object is a slow JSON object like dictonary who's values can be accessed by their names right away.
     E.g.: `a.b.c.str()` instead of `a['b']['c']`
     """
     def __str__(self):
@@ -140,93 +140,159 @@ class Object(object):
         except: pass
         try: return str(self.float())
         except: pass
+        try: return str(self.list())
+        except: pass
         return '{{{}}}'.format(", ".join([
-            f'{v}' if k == '_val' else f'{k}: {v}'
-            for (k, v) in self.__dict__.items()
+            f'{k}: {v}' for (k, v) in self.__dict__.items()
         ]))
 
     def __repr__(self):
         return self.__str__()
 
     def __init__(self, val=None):
+        """
+        Parameters
+        ----------
+        val : [float, int, str, list], optional
+            the value of the Config in case it's basic, by default None,
+            when a dictionary like object is created
+        """
         if val is not None:
             self._val = val
 
-    def str(self):
+    def str(self, index=None):
         """
         Returns
         -------
         str
-            the value of this Object if it is a string
+            the value of this Config if it is a string
 
         Raises
         ------
         Exception
             if it is not a string
         """
-        if self._val.__class__ is str:
-            return self._val
-        raise Exception(f"{self._val.__class__}, not string")
+        if index is None:
+            if self._val.__class__ is str:
+                return self._val
+            raise Exception(f"{self._val.__class__}, not str")
+        else:
+            if self._val.__class__ is list:
+                return self._val[index].str()
+            raise Exception(f"{self._val.__class__}, not list")
 
-    def int(self):
+    def int(self, index=None):
         """
         Returns
         -------
         int
-            the value of this Object if it is an integer
+            the value of this Config if it is an integer
 
         Raises
         ------
         Exception
             if it is not an integer
         """
-        if self._val.__class__ is int:
-            return self._val
-        raise Exception(f"{self._val.__class__}, not int")
+        if index is None:
+            if self._val.__class__ is int:
+                return self._val
+            raise Exception(f"{self._val.__class__}, not int")
+        else:
+            if self._val.__class__ is list:
+                return self._val[index].int()
+            raise Exception(f"{self._val.__class__}, not list")
 
-    def float(self):
+    def list(self, index=None):
+        """
+        Returns
+        -------
+        int
+            the value of this Config if it is a list
+
+        Raises
+        ------
+        Exception
+            if it is not an list
+        """
+        if index is None:
+            if self._val.__class__ is list:
+                return self._val
+            raise Exception(f"{self._val.__class__}, not list")
+        else:
+            if self._val.__class__ is list:
+                return self._val[index].list()
+            raise Exception(f"{self._val.__class__}, not list")
+
+    def float(self, index=None):
         """
         Returns
         -------
         float
-            the value of this Object if it is a float
+            the value of this Config if it is a float
 
         Raises
         ------
         Exception
             if it is not a float
         """
-        if self._val.__class__ is float:
-            return self._val
-        raise Exception(f"{self._val.__class__}, not float")
+        if index is None:
+            if self._val.__class__ is float:
+                return self._val
+            raise Exception(f"{self._val.__class__}, not float")
+        else:
+            if self._val.__class__ is list:
+                return self._val[index].float()
+            raise Exception(f"{self._val.__class__}, not list")
+
+    def get(self, *args):
+        """
+        Parameters
+        ----------
+        indices : list of int, optional
+            i for getting the i-th item in the list
+        Returns
+        -------
+        Config
+            the i-th Config object in the list
+        Raises
+        ------
+        Exception
+            if it is not a list
+        """
+        
+        if self._val.__class__ is list:
+            if len(list(args)) == 1:
+                return self._val[args[0]]
+            return self._val[args[0]].get(*list(args)[1:])
+        raise Exception(f"{self._val.__class__}, not list")
 
     @classmethod
     def _objectify_dict(cls, dct):
-        obj = Object()
+        obj = Config()
         for key, val in dct.items():
             if val.__class__ is dict:
                 obj.__setattr__(key, cls._objectify_dict(val))
             elif val.__class__ is list:
                 obj.__setattr__(key, cls._objectify_list(val))
             else:
-                obj.__setattr__(key, Object(val))
+                obj.__setattr__(key, Config(val))
         return obj
 
     @classmethod
     def _objectify_list(cls, lst):
-        lst = list()
+        objs = list()
         for item in lst:
             if item.__class__ is dict:
-                lst.append(cls._objectify_dict(item))
+                objs.append(cls._objectify_dict(item))
             elif item.__class__ is list:
-                lst.append(cls._objectify_list(item))
+                objs.append(cls._objectify_list(item))
             else:
-                lst.append(Object(item))
-        return lst
+                objs.append(Config(item))
+        return Config(objs)
 
     @classmethod
     def from_dict(cls, dct):
-        """Creates an Object from a json object like dictionary.
+        """Creates an Config from a json object like dictionary.
         Parameters
         ----------
         dct : dict
@@ -234,7 +300,7 @@ class Object(object):
             values can be one of these: int, float, str, dict, list.
         Returns
         -------
-        Object
+        Config
             contaning the same data as the input parameter `dct`
         """
         return cls._objectify_dict(dct)
