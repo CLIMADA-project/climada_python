0c2b2d55ec1496256ced5b8f9fcf426f406ff0a1
Thomas Vogt <thomas.vogt@pik-potsdam.de>
2020-11-26 15:12:51 +0100

    tc_surge_bathtub: implementation without elevation package

---
+++ climada/hazard/tc_surge_bathtub.py
@@ -0,0 +1,179 @@
+"""
+This file is part of CLIMADA.
+
+Copyright (C) 2017 ETH Zurich, CLIMADA contributors listed in AUTHORS.
+
+CLIMADA is free software: you can redistribute it and/or modify it under the
+terms of the GNU Lesser General Public License as published by the Free
+Software Foundation, version 3.
+
+CLIMADA is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along
+with CLIMADA. If not, see <https://www.gnu.org/licenses/>.
+
+---
+
+Define TCSurgeBathtub class.
+"""
+
+__all__ = ['TCSurgeBathtub']
+
+import copy
+import logging
+
+import numpy as np
+import rasterio.warp
+import scipy.sparse as sp
+
+from climada.hazard.base import Hazard
+import climada.util.coordinates as coord_util
+
+LOGGER = logging.getLogger(__name__)
+
+HAZ_TYPE = 'TCSurgeBathtub'
+"""Hazard type acronym for this module"""
+
+MAX_DIST_COAST = 50
+"""Maximum inland distance of the centroids in km"""
+
+MAX_ELEVATION = 10
+"""Maximum elevation of the centroids in m"""
+
+DECAY_RATE_M_KM = 0.2
+"""Decay rate of surge in meters per each km"""
+
+
+class TCSurgeBathtub(Hazard):
+    """TC surge heights in m, a bathtub model with wind-surge relationship and inland decay."""
+
+    def __init__(self):
+        Hazard.__init__(self, HAZ_TYPE)
+
+
+    @staticmethod
+    def from_tc_winds(wind_haz, topo_path, inland_decay=True, add_sea_level_rise=0.0):
+        """Compute tropical cyclone surge from input winds.
+
+        Parameters
+        ----------
+        wind_haz : TropCyclone
+            Tropical cyclone wind hazard object.
+        topo_path : str
+            Path to a raster file containing gridded elevation data.
+        inland_decay : bool, optional
+            Surge decays according to distance from coast. Default: True.
+        add_sea_level_rise : float, optional
+            Sea level rise effect in meters to be added to surge height.
+        """
+        centroids = copy.deepcopy(wind_haz.centroids)
+
+        if not centroids.coord.size:
+            centroids.set_meta_to_lat_lon()
+
+        # Select wind-affected centroids which are inside INLAND_MAX_DIST_KM and |lat| < 61
+        if not centroids.dist_coast.size or np.all(centroids.dist_coast >= 0):
+            centroids.set_dist_coast(signed=True, precomputed=True)
+        coastal_msk = (((wind_haz.intensity > 0).sum(axis=0).A1 > 0)
+                       & (centroids.dist_coast < 0)
+                       & (centroids.dist_coast > -MAX_DIST_COAST * 1000)
+                       & (np.abs(centroids.lat) < 61))
+
+        # elevation at coastal centroids, exclude high centroids
+        coastal_centroids_h = coord_util.read_raster_sample(
+            topo_path, centroids.lat[coastal_msk], centroids.lon[coastal_msk])
+        elevation_msk = ((coastal_centroids_h >= 0)
+                         & (coastal_centroids_h <= MAX_ELEVATION + add_sea_level_rise))
+        coastal_msk[coastal_msk] = elevation_msk
+        coastal_centroids_h = coastal_centroids_h[elevation_msk]
+        coastal_idx = coastal_msk.nonzero()[0]
+
+        # Initialize array at coastal centroids
+        inten_surge = wind_haz.intensity.copy()
+        inten_surge[:,(~coastal_msk).nonzero()[0]] = 0
+        inten_surge.eliminate_zeros()
+
+        # Conversion of wind to surge using the linear wind-surge relationship from
+        # figure 2 of the following paper:
+        #
+        #   Xu, Liming (2010): A Simple Coastline Storm Surge Model Based on Pre-run SLOSH Outputs.
+        #   In: 29th Conference on Hurricanes and Tropical Meteorology, 10–14 May. Tucson, Arizona.
+        #   https://ams.confex.com/ams/pdfpapers/168806.pdf
+        inten_surge.data = 0.1023 * np.fmax(inten_surge.data - 26.8224, 0) + 1.8288
+
+        if inland_decay:
+            # add decay according to distance from coast
+            dist_coast_km = np.abs(centroids.dist_coast[coastal_idx]) / 1000
+            coastal_centroids_h += DECAY_RATE_M_KM * dist_coast_km
+        coastal_centroids_h -= add_sea_level_rise
+        inten_surge[:,coastal_idx] -= coastal_centroids_h[None,:]
+
+        # discard negative surge height values
+        inten_surge.data = np.fmax(inten_surge.data, 0)
+        inten_surge.eliminate_zeros()
+
+        # get fraction of centroid cells on land according to given DEM
+        fract_surge = sp.csr_matrix(_fraction_on_land(centroids, topo_path))
+        fract_surge = sp.csr_matrix(np.ones((inten_surge.shape[0], 1))) * fract_surge
+
+        # set other attributes
+        haz = TCSurgeBathtub()
+        haz.centroids = centroids
+        haz.units = 'm'
+        haz.event_id = wind_haz.event_id
+        haz.event_name = wind_haz.event_name
+        haz.date = wind_haz.date
+        haz.orig = wind_haz.orig
+        haz.frequency = wind_haz.frequency
+        haz.intensity = inten_surge
+        haz.fraction = fract_surge
+        return haz
+
+
+def _fraction_on_land(centroids, topo_path):
+    """Determine fraction of each centroid cell that is on land.
+
+    Parameters
+    ----------
+    centroids : Centroids
+        Centroids to consider
+    topo_path : str
+        Path to a raster file containing gridded elevation data.
+
+    Returns
+    -------
+    fractions : ndarray of shape (ncentroids,)
+        For each centroid, the fraction of it's cell area that is on land according to the DEM.
+    """
+    bounds = np.array(centroids.total_bounds)
+    shape = [0, 0]
+    if centroids.meta:
+        shape = centroids.shape
+        cen_trans = centroids.meta['transform']
+    else:
+        shape[0], shape[1], cen_trans = coord_util.pts_to_raster_meta(
+            bounds, min(coord_util.get_resolution(centroids.lat, centroids.lon)))
+    bounds += np.array([-.05, -.05, .05, .05])
+
+    on_land, dem_trans = coord_util.read_raster_bounds(topo_path, bounds)
+    on_land = (on_land > 0).astype(np.float64)
+
+    with rasterio.open(topo_path, 'r') as src:
+        dem_crs = src.crs
+        dem_nodata = src.nodata
+
+    fractions = np.zeros(shape, dtype=np.float64)
+    rasterio.warp.reproject(source=on_land, destination=fractions,
+                            src_transform=dem_trans, src_crs=dem_crs,
+                            dst_transform=cen_trans, dst_crs=centroids.crs,
+                            resampling=rasterio.warp.Resampling.average,
+                            src_nodata=dem_nodata, dst_nodata=0.0)
+
+    if not centroids.meta:
+        x_i = ((centroids.lon - cen_trans[2]) / cen_trans[0]).astype(int)
+        y_i = ((centroids.lat - cen_trans[5]) / cen_trans[4]).astype(int)
+        fractions = fractions[y_i, x_i]
+
+    return fractions.reshape(-1)
