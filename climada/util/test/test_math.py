d755612653b208fde0a4c60ab1d1ca2d6059bede
Chahan Kropf <chahan.kropf@usys.ethz.ch>
2020-11-30 10:57:38 +0100

    Feature/sig dig (#91)
    * Add util.math with sig_dig and money_words functions
    
    * Add abbreviations variable to util.math.money_words
    
    * Amend util.math.money_unit to accept bellow 1000.
    
    * Add tests for math.py (sig_dig, sig_dig_list, money_unit)
    
    * Rename util.math.money_unit to value_to_monetary_unit
    
    * Rename math.py to value_representation.py
    
    * Update test.math.py 
    
    name change of money_unit -> value_to_monetary_unit
    
    * Update value_representation.py
    
    If single value given as input, change to list.
    
    * Update climada/util/value_representation.py
    
    PEP8 adjustement
    
    Co-authored-by: emanuel-schmid <51439563+emanuel-schmid@users.noreply.github.com>
    
    * Update climada/util/value_representation.py
    
    Make LOGGER lazy.
    
    Co-authored-by: emanuel-schmid <51439563+emanuel-schmid@users.noreply.github.com>
    
    Co-authored-by: emanuel-schmid <51439563+emanuel-schmid@users.noreply.github.com>

---
+++ climada/util/test/test_math.py
@@ -0,0 +1,95 @@
+"""
+This file is part of CLIMADA.
+
+Copyright (C) 2017 ETH Zurich, CLIMADA contributors listed in AUTHORS.
+
+CLIMADA is free software: you can redistribute it and/or modify it under the
+terms of the GNU Lesser General Public License as published by the Free
+Software Foundation, version 3.
+
+CLIMADA is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along
+with CLIMADA. If not, see <https://www.gnu.org/licenses/>.
+
+---
+
+Test of util.math module
+"""
+
+
+from climada.util.math import sig_dig, sig_dig_list, money_unit, ABBREV 
+import unittest
+import numpy as np
+
+
+class TestDigits(unittest.TestCase):
+    """Test date functions"""
+    
+    def test_sig_dig_pass(self):
+        """Test sig_dig function"""
+        n_sig_dig = 3
+        nbs_in = [1.2345, 12345, -12345, -12.345]
+        nbs_out = [1.23, 12300, -12300, -12.3]
+        for nb_in, nb_out in zip(nbs_in, nbs_out):
+            self.assertEqual(sig_dig(nb_in, n_sig_dig), nb_out)
+        self.assertTrue(
+            np.array_equal(sig_dig_list(nbs_in, n_sig_dig), nbs_out)
+            )
+        
+    def test_sig_dig_fail(self):
+        """Test sig_dig function"""
+        n_sig_dig_wrong = 4
+        nbs_in = [1.2345, 12345, -12345, -12.345]
+        nbs_out = [1.23, 12300, -12300, -12.3]
+        for nb_in, nb_out in zip(nbs_in, nbs_out):
+            self.assertNotEqual(sig_dig(nb_in, n_sig_dig_wrong), nb_out)
+        self.assertFalse(
+            np.array_equal(sig_dig_list(nbs_in, n_sig_dig_wrong), nbs_out)
+            )
+        
+    def test_value_to_monetary_unit_pass(self):
+        """Test money_unit function"""
+        nbs_in = [-1e10, -1e6, -1e2, 0, 1e3, 1e7, 1e11]
+        nbs_out = [-10, -1, -100, 0, 1, 10, 100]
+        names_out = [ABBREV[1e9], ABBREV[1e6], ABBREV[1], ABBREV[1],
+                 ABBREV[1e3], ABBREV[1e6], ABBREV[1e9]]
+        for j, (nb_in, nb_out) in enumerate(zip(nbs_in, nbs_out)):
+            money, names = value_to_monetary_unit(nb_in)
+            self.assertEqual(money[0], nb_out)
+            self.assertEqual(names, names_out[j])
+            
+    def test_value_to_monetary_unit_sigdig_pass(self):
+        """Test money_unit function with significant digits"""
+        nbs_in = [-1e10*1.2345, -1e6*1.2345, -1e2*1.2345, 0, 1e3*1.2345,
+                  1e7*1.2345, 1e11*1.2345]
+        nbs_out = [-12.3, -1.23, -123, 0, 1.23, 12.3, 123]
+        names_out = [ABBREV[1e9], ABBREV[1e6], ABBREV[1], ABBREV[1],
+                 ABBREV[1e3], ABBREV[1e6], ABBREV[1e9]]
+        for j, (nb_in, nb_out) in enumerate(zip(nbs_in, nbs_out)):
+            money, names = value_to_monetary_unit(nb_in, n_sig_dig=3)
+            self.assertEqual(money[0], nb_out)
+            self.assertEqual(names, names_out[j])
+            
+    def test_value_to_monetary_unit_list_pass(self):
+        """Test money_unit function with list of numbers"""
+        nbs_in = [-1e10*1.2345, -1e9*1.2345]
+        nbs_out = [-12.3, -1.23]
+        name_out = ABBREV[1e9]
+        money, name = value_to_monetary_unit(nbs_in, n_sig_dig=3)
+        self.assertTrue(np.array_equal(money, nbs_out))
+        self.assertEqual(name, name_out)
+        nbs_in = [1e4*1.2345, 1e3*1.2345, 1e2*1.2345]
+        nbs_out = [12.3, 1.23, 0.123]
+        name_out = ABBREV[1e3]
+        money, name = value_to_monetary_unit(nbs_in, n_sig_dig=3)
+        self.assertTrue(np.array_equal(money, nbs_out))
+        self.assertEqual(name, name_out)
+
+
+# Execute Tests
+if __name__ == "__main__":
+    TESTS = unittest.TestLoader().loadTestsFromTestCase(TestDigits)
+    unittest.TextTestRunner(verbosity=2).run(TESTS)
