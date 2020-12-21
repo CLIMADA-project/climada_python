680a2131be80e11167419a0cc497b31d4bedcb46
Chahan Kropf <chahan.kropf@gmail.com>
2020-12-21 22:43:57 +0100

    Implement sobol impact sampling

--- climada/engine/uncertainty.py
+++ climada/engine/uncertainty.py
@@ -7,7 +7,6 @@ Created on Fri Dec 11 17:17:00 2020
 """
 
 from SALib.sample import saltelli
-import scipy as sp
 import pandas as pd
 from climada.engine import Impact
 from climada.entity import ImpactFuncSet
@@ -17,68 +16,16 @@ from climada.hazard import Hazard
 
 class UncVar():
     
-    def __init__(self, labels, var, distr_dict):
-        self.labels = labels
-        self.distrs = {label: distr for label, distr in zip(labels, distr_dict)}
+    def __init__(self, unc_var, distr_dict):
+        self.labels = list(distr_dict.keys())
+        self.distr_dict = distr_dict
+        self.unc_var = unc_var
     
     def plot_distr(self):
         pass    
     
-    pass
-
-
-class UncVarCat(UncVar):
-    
-    def __init__(self, label, cat_var):
-        self.labels = [label]
-        self.unc_var = cat_var
-    
-    @property
-    def distrs(self):
-        [label] = self.labels
-        return {
-        label : sp.stats.randint(low=0, high=len(self.cat_var))
-            }
-    
-    def get_unc_var(self, idx):
-        return self.cat_var[idx]
-    pass
-   
- 
-class UncVarCont(UncVar):
-    
-    def __init__(self, labels, cont_var, bound_dict=None, distr_dict=None):
-        self.unc_var = cont_var
-        self.labels = list(distr_dict.keys())
-        
-        if bound_dict is not None:
-            if distr_dict() is None: 
-                self.distrs = {
-                    label: sp.stats.uniform(
-                        bound_dict[label][0],
-                        bound_dict[label][1]-bound_dict[label][0]
-                        )
-                    for label in labels
-                    }
-                
-            else:
-                raise ValueError("Specify either bounds or distr_dict, not both")
-        
-        if distr_dict() is not None:
-            if bound_dict is None:
-                self.distrs = distr_dict
-            else:
-                raise ValueError("Specify either bounds or distr_dict, not both")
-                
-            
-    def get_unc_var(self, *params):
-        return self.unc_var(params)
-    
-    pass
-    
-
-class UncVarCatCont(UncVar):
-    pass
+    def get_unc_var(self, kwargs):
+        return self.unc_var(**kwargs) 
 
 
 class UncSensitivity():
@@ -86,56 +33,64 @@ class UncSensitivity():
     def __init__(self, exp_unc, impf_unc, haz_unc):
         
         if isinstance(exp_unc, Exposures):
-            self.exp = UncVar(labels=[], unc_var=exp_unc, distr_dict={})
+            self.exp = UncVar(unc_var=lambda: exp_unc, distr_dict={})
         else:
             self.exp = exp_unc
             
         if isinstance(impf_unc, ImpactFuncSet):
-            self.impf = UncVar(labels=[], unc_var=impf_unc, distr_dict={})
+            self.impf = UncVar(unc_var=lambda: impf_unc, distr_dict={})
         else:
             self.impf = impf_unc
             
         if isinstance(haz_unc, Hazard):
-            self.haz = UncVar(labels=[], unc_var=haz_unc, distr_dict={})
+            self.haz = UncVar(unc_var=lambda: haz_unc, distr_dict={})
         else:
             self.haz = haz_unc
+            
         
-    def calc(self, N, save_impacts = False):
+    def calc_impact_distribution_sobol(self, N, save_impacts = False):
+        
         self.n_samples = N
         df_params = self._make_sobol_sample()
         
+        impacts = []
         for j, row in df_params.iterrows():
-            imp = Impact
-            exp_params = row[self.exp.labels].to_list()
-            haz_params = row[self.haz.labels].to_list()
-            impf_params = row[self.impf.labels].to_list()
             
-            exp = self.exp.get_unc_var(*exp_params)
-            haz = self.haz.get_unc_var(*haz_params)
-            impf = self.impf.get_unc_var(*impf_params)
+            exp_params = row[self.exp.labels].to_dict()
+            haz_params = row[self.haz.labels].to_dict()
+            impf_params = row[self.impf.labels].to_dict()
             
-            impact = imp.calc(exp, impf, haz)
+            exp = self.exp.get_unc_var(exp_params)
+            haz = self.haz.get_unc_var(haz_params)
+            impf = self.impf.get_unc_var(impf_params)
             
-            print(impact.aai_agg)
+            imp = Impact()
+            imp.calc(exposures=exp, impact_funcs=impf, hazard=haz)
             
+            impacts.append(imp)
+            
+        return df_params, impacts
+        
+        
 
     @property
-    def labels(self):
+    def params(self):
         return self.exp.labels + self.haz.labels + self.impf.labels
     
     @property
-    def distrs(self):
-        distrs = dict(self.exp.distrs)
-        distrs.update(self.haz.distrs)
-        distrs.update(self.impf.distrs)
-        return distrs
+    def distr_dict(self):
+        distr_dict = dict(self.exp.distr_dict)
+        distr_dict.update(self.haz.distr_dict)
+        distr_dict.update(self.impf.distr_dict)
+        return distr_dict
+
     
     def _make_sobol_sample(self):
         sobol_uniform_sample = self._make_uniform_sobol_sample()
-        df_params = pd.DataFrame(sobol_uniform_sample, columns=self.labels)
+        df_params = pd.DataFrame(sobol_uniform_sample, columns=self.params)
         for param in list(df_params):
             df_params[param] = df_params[param].apply(
-                self.distrs[param].ppf
+                self.distr_dict[param].ppf
                 )
         return df_params
     
@@ -146,8 +101,9 @@ class UncSensitivity():
             'names' : self.params,
             'bounds' : [[0, 1]]*len(self.params)
             }
-        params = saltelli.sample(problem, self.n_samples)
-        return params
+        self.problem = problem
+        sobol_params = saltelli.sample(problem, self.n_samples)
+        return sobol_params
     
     
     
@@ -156,6 +112,61 @@ class UncRobustness():
     Compute variance from multiplicative Gaussian noise
     """
     pass
+
+
+# class UncVarCat(UncVar):
+    
+#     def __init__(self, label, cat_var):
+#         self.labels = [label]
+#         self.unc_var = cat_var
+    
+#     @property
+#     def distrs(self):
+#         [label] = self.labels
+#         return {
+#         label : sp.stats.randint(low=0, high=len(self.cat_var))
+#             }
+    
+#     def get_unc_var(self, idx):
+#         return self.cat_var[idx]
+#     pass
+   
+ 
+# class UncVarCont(UncVar):
+    
+#     def __init__(self, labels, cont_var, bound_dict=None, distr_dict=None):
+#         self.unc_var = cont_var
+#         self.labels = list(distr_dict.keys())
+        
+#         if bound_dict is not None:
+#             if distr_dict() is None: 
+#                 self.distrs = {
+#                     label: sp.stats.uniform(
+#                         bound_dict[label][0],
+#                         bound_dict[label][1]-bound_dict[label][0]
+#                         )
+#                     for label in labels
+#                     }
+                
+#             else:
+#                 raise ValueError("Specify either bounds or distr_dict, not both")
+        
+#         if distr_dict() is not None:
+#             if bound_dict is None:
+#                 self.distrs = distr_dict
+#             else:
+#                 raise ValueError("Specify either bounds or distr_dict, not both")
+                
+            
+#     def get_unc_var(self, *params):
+#         return self.unc_var(params)
+    
+#     pass
+    
+
+# class UncVarCatCont(UncVar):
+#     pass
+
     
     
 # impacts = []
