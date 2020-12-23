867dccd946167f15b90ecae1c821e3573925cdd6
Chahan Kropf <chahan.kropf@gmail.com>
2020-12-23 14:18:30 +0100

    Add CLIMADA header with License notice

--- climada/engine/uncertainty.py
+++ climada/engine/uncertainty.py
@@ -1,7 +1,22 @@
 """
-Created on Fri Dec 11 17:17:00 2020
+This file is part of CLIMADA.
 
-@author: ckropf
+Copyright (C) 2017 ETH Zurich, CLIMADA contributors listed in AUTHORS.
+
+CLIMADA is free software: you can redistribute it and/or modify it under the
+terms of the GNU Lesser General Public License as published by the Free
+Software Foundation, version 3.
+
+CLIMADA is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along
+with CLIMADA. If not, see <https://www.gnu.org/licenses/>.
+
+---
+
+Define Uncertainty class.
 """
 
 from SALib.sample import saltelli
