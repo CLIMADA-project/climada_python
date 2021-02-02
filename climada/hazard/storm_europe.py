45fb3c97bd18cfb58a2517fbe34876c6edacb133
ThomasRoosli <ThomasRoosli@users.noreply.github.com>
2021-02-02 15:49:32 +0100

    move icon downloader to util
    create new util module that hosts all functions to download icon model from opendata.dwd.de

--- climada/hazard/storm_europe.py
+++ climada/hazard/storm_europe.py
@@ -22,28 +22,28 @@ Define StormEurope class.
 __all__ = ['StormEurope']
 
 import os
-import shutil
 import bz2
 import logging
-import requests
 import numpy as np
 import xarray as xr
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import sparse
-from datetime import datetime
 
 
 from climada.hazard.base import Hazard
 from climada.hazard.centroids.centr import Centroids
 from climada.hazard.tag import Tag as TagHazard
-from climada.util.files_handler import get_file_names, download_file
-from climada.util.dates_times import (
-    datetime64_to_ordinal,
-    last_year,
-    first_year,
-    date_to_str
-)
+from climada.util.files_handler import get_file_names
+from climada.util.dates_times import (datetime64_to_ordinal,
+                                      last_year,
+                                      first_year,
+                                      date_to_str
+                                      )
+from climada.util.dwd_icon_loader import (download_icon_centroids_file,
+                                          download_icon_grib,
+                                          delete_icon_grib,
+                                          )
 from climada.util import CONFIG
 
 LOGGER = logging.getLogger(__name__)
@@ -301,10 +301,10 @@ class StormEurope(Hazard):
         """
         self.clear()
         # download files, if they don't already exist
-        file_names = _download_icon_grib(run_date,model_name=model_name)
+        file_names = download_icon_grib(run_date,model_name=model_name)
        
         # create centroids
-        nc_centroids_file = self._get_icon_centroids_file(model_name)
+        nc_centroids_file = download_icon_centroids_file(model_name)
         self.centroids = self._centroids_from_nc(nc_centroids_file)
         
         # read intensity from files
@@ -386,49 +386,7 @@ class StormEurope(Hazard):
 
         if delete_raw_data:
             #delete downloaded .bz2 files
-            _delete_icon_grib(run_date, model_name=model_name)
-
-
-    @staticmethod
-    def _get_icon_centroids_file(model_name='icon-eu-eps'):
-        """ create centroids based on netcdf files provided by dwd, links 
-        found here: 
-        https://www.dwd.de/DE/leistungen/opendata/neuigkeiten/opendata_dez2018_02.html
-        https://www.dwd.de/DE/leistungen/opendata/neuigkeiten/opendata_aug2020_01.html
-        """
-        
-        # define url and filename
-        url = 'https://opendata.dwd.de/weather/lib/cdo/'
-        if model_name=='icon-eu-eps':
-            file_name = 'icon_grid_0028_R02B07_N02.nc.bz2'
-        elif model_name=='icon-eu':
-            file_name = 'icon_grid_0024_R02B06_G.nc.bz2'
-        elif model_name=='icon-d2-eps' or model_name=='icon-d2':
-            file_name = 'icon_grid_0047_R19B07_L.nc.bz2'
-        else:
-            LOGGER.error(('Creation of centroids for the icon model ' +
-                          model_name + 'is not implemented. Please define ' +
-                          'the default values in the code first.'))
-            raise ValueError
-        bz2_pathfile = os.path.join(CONFIG['local_data']['save_dir'],
-                                     file_name)
-        nc_pathfile = bz2_pathfile[:-4]
-        
-        # download and unzip file
-        if not os.path.exists(nc_pathfile):
-            if not os.path.exists(bz2_pathfile):
-                try:
-                    download_file(url + file_name)
-                    shutil.move(file_name, CONFIG['local_data']['save_dir'])
-                except ValueError as err:
-                    LOGGER.error('Error while downloading %s.', url + file_name)
-                    raise err
-            with open(bz2_pathfile, 'rb') as source, open(nc_pathfile, 'wb') as dest:
-                dest.write(bz2.decompress(source.read()))
-            os.remove(bz2_pathfile)
-        
-        return nc_pathfile
-
+            delete_icon_grib(run_date, model_name=model_name)
 
     @staticmethod
     def _centroids_from_nc(file_name):
@@ -826,157 +784,3 @@ class StormEurope(Hazard):
         return intensity_out[:, sel_cen], ssi
 
 
-
-
-def _download_icon_grib(run_date,model_name='icon-eu-eps',
-                        parameter_name='vmax_10m',
-                        max_lead_time=None):
-    """download the gribfiles of a weather forecast run for a certain 
-    weather parameter from opendata.dwd.de/weather/nwp/. 
-    
-    Parameters:
-        run_date (datetime): The starting timepoint of the forecast run 
-        model_name (str): the name of the forecast model written as it appears
-            in the folder structure in opendata.dwd.de/weather/nwp/
-        parameter_name (str): the name of the meteorological parameter 
-            written as it appears in the folder structure in 
-            opendata.dwd.de/weather/nwp/
-        max_lead_time (int): number of hours for which files should be 
-            downloaded, will default to maximum available data
-    
-    Returns:
-        file_names (list): a list of filenames that link to all just 
-            downloaded or available files from the forecast run, defined by
-            the input parameters
-    """
-    
-    LOGGER.info(('Downloading icon grib files of model ' +
-                 model_name + ' for parameter ' + parameter_name +
-                 ' with starting date ' + run_date.strftime('%Y%m%d%H') +
-                 '.'))
-    
-    url, file_name, lead_times = _create_icon_grib_name(run_date,
-                                                        model_name,
-                                                        parameter_name,
-                                                        max_lead_time)
-        
-    #download all files
-    file_names = []
-    for lead_i in lead_times:
-        file_name_i = file_name.format(lead_i=lead_i)
-        full_path_name_i = os.path.join(CONFIG['local_data']['save_dir'],
-                                        file_name_i)
-        file_names.append(full_path_name_i)
-        if os.path.exists(full_path_name_i):
-            continue
-        try:
-            download_file(url + file_name_i)
-            shutil.move(file_name_i, CONFIG['local_data']['save_dir'])
-        except ValueError as err:
-            LOGGER.error('Error while downloading %s.', file_name_i)
-            request = requests.get(url)
-            if request.status_code == 200:
-                raise FileNotFoundError(('Error while downloading %s.', file_name_i)) from err
-            else:
-                raise err
-            
-        
-    return file_names
-        
-    
- 
-def _delete_icon_grib(run_date,model_name='icon-eu-eps',
-                        parameter_name='vmax_10m',
-                        max_lead_time=None):
-    """delete the downloaded gribfiles of a weather forecast run for a 
-    certain weather parameter from opendata.dwd.de/weather/nwp/. 
-    
-    Parameters:
-        run_date (datetime): The starting timepoint of the forecast run 
-        model_name (str): the name of the forecast model written as it appears
-            in the folder structure in opendata.dwd.de/weather/nwp/
-        parameter_name (str): the name of the meteorological parameter 
-            written as it appears in the folder structure in 
-            opendata.dwd.de/weather/nwp/
-        max_lead_time (int): number of hours for which files should be 
-            deleted, will default to maximum available data
-    """    
-    
-    url, file_name, lead_times = _create_icon_grib_name(run_date,
-                                                        model_name,
-                                                        parameter_name,
-                                                        max_lead_time)
-    #delete all files
-    for lead_i in lead_times:
-        file_name_i = file_name.format(lead_i=lead_i)
-        full_path_name_i = os.path.join(CONFIG['local_data']['save_dir'],
-                                        file_name_i)
-        if os.path.exists(full_path_name_i):
-          os.remove(full_path_name_i)
-        else:
-          LOGGER.warning('File %s does not exist and could not be deleted.',
-                         full_path_name_i)
-
-
-def _create_icon_grib_name(run_date,model_name='icon-eu-eps',
-                        parameter_name='vmax_10m',
-                        max_lead_time=None):
-    """create all parameters to download or delete gribfiles of a weather 
-    forecast run for a certain weather parameter from 
-    opendata.dwd.de/weather/nwp/. 
-    
-    Parameters:
-        run_date (datetime): The starting timepoint of the forecast run 
-        model_name (str): the name of the forecast model written as it appears
-            in the folder structure in opendata.dwd.de/weather/nwp/
-        parameter_name (str): the name of the meteorological parameter 
-            written as it appears in the folder structure in 
-            opendata.dwd.de/weather/nwp/
-        max_lead_time (int): number of hours for which files should be 
-            selected, will default to maximum available data
-    """    
-    # define defaults of the url for each model and parameter combination
-    if (model_name=='icon-eu-eps') & (parameter_name=='vmax_10m'):
-        file_extension = '_europe_icosahedral_single-level_'
-        max_lead_time_default = 120 # maximum available data
-        lead_times = np.concatenate((np.arange(1,49),
-                                     np.arange(51,73,3),
-                                     np.arange(78,121,6)
-                                     ))
-    else:
-        LOGGER.error(('Download for model ' + model_name + 
-                      ' and parameter ' + parameter_name + 
-                      ' is not yet implemented. Please define ' +
-                      'the default values in the code first.'))
-        raise ValueError
-        
-    # create the url for download
-    url = ('https://opendata.dwd.de/weather/nwp/' +
-           model_name +
-           '/grib/' +
-           run_date.strftime('%H') + 
-           '/' + 
-           parameter_name +
-           '/')
-    file_name = (model_name +
-                 file_extension +
-                 run_date.strftime('%Y%m%d%H') +
-                 '_' +
-                 '{lead_i:03}' +
-                 '_' +
-                 parameter_name +
-                 '.grib2.bz2')
-    
-    
-    # define the leadtimes
-    if  not max_lead_time:
-        max_lead_time = max_lead_time_default
-    elif max_lead_time > max_lead_time_default:
-        LOGGER.warning(('Parameter max_lead_time ' +
-                        str(max_lead_time) + ' is bigger than maximum ' +
-                        'available files. max_lead_time is adjusted to ' +
-                        str(max_lead_time_default)))
-        max_lead_time = max_lead_time_default
-    lead_times = lead_times[lead_times<=max_lead_time]
-    
-    return url, file_name, lead_times
