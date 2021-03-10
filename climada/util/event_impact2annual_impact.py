24a17d1f5eecf0a038d7ded819f0d6d38a031978
Carmen Steinmann <carmenst@student.ethz.ch>
2021-03-10 10:41:57 +0100

    Restructure and clean-up code in event_impact2annual_impact

--- climada/util/event_impact2annual_impact.py
+++ climada/util/event_impact2annual_impact.py
@@ -7,17 +7,17 @@ Created on Thu Dec  3 15:59:03 2020
 """
 
 import copy
+import logging
 import numpy as np
 from numpy.random import default_rng
-import logging
 
 from climada.util.dates_times import str_to_date
 
 LOGGER = logging.getLogger(__name__)
 
-def eis2ais(eis, hazard= None, number_of_years=None, amount_events=None, 
+def eis2ais(eis, number_of_years=None, amount_events=None,
             distribution=None, sampling_vect=None):
-    
+
     """PURPOSE:
       convert an event (per occurrence) impact set (eis) into an annual impact
       set (ais), making reference to hazard.orig_yearset (if exists). If
@@ -81,128 +81,133 @@ def eis2ais(eis, hazard= None, number_of_years=None, amount_events=None,
       sampling_vect: the sampling vector, techincal, see code (can be used to
                      re-create the exact same yearset)
       """
-    
+
     # OLD CODE - STILL NEEDED?
-    #nr_years als input 
+    #nr_years als input
     # if years are known:
     # events = list(EIS.calc_impact_year_set().keys())
-    
+
     # event_date = date_to_str(eis.date)
     # event_year = [i.split('-', 1)[0] for i in event_date]
-    
+
     #n_years_his = np.ceil(1/min(eis.frequency)).astype('int')
     # n_events = len(eis.event_id)
-    # sollte ich nur die frequency der non-zero events zusammenrechnen?
-    
-    # INITIALISATION
-    # artificially generate a generic yearset (by just sampling the event impact set)
-    # annual impact set, event impact set
-    # essentially a copy, reset some fields:
-    ais = copy.deepcopy(eis) 
-    ais.event_id = [] # to indicate not by event any more
-    ais.frequency = [] # init, see below
-    ais.at_event = np.zeros([1,number_of_years+1])
-    ais.event_id = np.arange(1, number_of_years+1) 
-    #ais['ais'] = True #add a flag to distinguish ais from other impact class objects
 
 
     #NUMBER OF EVENTS
     nonzero_pos = np.where(eis.at_event >= (10*np.finfo(float).eps))
+    nonzero_impact = eis.at_event[nonzero_pos]
+    sorted_impact = np.sort(nonzero_impact)
     n_annual_events = np.sum(eis.frequency[nonzero_pos])
-    year_list = [str(date) + '-01-01' for date in np.arange(1,number_of_years+1).tolist()]
+    year_list = [str(date) + '-01-01' for date in np.arange(1, number_of_years+1).tolist()]
+
 
     if len(nonzero_pos) == 0:
         LOGGER.warning("No impact causing events.")
-    
+
     if not np.all(eis.frequency == eis.frequency[0]):
-        LOGGER.warning("The frequencies of the single events differ among each other. Please beware that this might influence the results.")
-     
-    #test: all frequencies equal
-    
-    nonzero_impact = eis.at_event[nonzero_pos]
-    # randomly populate years
-    sorted_impact = np.sort(nonzero_impact)
-    impact_per_year = np.zeros(number_of_years) 
-    
+        LOGGER.warning("The frequencies of the single events differ among each other."
+                       "Please beware that this might influence the results.")
+
+
+    # INITIALISATION
+    #to do: add a flag to distinguish ais from other impact class objects
+    # artificially generate a generic  annual impact set (by sampling the event impact set)
+    # essentially a copy, reset some fields:
+    ais = copy.deepcopy(eis)
+    ais.event_id = [] # to indicate not by event any more
+    ais.event_id = np.arange(1, number_of_years+1)
+    ais.frequency = [] # init, see below
+    ais.at_event = np.zeros([1, number_of_years+1])
+    ais.date = []
+
+
+    impact_per_year = np.zeros(number_of_years)
+
     # sample from the sorted impact
 
     if distribution is None: #for hazards such as RC where there's an event every year
         if not sampling_vect:
             sampling_vector = sampling_uniform(number_of_years, nonzero_pos)
-            
+
         amount_events = np.ones(number_of_years)
-        
+
         for idx_event, event in enumerate(sampling_vector):
             impact_per_year[idx_event] = sorted_impact[sampling_vector[event]]
-        
+
         event_names = year_list
     elif distribution == 'Poisson':
-        if not sampling_vect:    
-            sampling_vector, amount_events = sampling_poisson(number_of_years, n_annual_events, nonzero_pos)
-        
-             
+        if not sampling_vect:
+            sampling_vector, amount_events = sampling_poisson(number_of_years,
+                                                              n_annual_events, nonzero_pos)
+
+
         impact_per_event = np.zeros(np.sum(amount_events))
-        
+
         for idx_event, event in enumerate(sampling_vector):
             impact_per_event[idx_event] = sorted_impact[sampling_vector[event]]
             #event_names.append(year_list[year]*amount_events[year])
-        
+
         idx = 0
         event_names = []
-        for year in range(number_of_years):   
-            
-            
-            impact_per_year[year] =  np.sum(impact_per_event[idx:(idx+amount_events[year])])
+        for year in range(number_of_years):
+            impact_per_year[year] = np.sum(impact_per_event[idx:(idx+amount_events[year])])
             idx += amount_events[year]
-    
-    
-    #das hier stimmt irgendwie noch nicht
-    #adjust for sampling error (i.e. rand will not pick each impact once...)
-    ais_ed = np.sum(impact_per_year)/number_of_years #hier ist der Fehler
+
+
+    #adjust for sampling error
+    ais_ed = np.sum(impact_per_year)/number_of_years
     eis_ed = np.sum(eis.frequency*eis.at_event)
     correction_factor = eis_ed/ais_ed
-    #das printen funktioiert noch nicht
-    LOGGER.info("The correction factor amounts to"+str((correction_factor-1)*100)+"%")
+    LOGGER.info("The correction factor amounts to %s", (correction_factor-1)*100)
     # if correction_factor > 0.1:
     #     tex = raw_input("Do you want to exclude small events?")
-    #Korrekturfaktor >10% logger warning (consider neglecting small events)
-    #Schäden vergrössern unproblematisch, bei grossen add the posibility (for the user) to delete small events instead of rescaling all events
-    
-    
+
+
     ais.date = str_to_date(event_names)
     ais.at_event = impact_per_year / correction_factor
     ais.frequency = np.ones(number_of_years)*sum(amount_events)/number_of_years
-    
+
 
     return ais, sampling_vector, amount_events
 
-def sampling_uniform(number_events, nonzero_pos):    
+def sampling_uniform(number_events, nonzero_pos):
+    """Sample uniformely from an array (nonzero_pos) for a given amount of
+    events (number events)
+      """
+
     repetitions = np.ceil(number_events/(len(nonzero_pos)-1)).astype('int')
-    
+
     rng = default_rng()
     if repetitions >= 2:
-        sampling_vector = np.round(rng.choice((len(nonzero_pos)-1)*repetitions, size=number_events, replace=False)/repetitions)
+        sampling_vector = np.round(rng.choice((len(nonzero_pos)-1)*repetitions,
+                                              size=number_events, replace=False)/repetitions)
     else:
         sampling_vector = rng.choice((len(nonzero_pos)-1), size=number_events, replace=False)
-    
+
     return sampling_vector
 
 def sampling_poisson(number_of_years, n_annual_events, nonzero_pos):
-    amount_events_per_year = np.round(np.random.poisson(lam=n_annual_events , size=number_of_years)).astype('int')
-    
+    """Sample amount of events per year following a Poisson distribution
+      """
+
+    amount_events_per_year = np.round(np.random.poisson(lam=n_annual_events,
+                                                        size=number_of_years)).astype('int')
+
     #non_zero_years =  np.where(amount_events_per_year != 0)
     number_events = sum(amount_events_per_year)
-    
+
     sampling_vector = sampling_uniform(number_events, nonzero_pos)
-    
+
     return sampling_vector, amount_events_per_year
 
 def wrapper_multi_impact(list_impacts, number_of_years):
-    
+    """Compute the total impact of several event impact sets in one annual impact set
+      """
+
     ais_total = np.zeros(number_of_years)
     for impact in list_impacts:
         ais_impact = eis2ais(impact, number_of_years)
-        ais_total =+ ais_impact
-        
+        ais_total += ais_impact
+
     return ais_total
-        
