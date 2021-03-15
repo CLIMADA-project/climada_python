Apply landfall decay to all landfall and fix to the calculation of distance since landfall

climada/hazard/tc_tracks.py
@@ -1410,20 +1410,23 @@ def _dist_since_lf(track):
     dist_since_lf = np.zeros(track.time.values.shape)
 
     # Index in sea that follows a land index
-    sea_land_idx = np.where(np.diff(track.on_land.astype(int)) == 1)[0]
+    sea_land_idx, land_sea_idx = _get_landfall_idx(track, True)
     if not sea_land_idx.size:
         return (dist_since_lf + 1) * np.nan
-
-    # Index in sea that comes from previous land index
-    land_sea_idx = np.where(np.diff(track.on_land.astype(int)) == -1)[0] + 1
-    if track.on_land[-1]:
-        land_sea_idx = np.append(land_sea_idx, track.time.size)
+    
     orig_lf = np.empty((sea_land_idx.size, 2))
     for i_lf, lf_point in enumerate(sea_land_idx):
-        orig_lf[i_lf][0] = track.lat[lf_point] + \
-            (track.lat[lf_point + 1] - track.lat[lf_point]) / 2
-        orig_lf[i_lf][1] = track.lon[lf_point] + \
-            (track.lon[lf_point + 1] - track.lon[lf_point]) / 2
+        if lf_point > 0:
+            # Assume the landfall started between this and the previous point
+            orig_lf[i_lf][0] = track.lat[lf_point - 1] + \
+                (track.lat[lf_point] - track.lat[lf_point - 1]) / 2
+            orig_lf[i_lf][1] = track.lon[lf_point - 1] + \
+                (track.lon[lf_point] - track.lon[lf_point - 1]) / 2
+        else:
+            # track starts over land, assume first 'landfall' starts here
+            orig_lf[i_lf][0] = track.lat[lf_point]
+            orig_lf[i_lf][1] = track.lon[lf_point]
+
 
     dist = DistanceMetric.get_metric('haversine')
     nodes1 = np.radians(np.array([track.lat.values[1:],
@@ -1432,18 +1435,56 @@ def _dist_since_lf(track):
                                   track.lon.values[:-1]]).transpose())
     dist_since_lf[1:] = dist.pairwise(nodes1, nodes0).diagonal()
     dist_since_lf[~track.on_land.values] = 0.0
-    nodes1 = np.array([track.lat.values[sea_land_idx + 1],
-                       track.lon.values[sea_land_idx + 1]]).transpose() / 180 * np.pi
-    dist_since_lf[sea_land_idx + 1] = \
+    nodes1 = np.array([track.lat.values[sea_land_idx],
+                       track.lon.values[sea_land_idx]]).transpose() / 180 * np.pi
+    dist_since_lf[sea_land_idx] = \
         dist.pairwise(nodes1, orig_lf / 180 * np.pi).diagonal()
     for sea_land, land_sea in zip(sea_land_idx, land_sea_idx):
-        dist_since_lf[sea_land + 1:land_sea] = \
-            np.cumsum(dist_since_lf[sea_land + 1:land_sea])
+        dist_since_lf[sea_land:land_sea] = \
+            np.cumsum(dist_since_lf[sea_land:land_sea])
 
     dist_since_lf *= EARTH_RADIUS_KM
     dist_since_lf[~track.on_land.values] = np.nan
 
     return dist_since_lf
+
+def _get_landfall_idx(track, include_starting_landfall=False):
+    """Get the position of the start and end of landfalls for a TC track.
+
+    Parameters
+    ----------
+    track : xr.Dataset
+        track (variable 'on_land' must exist, if not present they can be added with
+               climada.hazard.tc_tracks.track_land_params(track, land_geom))
+    include_starting_landfall : bool
+        If the track starts over land, whether to include the track segment before
+        reaching the ocean as a landfall. Default: False.
+        
+
+    Returns
+    -------
+    sea_land_idx : numpy.ndarray of dtype int
+        Indexes of the first point over land for each landfall
+    land_sea_idx : numpy.ndarrat of dtype int
+        Indexes of first point over the ocean after each landfall. If the track
+        ends over land, the last value is set to track.time.size.
+    """
+    # Index in land that comes from previous sea index
+    sea_land_idx = np.where(np.diff(track.on_land.astype(int)) == 1)[0] + 1
+    # Index in sea that comes from previous land index
+    land_sea_idx = np.where(np.diff(track.on_land.astype(int)) == -1)[0] + 1
+    if track.on_land[-1]:
+        # track ends over land: add last track point as the end of that landfall
+        land_sea_idx = np.append(land_sea_idx, track.time.size)
+    if track.on_land[0]:
+        # track starts over land: remove first land-to-sea transition (not a landfall)?
+        if include_starting_landfall:
+            sea_land_idx = np.append(1, sea_land_idx)
+        else:
+            land_sea_idx = land_sea_idx[1:]
+    if land_sea_idx.size != sea_land_idx.size:
+        raise ValueError('Mismatch')
+    return sea_land_idx,land_sea_idx
 
 def _estimate_pressure(cen_pres, lat, lon, v_max):
     """Replace missing pressure values with statistical estimate.

climada/hazard/tc_tracks_synth.py
@@ -630,6 +630,9 @@ def _apply_land_decay(tracks, v_rel, p_rel, land_geom, s_rel=True,
         tracks = [_apply_decay_coeffs(track, v_rel, p_rel, land_geom, s_rel)
                   for track in tracks]
 
+    for track in tracks:
+        if track.orig_event_flag:
+            climada.hazard.tc_tracks.track_land_params(track, land_geom)
     if check_plot:
         _check_apply_decay_plot(tracks, orig_wind, orig_pres)
     return tracks
@@ -664,13 +667,8 @@ def _decay_values(track, land_geom, s_rel):
     x_val = dict()
 
     climada.hazard.tc_tracks.track_land_params(track, land_geom)
-    # Index in land that comes from previous sea index
-    sea_land_idx = np.where(np.diff(track.on_land.astype(int)) == 1)[0] + 1
-    # Index in sea that comes from previous land index
-    land_sea_idx = np.where(np.diff(track.on_land.astype(int)) == -1)[0] + 1
-    if track.on_land[-1]:
-        land_sea_idx = np.append(land_sea_idx, track.time.size)
-    if sea_land_idx.size and land_sea_idx.size <= sea_land_idx.size:
+    sea_land_idx, land_sea_idx = climada.hazard.tc_tracks._get_landfall_idx(track)
+    if sea_land_idx.size:
         for sea_land, land_sea in zip(sea_land_idx, land_sea_idx):
             v_landfall = track.max_sustained_wind[sea_land - 1].values
             ss_scale = climada.hazard.tc_tracks.set_category(v_landfall,
@@ -835,13 +833,8 @@ def _apply_decay_coeffs(track, v_rel, p_rel, land_geom, s_rel):
         return track
 
     climada.hazard.tc_tracks.track_land_params(track, land_geom)
-    # Index in land that comes from previous sea index
-    sea_land_idx = np.where(np.diff(track.on_land.astype(int)) == 1)[0] + 1
-    # Index in sea that comes from previous land index
-    land_sea_idx = np.where(np.diff(track.on_land.astype(int)) == -1)[0] + 1
-    if track.on_land[-1]:
-        land_sea_idx = np.append(land_sea_idx, track.time.size)
-    if not sea_land_idx.size or land_sea_idx.size > sea_land_idx.size:
+    sea_land_idx, land_sea_idx = climada.hazard.tc_tracks._get_landfall_idx(track)
+    if not sea_land_idx.size:
         return track
     for idx, (sea_land, land_sea) \
             in enumerate(zip(sea_land_idx, land_sea_idx)):
@@ -1011,13 +1004,8 @@ def _check_apply_decay_syn_plot(sy_tracks, syn_orig_wind,
 
     for track, orig_wind, orig_pres in \
             zip(sy_tracks, syn_orig_wind, syn_orig_pres):
-        # Index in land that comes from previous sea index
-        sea_land_idx = np.where(np.diff(track.on_land.astype(int)) == 1)[0] + 1
-        # Index in sea that comes from previous land index
-        land_sea_idx = np.where(np.diff(track.on_land.astype(int)) == -1)[0] + 1
-        if track.on_land[-1]:
-            land_sea_idx = np.append(land_sea_idx, track.time.size)
-        if sea_land_idx.size and land_sea_idx.size <= sea_land_idx.size:
+        sea_land_idx, land_sea_idx = climada.hazard.tc_tracks._get_landfall_idx(track)
+        if sea_land_idx.size:
             for sea_land, land_sea in zip(sea_land_idx, land_sea_idx):
                 v_lf = track.max_sustained_wind[sea_land - 1].values
                 p_lf = track.central_pressure[sea_land - 1].values
@@ -1077,13 +1065,8 @@ def _check_apply_decay_hist_plot(hist_tracks):
     graph_hped_a.set_ylabel('Environmental pressure - Central pressure (mb)')
 
     for track in hist_tracks:
-        # Index in land that comes from previous sea index
-        sea_land_idx = np.where(np.diff(track.on_land.astype(int)) == 1)[0] + 1
-        # Index in sea that comes from previous land index
-        land_sea_idx = np.where(np.diff(track.on_land.astype(int)) == -1)[0] + 1
-        if track.on_land[-1]:
-            land_sea_idx = np.append(land_sea_idx, track.time.size)
-        if sea_land_idx.size and land_sea_idx.size <= sea_land_idx.size:
+        sea_land_idx, land_sea_idx = climada.hazard.tc_tracks._get_landfall_idx(track)
+        if sea_land_idx.size:
             for sea_land, land_sea in zip(sea_land_idx, land_sea_idx):
                 scale_thresholds = climada.hazard.tc_tracks.SAFFIR_SIM_CAT
                 ss_scale_idx = np.where(track.max_sustained_wind[sea_land - 1].values