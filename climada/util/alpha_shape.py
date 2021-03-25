59918d0c62b6360e1c4fe30bc32daa93c15c9e40
samluethi <45563820+samluethi@users.noreply.github.com>
2021-03-25 16:08:38 +0100

    adjusted alpha shape
    Adjusted function in alpha_shape to not depend on the decarts library. Results are the same

--- climada/util/alpha_shape.py
+++ climada/util/alpha_shape.py
@@ -6,14 +6,13 @@ from shapely.ops import cascaded_union, polygonize
 from scipy.spatial import Delaunay
 import math
 import shapely.geometry as geometry
-from descartes import PolygonPatch
 import pylab as pl
 
 def alpha_shape(points, alpha):
     """
     Compute the alpha shape (concave hull) of a set
     of points.
-    Source: http://blog.thehumangeo.com/2014/05/12/drawing-boundaries-in-python/
+    Source: https://thehumangeo.wordpress.com/2014/05/12/drawing-boundaries-in-python/
     @param points: Iterable container of points.
     @param alpha: alpha value to influence the
         gooeyness of the border. Smaller numbers
@@ -74,8 +73,8 @@ def plot_polygon(polygon):
     x_min, y_min, x_max, y_max = polygon.bounds
     ax.set_xlim([x_min-margin, x_max+margin])
     ax.set_ylim([y_min-margin, y_max+margin])
-    patch = PolygonPatch(polygon, fc='#999999',
-                         ec='#000000', fill=True,
-                         zorder=-1)
-    ax.add_patch(patch)
+    for geom in polygon.geoms:
+        xs, ys = geom.exterior.xy
+        ax.fill(xs, ys, fc='#999999', ec='#000000', fill=True)
+
     return fig
