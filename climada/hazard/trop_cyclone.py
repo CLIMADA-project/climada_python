af8d7f6df928627594c0767e4c821c4ab75b4c38
Chahan Kropf <chahan.kropf@usys.ethz.ch>
2021-05-10 17:47:29 +0200

    Reverse cumulative knutson cc (#224)
    * Reverse cumulative knutson cc
    
    * Update climada/hazard/test/test_trop_cyclone.py
    
    * Remove inacurate comment
    
    Co-authored-by: Chahan Kropf <chahan.kropf@posteo.com>

--- climada/hazard/trop_cyclone.py
+++ climada/hazard/trop_cyclone.py
@@ -441,8 +441,8 @@ class TropCyclone(Hazard):
         Returns
         -------
         tc_cc : climada.hazard.TropCyclone
-            Tropical cyclone with frequency and intensity scaled according
-            to the Knutson criterion. Returns a new instance of TropCyclone.
+            Tropical cyclone with frequency and intensity scaled inspired by
+            the Knutson criterion. Returns a new instance of TropCyclone.
         """
 
         tc_cc = copy.deepcopy(self)
@@ -473,9 +473,7 @@ class TropCyclone(Hazard):
                         ]
             freq_chg.sort(reverse=False, key=lambda x: len(x['category']))
 
-            # Iteratively scale frequencies for each category such that
-            # cumulative frequencies are scaled according to Knutson criterion.
-
+            # Scale frequencies by category
             cat_larger_list = []
             for chg in freq_chg:
                 cat_chg_list = [cat
@@ -485,24 +483,12 @@ class TropCyclone(Hazard):
                 sel_cat_chg = np.isin(tc_cc.category, cat_chg_list) & bas_sel
                 if sel_cat_chg.any():
                     freq_scaling = 1 + (chg['change'] - 1) * scaling_rcp_year
-                    sel_cat_all = (np.isin(tc_cc.category, chg['category'])
-                                   & bas_sel)
-                    sel_cat_larger = (np.isin(tc_cc.category, cat_larger_list)
-                                      & bas_sel)
-                    freq_scaling_cor = (
-                        (np.sum(self.frequency[sel_cat_all]) * freq_scaling
-                         - np.sum(tc_cc.frequency[sel_cat_larger]))
-                        / np.sum(self.frequency[sel_cat_chg])
-                    )
-                    tc_cc.frequency[sel_cat_chg] *= freq_scaling_cor
+                    tc_cc.frequency[sel_cat_chg] *= freq_scaling
                 cat_larger_list += cat_chg_list
 
         if (tc_cc.frequency < 0).any():
             raise ValueError("The application of the given climate scenario"
-                             "resulted in at least one negative frequenciy."
-                             "This is likely due to the use of a"
-                             "non-representative event set (too small, "
-                             "incorrect reference period, ...)")
+                             "resulted in at least one negative frequency.")
 
         return tc_cc
 
