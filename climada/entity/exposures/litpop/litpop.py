cfcf45f75850e74491ab912bd404807d433974d2
sameberenz <>
2021-06-23 14:30:15 +0200

    litpop: clean up

--- climada/entity/exposures/litpop/litpop.py
+++ climada/entity/exposures/litpop/litpop.py
@@ -15,7 +15,6 @@ You should have received a copy of the GNU General Public License along
 with CLIMADA. If not, see <https://www.gnu.org/licenses/>.
 
 ---
-f
 Define LitPop class.
 """
 import logging
