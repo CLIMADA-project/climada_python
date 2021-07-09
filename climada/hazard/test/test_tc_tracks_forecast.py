7205e1bd86a6aff855205f2c42f68ee0ebfdb3a1
Pui Man (Mannie) Kam <manniekam@gmail.com>
2021-07-09 14:58:04 +0200

    modified test file, not finalised

--- climada/hazard/test/test_tc_tracks_forecast.py
+++ climada/hazard/test/test_tc_tracks_forecast.py
@@ -26,12 +26,13 @@ from climada import CONFIG
 from climada.hazard.tc_tracks_forecast import TCForecast
 
 DATA_DIR = CONFIG.hazard.test_data.dir()
-TEST_BUFR_FILES = [
+TEST_BUFR_FILES_PATH = [
     DATA_DIR.joinpath(tbf) for tbf in [
         'tracks_22S_HEROLD_2020031912.det.bufr4',
         'tracks_22S_HEROLD_2020031912.eps.bufr4',
     ]
 ]
+TEST_BUFR_FILES = [open(f, 'rb') for f in TEST_BUFR_FILES_PATH]
 """TC tracks in four BUFR formats as provided by ECMWF. Sourced from
 https://confluence.ecmwf.int/display/FCST/New+Tropical+Cyclone+Wind+Radii+product
 """
@@ -43,15 +44,15 @@ class TestECMWF(unittest.TestCase):
     def test_fetch_ecmwf(self):
         """Test ECMWF reader with static files"""
         forecast = TCForecast()
-        forecast.fetch_ecmwf(TEST_BUFR_FILES)
+        forecast.fetch_ecmwf(files=TEST_BUFR_FILES)
 
         self.assertEqual(forecast.data[0].time.size, 2)
-        self.assertEqual(forecast.data[1].lat[2], -36.79)
-        self.assertEqual(forecast.data[0].lon[1], 73.5)
-        self.assertEqual(forecast.data[1].time_step[2], 42)
-        self.assertEqual(forecast.data[1].max_sustained_wind[2], 17.1)
-        self.assertEqual(forecast.data[0].central_pressure[0], 1000.)
-        self.assertEqual(forecast.data[0]['time.year'][0], 2020)
+        self.assertEqual(forecast.data[1].lat[2], -27.)
+        self.assertEqual(forecast.data[0].lon[2], 73.5)
+        self.assertEqual(forecast.data[1].time_step[2], 6)
+        self.assertEqual(forecast.data[1].max_sustained_wind[2], 14.9)
+        self.assertEqual(forecast.data[0].central_pressure[1], 1000.)
+        self.assertEqual(forecast.data[0]['time.year'][1], 2020)
         self.assertEqual(forecast.data[17]['time.month'][7], 3)
         self.assertEqual(forecast.data[17]['time.day'][7], 21)
         self.assertEqual(forecast.data[0].max_sustained_wind_unit, 'm/s')
@@ -67,16 +68,16 @@ class TestECMWF(unittest.TestCase):
     def test_equal_timestep(self):
         """Test equal timestep"""
         forecast = TCForecast()
-        forecast.fetch_ecmwf(TEST_BUFR_FILES)
+        forecast.fetch_ecmwf(files=TEST_BUFR_FILES)
         forecast.equal_timestep(1)
 
-        self.assertEqual(forecast.data[1].time.size, 49)
-        self.assertEqual(forecast.data[1].lat.size, 49)
-        self.assertEqual(forecast.data[1].lon.size, 49)
-        self.assertEqual(forecast.data[1].max_sustained_wind.size, 49)
-        self.assertEqual(forecast.data[1].central_pressure.size, 49)
-        self.assertEqual(forecast.data[1].radius_max_wind.size, 49)
-        self.assertEqual(forecast.data[1].environmental_pressure.size, 49)
+        self.assertEqual(forecast.data[1].time.size, 13)
+        self.assertEqual(forecast.data[1].lat.size, 13)
+        self.assertEqual(forecast.data[1].lon.size, 13)
+        self.assertEqual(forecast.data[1].max_sustained_wind.size, 13)
+        self.assertEqual(forecast.data[1].central_pressure.size, 13)
+        self.assertEqual(forecast.data[1].radius_max_wind.size, 13)
+        self.assertEqual(forecast.data[1].environmental_pressure.size, 13)
         self.assertEqual(forecast.data[1].time_step[2], 1.)
 
 # Execute Tests
