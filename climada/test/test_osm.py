4bf2baf5be963606b44242555a85901cc2181679
emanuel-schmid <schmide@ethz.ch>
2021-07-15 09:47:49 +0200

    test_osm: randint has an inclusive upper boundary (unlike range)

--- climada/test/test_osm.py
+++ climada/test/test_osm.py
@@ -61,7 +61,9 @@ class TestOpenStreetMapModule(unittest.TestCase):
             self.assertIsInstance(exposure_high_47_8, Exposures)
             self.assertEqual(len(exposure_high_47_8.gdf.columns), 8)
             self.assertGreater(
-                exposure_high_47_8.gdf.iloc[random.randint(0, len(exposure_high_47_8.gdf))].value,
+                exposure_high_47_8.gdf.iloc[
+                    random.randint(0, len(exposure_high_47_8.gdf) - 1)
+                ].value,
                 0)
 
     def test_make_osmexposure(self):
