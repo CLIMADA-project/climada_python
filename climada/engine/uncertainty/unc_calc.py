cb951e17172569d93e40ecf475730ce6a45ba8ff
Chahan Kropf <chahan.kropf@posteo.com>
2021-07-26 19:28:16 +0200

    Add CLIMADA license

--- climada/engine/uncertainty/unc_calc.py
+++ climada/engine/uncertainty/unc_calc.py
@@ -1,13 +1,25 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Mon Jul  5 10:07:53 2021
+This file is part of CLIMADA.
 
-@author: ckropf
+Copyright (C) 2017 ETH Zurich, CLIMADA contributors listed in AUTHORS.
+
+CLIMADA is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free
+Software Foundation, version 3.
+
+CLIMADA is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with CLIMADA. If not, see <https://www.gnu.org/licenses/>.
+
+---
+
+Define Uncertainty class.
 """
 
 import logging
-import itertools
 
 import pandas as pd
 import datetime as dt
