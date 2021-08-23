f40ff82fabb8ce48888a966ac94a0109cd49da67
Chahan Kropf <chahan.kropf@posteo.com>
2021-08-23 13:34:39 +0200

    Change calc to calc_base

--- climada/engine/uncertainty_quantification/__init__.py
+++ climada/engine/uncertainty_quantification/__init__.py
@@ -19,6 +19,6 @@ with CLIMADA. If not, see <https://www.gnu.org/licenses/>.
 
 from .unc_output import *
 from .input_var import *
-from .calc import *
+from .calc_base import *
 from .calc_impact import *
 from .calc_cost_benefit import *
