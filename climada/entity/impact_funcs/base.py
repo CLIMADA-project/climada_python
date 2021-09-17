25e5d27def15136346ac1fda2da8e57ea2a12955
Chahan Kropf <chahan.kropf@posteo.com>
2021-09-17 13:47:52 +0200

    Check for None axis instead of not axis

--- climada/entity/impact_funcs/base.py
+++ climada/entity/impact_funcs/base.py
@@ -80,7 +80,7 @@ class ImpactFunc():
         Returns:
             matplotlib.axes._subplots.AxesSubplot
         """
-        if not axis:
+        if axis is None:
             _, axis = plt.subplots(1, 1)
 
         title = '%s %s' % (self.haz_type, str(self.id))
