206b9ded3c42daa7c047695615b2da72f5f0c84b
Chahan Kropf <chahan.kropf@usys.ethz.ch>
2021-10-18 14:56:23 +0200

    Update climada/engine/impact.py
    Co-authored-by: Chris Fairless <chrisfairless@hotmail.com>

--- climada/engine/impact.py
+++ climada/engine/impact.py
@@ -849,7 +849,7 @@ class Impact():
         """This function is deprecated, use Impact.from_excel instead."""
         LOGGER.warning("The use of Impact.read_excel is deprecated."
                        "Use Impact.from_excel instead.")
-        self.__dict__ = Impact.from_csv(*args, **kwargs).__dict__
+        self.__dict__ = Impact.from_excel(*args, **kwargs).__dict__
 
     @staticmethod
     def video_direct_impact(exp, impf_set, haz_list, file_name='',
