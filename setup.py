fd3077434112823e4ca1603fd77d59bb0bf3832e
emanuel-schmid <schmide@ethz.ch>
2022-02-11 14:15:10 +0100

    set version number 3.1.1-dev

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(here.joinpath('README.md'), encoding='utf-8') as f:
 setup(
     name='climada',
 
-    version='3.1.0',
+    version='3.1.1-dev',
 
     description='CLIMADA in Python',
 
