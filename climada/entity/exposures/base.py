4e33666cb9ec656ec9da322e8d495dad43aa2f9a
frqqyy <yyljy1999@126.com>
2022-07-04 19:16:39 +0200

    Revert "Revert "Merge branch 'develop' into feature/ignore_protected_access""
    This reverts commit a565a6a043b5832bbf3faaf19ab425d78eb60f65.

--- climada/entity/exposures/base.py
+++ climada/entity/exposures/base.py
@@ -137,13 +137,11 @@ class Exposures():
     @property
     def crs(self):
         """Coordinate Reference System, refers to the crs attribute of the inherent GeoDataFrame"""
-        if getattr(self.gdf, "crs", None):
-            return self.gdf.crs
-        # Due to a bug, the CRS of a GeoDataFrame and its geometry column might be out of sync:
-        # https://github.com/geopandas/geopandas/issues/1960
-        if "geometry" in self.gdf and getattr(self.gdf.geometry, "crs", None):
-            return self.gdf.geometry.crs
-        return self.meta.get('crs')
+        try:
+            return self.gdf.geometry.crs or self.meta.get('crs')
+        except AttributeError:  # i.e., no geometry, crs is assumed to be a property
+            # In case of gdf without geometry, empty or before set_geometry_points was called
+            return self.meta.get('crs')
 
     def __init__(self, *args, meta=None, tag=None, ref_year=DEF_REF_YEAR,
                  value_unit=DEF_VALUE_UNIT, crs=None, **kwargs):
