dd3066aeb213442b42f70af98f0088f74fdd768f
Emanuel Schmid <51439563+emanuel-schmid@users.noreply.github.com>
2022-10-27 16:19:55 +0200

    white space cosmetics

--- climada/entity/impact_funcs/impact_func_set.py
+++ climada/entity/impact_funcs/impact_func_set.py
@@ -541,7 +541,7 @@ class ImpactFuncSet():
                 try:
                     if len(df_func[var_names['col_name']['unit']].unique()) != 1:
                         raise ValueError('Impact function with two different \
-                                         intensity units.'                                                          )
+                                         intensity units.')
                     impf_kwargs["intensity_unit"] = df_func[var_names['col_name']
                                                             ['unit']].values[0]
                 except KeyError:
