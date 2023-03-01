187a7d5cbd0c0b53e65acb72a959a5b7292ed182
Alessio Ciullo <29018864+aleeciu@users.noreply.github.com>
2023-03-01 10:20:05 +0100

    Add Impact.impact_at_reg method for aggregating impacts per region (#642)
    The method either takes an array indicating the region for each exposure centroid, or
    aggregates the impact per country. It returns a pandas.DataFrame with one event per row
    where the columns are the unique aggregation regions.
    
    Changes:
    * Add `Impact.impact_at_reg` method.
    * Add unit tests.
    
    ---------
    
    Co-authored-by: Lukas Riedel <34276446+peanutfun@users.noreply.github.com>
    Co-authored-by: Chahan M. Kropf <chahan.kropf@usys.ethz.ch>
    Co-authored-by: Emanuel Schmid <51439563+emanuel-schmid@users.noreply.github.com>

--- climada/engine/test/test_impact.py
+++ climada/engine/test/test_impact.py
@@ -496,6 +496,78 @@ class TestRPmatrix(unittest.TestCase):
         self.assertAlmostEqual(np.max(impact_rp), 2916964966.388219, places=5)
         self.assertAlmostEqual(np.min(impact_rp), 444457580.131494, places=5)
 
+
+class TestImpactReg(unittest.TestCase):
+    """Test impact aggregation per aggregation region or admin 0"""
+
+    def setUp(self):
+        """Build the impact object for testing"""
+        self.imp = dummy_impact()
+
+    def test_agg_regions(self):
+        """Test calc local impacts per region"""
+        # Aggregate over a single region
+        region_ids = ["A", "A"]
+        at_reg_event = self.imp.impact_at_reg(region_ids)
+
+        self.assertEqual(at_reg_event.sum().sum(), self.imp.at_event.sum())
+        self.assertEqual(at_reg_event.shape[0], self.imp.at_event.shape[0])
+        self.assertEqual(at_reg_event.shape[1], np.unique(region_ids).shape[0])
+
+        # Aggregate over two different regions
+        region_ids = ["A", "B"]
+        at_reg_event = self.imp.impact_at_reg(region_ids)
+
+        self.assertEqual(at_reg_event["A"].sum(), self.imp.imp_mat[:, 0].sum())
+        self.assertEqual(at_reg_event["B"].sum(), self.imp.imp_mat[:, 1].sum())
+
+        self.assertEqual(at_reg_event.sum().sum(), self.imp.at_event.sum())
+        self.assertEqual(at_reg_event.shape[0], self.imp.at_event.shape[0])
+        self.assertEqual(at_reg_event.shape[1], np.unique(region_ids).shape[0])
+
+    def test_admin0(self):
+        """Test with aggregation to countries"""
+        # Let's specify sample cities' coords
+        zurich_lat, zurich_lon = 47.37, 8.55
+        bern_lat, bern_lon = 46.94, 7.44
+        rome_lat, rome_lon = 41.89, 12.51
+
+        # Test admin 0 with one country
+        self.imp.coord_exp = np.array([[zurich_lat, zurich_lon], [bern_lat, bern_lon]])
+
+        at_reg_event = self.imp.impact_at_reg()
+
+        self.assertEqual(len(at_reg_event.columns), 1)
+        self.assertEqual(at_reg_event.columns[0], "CHE")
+
+        self.assertEqual(at_reg_event.shape[0], self.imp.at_event.shape[0])
+        self.assertEqual(
+            at_reg_event["CHE"].sum(), at_reg_event.sum().sum(), self.imp.at_event.sum()
+        )
+
+        # Test admin 0 with two countries
+        self.imp.coord_exp = np.array([[rome_lat, rome_lon], [bern_lat, bern_lon]])
+        at_reg_event = self.imp.impact_at_reg()
+
+        self.assertEqual(len(at_reg_event.columns), 2)
+        self.assertEqual(at_reg_event.columns[0], "CHE")
+        self.assertEqual(at_reg_event.columns[1], "ITA")
+
+        self.assertEqual(at_reg_event.shape[0], self.imp.at_event.shape[0])
+        self.assertEqual(at_reg_event["CHE"].sum(), self.imp.imp_mat[:, 0].sum())
+        self.assertEqual(at_reg_event["ITA"].sum(), self.imp.imp_mat[:, 1].sum())
+        self.assertEqual(at_reg_event.sum().sum(), self.imp.at_event.sum())
+
+    def test_no_imp_mat(self):
+        """Check error if no impact matrix is stored"""
+        # Test error when no imp_mat is stored
+        self.imp.imp_mat = sparse.csr_matrix((0, 0))
+
+        with self.assertRaises(ValueError) as cm:
+            self.imp.impact_at_reg()
+        self.assertIn("no Impact.imp_mat was stored", str(cm.exception))
+
+
 class TestRiskTrans(unittest.TestCase):
     """Test risk transfer methods"""
     def test_risk_trans_pass(self):
@@ -1027,6 +1099,7 @@ if __name__ == "__main__":
     TESTS.addTests(unittest.TestLoader().loadTestsFromTestCase(TestImpactPerYear))
     TESTS.addTests(unittest.TestLoader().loadTestsFromTestCase(TestIO))
     TESTS.addTests(unittest.TestLoader().loadTestsFromTestCase(TestRPmatrix))
+    TESTS.addTests(unittest.TestLoader().loadTestsFromTestCase(TestImpactReg))
     TESTS.addTests(unittest.TestLoader().loadTestsFromTestCase(TestRiskTrans))
     TESTS.addTests(unittest.TestLoader().loadTestsFromTestCase(TestSelect))
     TESTS.addTests(unittest.TestLoader().loadTestsFromTestCase(TestConvertExp))
