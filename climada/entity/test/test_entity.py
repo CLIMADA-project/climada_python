60afbed76ed5cdeda9b16cf0834f297cbbec919b
emanuel-schmid <schmide@ethz.ch>
2023-07-12 15:41:15 +0200

    climada.entity.disc_rates: remove Tag from DiscRates

--- climada/entity/test/test_entity.py
+++ climada/entity/test/test_entity.py
@@ -59,7 +59,7 @@ class TestReader(unittest.TestCase):
         """Read entity from mat file produced by climada."""
         entity_mat = Entity.from_mat(ENT_TEST_MAT)
         self.assertEqual(entity_mat.exposures.tag.file_name, [str(ENT_TEST_MAT)])
-        self.assertEqual(entity_mat.disc_rates.tag.file_name, [str(ENT_TEST_MAT)])
+        self.assertIsInstance(entity_mat.disc_rates, DiscRates)
         self.assertEqual(entity_mat.measures.tag.file_name, [str(ENT_TEST_MAT)])
         self.assertEqual(entity_mat.impact_funcs.tag.file_name, [str(ENT_TEST_MAT)])
 
@@ -67,7 +67,6 @@ class TestReader(unittest.TestCase):
         """Read entity from an xls file following the template."""
         entity_xls = Entity.from_excel(ENT_TEMPLATE_XLS)
         self.assertEqual(entity_xls.exposures.tag.file_name, [str(ENT_TEMPLATE_XLS)])
-        self.assertEqual(entity_xls.disc_rates.tag.file_name, [str(ENT_TEMPLATE_XLS)])
         self.assertEqual(entity_xls.measures.tag.file_name, [str(ENT_TEMPLATE_XLS)])
         self.assertEqual(entity_xls.impact_funcs.tag.file_name,
                          [str(ENT_TEMPLATE_XLS)])
