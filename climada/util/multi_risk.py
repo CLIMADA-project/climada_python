5d4c0db32ff4bc4396db8955d01764cb49d526ef
zeliest <zelie.stalhandske@usys.ethz.ch>
2023-07-14 11:32:48 +0200

    continue developing multi-risk methods

--- climada/util/multi_risk.py
+++ climada/util/multi_risk.py
@@ -12,7 +12,7 @@ with CLIMADA. If not, see <https://www.gnu.org/licenses/>.
 ---
 Define functions to handle impact_yearsets
 """
-
+import itertools
 import copy
 import datetime
 import warnings
@@ -28,7 +28,6 @@ from climada.engine import Impact, ImpactCalc
 from climada.util.constants import DEF_CRS
 
 
-#TODO try in impactcalc class
 def calc_impacts(haz_dict, exp_dict, impf_set, mapping_exp_haz=None):
     """
         Calculate impacts based on hazard, exposure, and impact function data.
@@ -43,12 +42,14 @@ def calc_impacts(haz_dict, exp_dict, impf_set, mapping_exp_haz=None):
         dict: Dictionary of impacts calculated for each combination of exposure and hazard.
         """
     if mapping_exp_haz is None:
-        exp_keys = exp_dict.keys()
-        haz_keys = haz_dict.keys()
-        mapping_exp_haz = list(product(exp_keys, haz_keys))
+        exp_keys = list(exp_dict.keys())
+        haz_keys = list(haz_dict.keys())
+
+        # Generate all possible combinations of keys
+        mapping_exp_haz = list(itertools.product(exp_keys, haz_keys))
 
     impacts_dict = {keys:
-        ImpactCalc().impact(exp_dict[keys[0]], impf_set, haz_dict[keys[1]])
+        ImpactCalc(exp_dict[keys[0]], impf_set, haz_dict[keys[1]]).impact()
         for keys in mapping_exp_haz
     }
 
@@ -75,14 +76,20 @@ def find_common_time_definition(dates):
         if len(date_array) == 1:
             warnings.warn("Array with a single date encountered. It will not contribute to the common time definition.")
             continue
-        if len(set(datetime.datetime.fromordinal(date).day for date in date_array)) > 1:
-            res = 'day'
-        elif len(set(datetime.datetime.fromordinal(date).isocalendar()[1] for date in date_array)) > 1:
-            res = 'week'
-        elif len(set(datetime.datetime.fromordinal(date).month for date in date_array)) > 1:
-            res = 'month'
-        elif len(set(datetime.datetime.fromordinal(date).year for date in date_array)) > 1:
+
+        date_list = [datetime.date.fromordinal(date) for date in date_array]
+        differences = [(date_list[i + 1] - date_list[i]).days for i in range(len(date_list) - 1)]
+
+        if all(diff % 365 == 0 or diff % 366 == 0 for diff in differences):
             res = 'year'
+        elif all(diff % 28 == 0 or diff % 29 == 0 or diff % 30 == 0 or diff % 31 == 0 for diff in differences):
+            res = 'month'
+        elif all(diff % 7 == 0 for diff in differences):
+            res = 'week'
+        elif all(diff % 1 == 0 for diff in differences):
+            res = 'day'
+        else:
+            res = 'custom'
 
         resolutions.append(res)
 
@@ -110,7 +117,7 @@ def upscale_dates(impact, by='year'):
     dict: Dictionary containing the impact data with upscaled dates.
     """
     imp = copy.deepcopy(impact)
-    dates = [datetime.datetime.fromordinal(date) for date in imp['date']]
+    dates = [datetime.datetime.fromordinal(date) for date in imp.date]
     if by == 'year':
         dates = [datetime.date(date.year, 1, 1).toordinal() for date in dates]
     elif by == 'month':
@@ -118,11 +125,11 @@ def upscale_dates(impact, by='year'):
     elif by == 'week':
         dates = [datetime.date.fromisocalendar(date.year, date.isocalendar()[1], 1).toordinal() for date in dates]
 
-    imp['date'] = np.array(dates)
+    imp.date = np.array(dates)
     return imp
 
 
-def aggregate_impact_by_date(impact, how='sum', by='year', exp=None):
+def aggregate_impact_by_date(impact, how='sum', exp=None):
     """
     Aggregate events to have lower resolution. Maximum impact per year
     at each exposure point is exposure value if exp is not None.
@@ -146,7 +153,7 @@ def aggregate_impact_by_date(impact, how='sum', by='year', exp=None):
         Impact yearset.
 
     """
-    impact_upscaled_dates = upscale_dates(impact, by=by)
+    impact_upscaled_dates = copy.deepcopy(impact)
     if how == 'sum':
         mask = [np.ma.make_mask(np.array(impact_upscaled_dates.date) == event).astype(int)
                 for event in np.unique(impact_upscaled_dates.date)]
@@ -155,8 +162,8 @@ def aggregate_impact_by_date(impact, how='sum', by='year', exp=None):
 
     elif how == 'max':
         imp_mat = sp.sparse.csr_matrix(sp.sparse.vstack(
-            [impact_upscaled_dates.imp_mat[(np.array(impact_upscaled_dates.event_name) == event).astype(bool)].max(axis=0)
-             for event in np.unique(impact_upscaled_dates.date)]))
+            [impact_upscaled_dates.imp_mat[(np.array(impact_upscaled_dates.date) == date).astype(bool)].max(axis=0)
+             for date in np.unique(impact_upscaled_dates.date)]))
     else:
         warnings.warn("Unsupported value for 'how' parameter. Only 'sum' and 'max' methods are currently supported.")
 
@@ -165,10 +172,10 @@ def aggregate_impact_by_date(impact, how='sum', by='year', exp=None):
         m2 = np.array(exp.gdf.value[imp_mat.nonzero()[1]])
         imp_mat = sp.sparse.csr_matrix((np.minimum(m1, m2), imp_mat.indices, imp_mat.indptr))
 
-    years = np.unique([datetime.date.fromordinal(date).year for date in impact.date])
+    years = np.unique([datetime.date.fromordinal(date) for date in impact.date])
     frequency = np.ones(imp_mat.shape[0]) / len(np.unique(years))
     at_event, eai_exp, aai_agg = ImpactCalc.risk_metrics(imp_mat, frequency)
-    date = np.arange(1, len(at_event) + 1)
+    date = np.unique(impact_upscaled_dates.date)
     event_id = np.arange(1, len(at_event) + 1)
     event_name = np.unique(event_id)
     impact_aggr = Impact(
@@ -234,10 +241,61 @@ def fill_impact_gaps(impact_dict):
         filled_impact = copy.deepcopy(imp)
         filled_impact.imp_mat = new_mat
         filled_impact.date = all_dates
+        filled_impact.event_id = np.arange(1, len(all_dates)+1)
+        filled_impact.event_name = np.arange(1, len(all_dates) + 1)
         filled_impact.coord_exp = np.array([list(coord) for coord in all_coords])
+        years = np.unique([datetime.date.fromordinal(date).year for date in filled_impact.date])
+        filled_impact.frequency = np.ones(new_mat.shape[0]) / len(np.unique(years))
+        filled_impact.at_event, filled_impact.eai_exp, filled_impact.aai_agg = \
+            ImpactCalc.risk_metrics(filled_impact.imp_mat, filled_impact.frequency)
         filled_impacts[hazard] = filled_impact
 
     return filled_impacts
+
+
+import itertools
+
+def calculate_combinations_dict(impact_dict, how='sum', by='date', exp=None, combination_type='all'):
+    """
+    Calculates combinations of impacts from a dictionary.
+
+    Parameters
+    ----------
+    impact_dict : dict
+        Dictionary of impacts with the same coord and dates
+    how : str, optional
+        How to combine the impacts, options are 'sum', 'max', or 'min', by default 'sum'
+    by : str, optional
+        Common value used to combine events, can be 'date', 'event_id', or 'event_name', by default 'date'
+    exp : object, optional
+        If the exposures are given, the impacts are capped at their value, by default None
+    combination_type : str, optional
+        Type of combinations to calculate, can be 'all' or 'pairs', by default 'all'
+
+    Returns
+    -------
+    combined_impacts : dict
+        Dictionary of combined impacts with hazard keys
+    """
+    impact_list = list(impact_dict.values())
+    hazard_keys = list(impact_dict.keys())
+
+    if combination_type == 'all':
+        combinations = []
+        for r in range(1, len(impact_list) + 1):
+            combinations.extend(list(itertools.combinations(impact_list, r)))
+    elif combination_type == 'pairs':
+        combinations = list(itertools.combinations(impact_list, 2))
+    else:
+        raise ValueError("Invalid combination_type. Valid options are 'all' or 'pairs'.")
+
+    combined_impacts = {}
+    for combination in combinations:
+        combined_impact = combine_impacts(list(combination), how=how, by=by, exp=exp)
+        hazard_key = "-".join([str(hazard_keys[impact_list.index(imp)]) for imp in combination])
+        combined_impacts[hazard_key] = combined_impact
+
+    return combined_impacts
 
 
 def combine_impacts(impact_list, how='sum', by='date', exp=None):
@@ -246,6 +304,7 @@ def combine_impacts(impact_list, how='sum', by='date', exp=None):
     ----------
     impact_list : list or dict of impacts with the same coord and dates
     how : how to combine the impacts, options are 'sum', 'max' or 'min'
+    by : array of common value used to combine events, can be date, event_id or event_name
     exp : If the exposures are given, the impacts are caped at their value
 
     Returns
@@ -258,6 +317,8 @@ def combine_impacts(impact_list, how='sum', by='date', exp=None):
 
     imp0 = copy.deepcopy(impact_list[0])
     for imp in impact_list:
+        if imp.unit != imp0.unit:
+            raise ValueError("The impacts do not have the same units and cannot be combined.")
         # Check if imp_mat contains non-zero values
         if imp.imp_mat.nnz == 0:
             raise ValueError("imp_mat contains only zero values")
@@ -281,7 +342,7 @@ def combine_impacts(impact_list, how='sum', by='date', exp=None):
                              "affecting exposures with different unit, you may normalize the impact first.")
 
     if by == 'event_name' or by == 'date':
-        aggr_attr = {'event_name':imp.event_name, 'date':imp.date}
+        aggr_attr = {'event_name': imp.event_name, 'date':imp.date}
         unique_elements = np.unique(aggr_attr[by])
         # Create a dictionary mapping unique elements to integers
         element_to_int = {element: i + 1 for i, element in enumerate(unique_elements)}
@@ -346,7 +407,6 @@ def combine_impacts(impact_list, how='sum', by='date', exp=None):
     )
     return impact_aggr
 
-
 def mask_single_hazard_impact(impact, impact_list):
     """ Mask points in impact matrix of an impact where other impacts provided in impact_list do not cause impacts
     and return a new impact object. This allow to to estimate impacts that happen at the same time at the same exposure.
@@ -400,15 +460,35 @@ def corr_btw_hazards(hazard_dict, temporal=True, spatial=False):
     -------
     corr_df : pd.DataFrame
     """
-    if temporal is True and spatial is False:
-        df = pd.DataFrame.from_dict({hazard: hazard_dict[hazard].intensity.max(axis=1) for hazard in hazard_dict})
-    if spatial is True and temporal is False:
-        df = pd.DataFrame.from_dict({hazard: hazard_dict[hazard].intensity.max(axis=0) for hazard in hazard_dict})
-    if spatial is True and temporal is True:
-        df = pd.DataFrame.from_dict({hazard: np.array(hazard_dict[hazard].imp_mat.todense().flatten())[0]
-                                     for hazard in hazard_dict})
-    return df.corr()
+    all_dates = sorted(set(date for haz in hazard_dict.values() for date in haz.date))
+    all_coords = sorted(set(tuple(coord) for haz in hazard_dict.values() for coord in haz.centroids.coord))
 
+    date_mapping = {date: i for i, date in enumerate(all_dates)}
+    coord_mapping = {tuple(coord): i for i, coord in enumerate(all_coords)}
+
+    filled_intensities = {}
+
+    for haz_name, hazard in hazard_dict.items():
+        if hazard.intensity.nnz == 0:
+            raise ValueError("An element in impact_dict's imp_mat contains only zero values")
+
+        new_mat = lil_matrix((len(all_dates), len(all_coords)))
+
+        # Step 3: Fill the new matrix with the data from the impact
+        for date, coord, data in zip(hazard.date[hazard.intensity.nonzero()[0]],
+                                     hazard.centroids.coord[hazard.intensity.nonzero()[1]], hazard.intensity.data):
+            new_mat[date_mapping[date], coord_mapping[tuple(coord)]] = data
+
+        # Convert the new matrix to a CSR matrix (more efficient for calculations)
+        filled_intensities[haz_name] = new_mat.tocsr()
+    if temporal is True and spatial is False:
+        df = pd.DataFrame.from_dict({hazard: filled_intensities[hazard].max(axis=1).toarray().flatten() for hazard in filled_intensities})
+    if spatial is True and temporal is False:
+        df = pd.DataFrame.from_dict({hazard:  filled_intensities[hazard].max(axis=0).toarray().flatten() for hazard in filled_intensities})
+    if spatial is True and temporal is True:
+        df = pd.DataFrame.from_dict({hazard: np.array(filled_intensities[hazard].todense().flatten())[0]
+                                     for hazard in filled_intensities})
+    return df.corr()
 
 def corr_btw_impacts(impact_dict, temporal=True, spatial=False):
     """
@@ -422,6 +502,7 @@ def corr_btw_impacts(impact_dict, temporal=True, spatial=False):
     -------
     corr_df : pd.DataFrame
     """
+
     if temporal is True and spatial is False:
         df = pd.DataFrame.from_dict({hazard: impact_dict[hazard].at_event for hazard in impact_dict})
     if spatial is True and temporal is False:
@@ -430,3 +511,4 @@ def corr_btw_impacts(impact_dict, temporal=True, spatial=False):
         df = pd.DataFrame.from_dict({hazard: np.array(impact_dict[hazard].imp_mat.todense().flatten())[0]
                                      for hazard in impact_dict})
     return df.corr()
+
