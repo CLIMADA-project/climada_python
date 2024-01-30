eda27554fa1c396e68934a8a63adc5426926238c
emanuel-schmid <schmide@ethz.ch>
2024-01-30 14:40:27 +0100

    hazard.base.write_htdf5: add inline comment.

--- climada/hazard/base.py
+++ climada/hazard/base.py
@@ -1775,6 +1775,8 @@ class Hazard():
             str_dt = h5py.special_dtype(vlen=str)
             for (var_name, var_val) in self.__dict__.items():
                 if var_name == 'centroids':
+                    # Centroids have their own write_hdf2 method,
+                    # which is invoked at the end of this method (s.b.)
                     pass
                 elif isinstance(var_val, sparse.csr_matrix):
                     if todense:
