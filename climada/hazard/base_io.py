2ed47fb7041d7ae3612c3a5941debe6749b24d89
emanuel-schmid <schmide@ethz.ch>
2024-02-09 16:03:47 +0100

    climada.hazard.HazardIO: remove broken vector file support

--- climada/hazard/base_io.py
+++ climada/hazard/base_io.py
@@ -831,72 +831,6 @@ class HazardIO():
         LOGGER.debug("Hazard successfully loaded. Number of events: %i", num_events)
         return cls(centroids=centroids, intensity=intensity_matrix, **hazard_kwargs)
 
-    @classmethod
-    def from_vector(cls, files_intensity, files_fraction=None, attrs=None,
-                    inten_name=None, frac_name=None, dst_crs=None, haz_type=None):
-        """Read vector files format supported by fiona. Each intensity name is
-        considered an event.
-
-        Parameters
-        ----------
-        files_intensity : list(str)
-            file names containing intensity, default: ['intensity']
-        files_fraction : (list(str))
-            file names containing fraction,
-            default: ['fraction']
-        attrs : dict, optional
-            name of Hazard attributes and their values
-        inten_name : list(str), optional
-            name of variables containing the intensities of each event
-        frac_name : list(str), optional
-            name of variables containing
-            the fractions of each event
-        dst_crs : crs, optional
-            reproject to given crs
-        haz_type : str, optional
-            acronym of the hazard type (e.g. 'TC').
-            default: None, which will use the class default ('' for vanilla
-            `Hazard` objects, hard coded in some subclasses)
-
-        Returns
-        -------
-        haz : climada.hazard.Hazard
-            Hazard from vector file
-        """
-        if not attrs:
-            attrs = {}
-        if not inten_name:
-            inten_name = ['intensity']
-        if not frac_name:
-            inten_name = ['fraction']
-        if files_fraction is not None and len(files_intensity) != len(files_fraction):
-            raise ValueError('Number of intensity files differs from fraction files:'
-                             f' {len(files_intensity)} != {len(files_fraction)}')
-
-        hazard_kwargs = {}
-        if haz_type is not None:
-            hazard_kwargs["haz_type"] = haz_type
-
-        if len(files_intensity) > 0:
-            centroids = Centroids.from_vector_file(files_intensity[0], dst_crs=dst_crs)
-        elif files_fraction is not None and len(files_fraction) > 0:
-            centroids = Centroids.from_vector_file(files_fraction[0], dst_crs=dst_crs)
-        else:
-            centroids = Centroids(lat=[], lon=[])
-
-        intensity = centroids.values_from_vector_files(
-            files_intensity, val_names=inten_name, dst_crs=dst_crs)
-        if files_fraction is None:
-            fraction = intensity.copy()
-            fraction.data.fill(1)
-        else:
-            fraction = centroids.values_from_vector_files(
-                files_fraction, val_names=frac_name, dst_crs=dst_crs)
-
-        hazard_kwargs.update(cls._attrs_to_kwargs(attrs, num_events=intensity.shape[0]))
-        return cls(
-            centroids=centroids, intensity=intensity, fraction=fraction, **hazard_kwargs)
-
     @staticmethod
     def _attrs_to_kwargs(attrs: Dict[str, Any], num_events: int) -> Dict[str, Any]:
         """Transform attributes to init kwargs or use default values
@@ -957,7 +891,6 @@ class HazardIO():
         """
         self.centroids.gdf.to_crs(dst_crs, inplace=True)
         self.check()
-
 
     @classmethod
     def from_mat(cls, file_name, var_names=None):
