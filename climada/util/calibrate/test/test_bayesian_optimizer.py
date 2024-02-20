cab68c48b7780f1c0804cd1ac35c18897d58272a
Lukas Riedel <34276446+peanutfun@users.noreply.github.com>
2024-02-20 15:15:40 +0100

    Fix calibrate module init and add first controller tests

--- climada/util/calibrate/test/test_bayesian_optimizer.py
+++ climada/util/calibrate/test/test_bayesian_optimizer.py
@@ -24,9 +24,43 @@ from unittest.mock import patch, MagicMock
 import numpy.testing as npt
 import pandas as pd
 
-from climada.util.calibrate import Input, BayesianOptimizer
+from climada.util.calibrate import Input, BayesianOptimizer, BayesianOptimizerController
 
 from .test_base import hazard, exposure
+
+
+def input():
+    """Return a mocked input"""
+    return Input(
+        hazard=hazard(),
+        exposure=exposure(),
+        data=pd.DataFrame(data={"col1": [1, 2], "col2": [2, 3]}, index=[0, 1]),
+        cost_func=MagicMock(),
+        impact_func_creator=MagicMock(),
+        impact_to_dataframe=MagicMock(),
+    )
+
+
+class TestBayesianOptimizerController(unittest.TestCase):
+    """Tests for the controller of the BayesianOptimizer"""
+
+    def test_kappa_decay(self):
+        """Check correct values for kappa_decay"""
+        contr = BayesianOptimizerController(kappa=3, kappa_min=3, n_iter=10)
+        self.assertAlmostEqual(contr.kappa_decay, 1.0)
+
+        contr = BayesianOptimizerController(kappa=3, kappa_min=1, n_iter=10)
+        self.assertAlmostEqual(contr.kappa * (contr.kappa_decay**10), 1.0)
+
+    def test_from_input(self):
+        """Check if input data is used correctly to set up controller"""
+        inp = input()
+        inp.bounds = {"a": (0, 1), "b": (1, 2)}
+
+        contr = BayesianOptimizerController.from_input(inp, sampling_base=3, kappa=3)
+        self.assertEqual(contr.kappa, 3)
+        self.assertEqual(contr.init_points, 3**2)
+        self.assertEqual(contr.n_iter, 3**2)
 
 
 class TestBayesianOptimizer(unittest.TestCase):
@@ -34,14 +68,7 @@ class TestBayesianOptimizer(unittest.TestCase):
 
     def setUp(self):
         """Mock the input"""
-        self.input = Input(
-            hazard=hazard(),
-            exposure=exposure(),
-            data=pd.DataFrame(data={"col1": [1, 2], "col2": [2, 3]}, index=[0, 1]),
-            cost_func=MagicMock(),
-            impact_func_creator=MagicMock(),
-            impact_to_dataframe=MagicMock(),
-        )
+        self.input = input()
 
     @patch("climada.util.calibrate.base.ImpactCalc", autospec=True)
     def test_kwargs_to_impact_func_creator(self, _):
@@ -54,11 +81,14 @@ class TestBayesianOptimizer(unittest.TestCase):
         self.input.bounds = {"x_2": (0, 1), "x 1": (1, 2)}
         self.input.cost_func.return_value = 1.0
         self.optimizer = BayesianOptimizer(self.input)
+        self.controller = BayesianOptimizerController(
+            init_points=2, n_iter=1, max_iterations=1
+        )
 
         # Call 'run'
         with patch.object(self.input, "impact_to_aligned_df") as align:
             align.return_value = (None, None)
-            self.optimizer.run(init_points=2, n_iter=1)
+            self.optimizer.run(self.controller)
 
         # Check call to '_kwargs_to_impact_func_gen'
         call_args = self.input.impact_func_creator.call_args_list
@@ -75,11 +105,14 @@ class TestBayesianOptimizer(unittest.TestCase):
         self.input.bounds = {"x_2": (0, 1), "x 1": (1, 2)}
         self.input.cost_func.side_effect = [1.0, -1.0]
         self.optimizer = BayesianOptimizer(self.input)
+        self.controller = BayesianOptimizerController(
+            init_points=1, n_iter=1, max_iterations=1
+        )
 
         # Call 'run'
         with patch.object(self.input, "impact_to_aligned_df") as align:
             align.return_value = (None, None)
-            output = self.optimizer.run(init_points=1, n_iter=1)
+            output = self.optimizer.run(self.controller)
 
         # Check target space
         npt.assert_array_equal(output.p_space.target, [-1.0, 1.0])
