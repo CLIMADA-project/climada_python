512ff8ea919198856f2a0f573ed44167791a0c5c
Bertrand <62471481+bergalli@users.noreply.github.com>
2024-04-21 09:49:59 +0200

    Update impact_calc.py
    convert mask from BooleanArray to numpy before calling .nonzero()

--- climada/engine/impact_calc.py
+++ climada/engine/impact_calc.py
@@ -251,7 +251,7 @@ class ImpactCalc():
             )
         if exp_gdf.size == 0:
             LOGGER.warning("No exposures with value >0 in the vicinity of the hazard.")
-        self._orig_exp_idx = mask.nonzero()[0]  # update index of kept exposures points in exp_gdf
+        self._orig_exp_idx = mask.to_numpy().nonzero()[0]  # update index of kept exposures points in exp_gdf
                                                 # within the full exposures
         return exp_gdf
 
