06ff1aadc0d847e61603eec4abd6810db07fceb1
Valentin Gebhart <valentin.gebhart@usys.ethz.ch>
2025-03-06 12:11:16 +0100

    shortened code of bounding_box_from_countries

--- climada/util/coordinates.py
+++ climada/util/coordinates.py
@@ -1723,19 +1723,20 @@ def bounding_box_from_countries(country_names, buffer=1.0):
     """
 
     country_geometry = get_country_geometries(country_names).geometry
-    longitudes, latitudes = [], []
-    for multipolygon in country_geometry:
-        if isinstance(multipolygon, Polygon):  # if entry is polygon
-            for coord in multipolygon.exterior.coords:  # Extract exterior coordinates
-                longitudes.append(coord[0])
-                latitudes.append(coord[1])
-        else:  # if entry is multipolygon
-            for polygon in multipolygon.geoms:
-                for coord in polygon.exterior.coords:  # Extract exterior coordinates
-                    longitudes.append(coord[0])
-                    latitudes.append(coord[1])
+    generator_country_geometry = (
+        poly if isinstance(poly, MultiPolygon) else MultiPolygon([poly])
+        for poly in country_geometry
+    )
+    lon, lat = np.concatenate(
+        [
+            np.array(pg.exterior.coords).T
+            for poly in generator_country_geometry
+            for pg in poly.geoms
+        ],
+        axis=1,
+    )
 
-    return latlon_bounds(np.array(latitudes), np.array(longitudes), buffer=buffer)
+    return latlon_bounds(lat, lon, buffer=buffer)
 
 
 def bounding_box_from_cardinal_bounds(*, northern, eastern, western, southern):
