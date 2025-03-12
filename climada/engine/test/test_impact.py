e5da2c02ee17ae9d1d37d90c8a47a189e310cfe2
Valentin Gebhart <valentin.gebhart@usys.ethz.ch>
2025-03-12 18:01:47 +0100

    fixed tests in test_impact

--- climada/engine/test/test_impact.py
+++ climada/engine/test/test_impact.py
@@ -556,6 +556,7 @@ class TestRPmatrix(unittest.TestCase):
             method="extrapolate",
             log_frequency=False,
             log_impact=False,
+            bin_decimals=2,
         )
         np.testing.assert_allclose(
             impact_stats.values[:, 1:].astype(float),
@@ -594,7 +595,7 @@ class TestRPmatrix(unittest.TestCase):
 
         # test log log extrapolation
         impact_stats, _, _ = impact.local_exceedance_impact(
-            return_periods=(1000, 30, 0.1), method="extrapolate"
+            return_periods=(1000, 30, 0.1), method="extrapolate", bin_decimals=2
         )
         np.testing.assert_allclose(
             impact_stats.values[:, 1:].astype(float),
@@ -661,6 +662,7 @@ class TestRPmatrix(unittest.TestCase):
             method="extrapolate",
             log_frequency=False,
             log_impact=False,
+            bin_decimals=2,
         )
 
         np.testing.assert_allclose(
@@ -707,7 +709,7 @@ class TestRPmatrix(unittest.TestCase):
 
         # test log log extrapolation
         return_stats, _, _ = impact.local_return_period(
-            threshold_impact=(0.1, 300, 1e5), method="extrapolate"
+            threshold_impact=(0.1, 300, 1e5), method="extrapolate", bin_decimals=2
         )
         np.testing.assert_allclose(
             return_stats.values[:, 1:].astype(float),
