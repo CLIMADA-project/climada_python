95052b2d653cadc3382124f4c52c4681bb4a9ed3
Lukas Riedel <34276446+peanutfun@users.noreply.github.com>
2025-03-19 10:34:15 +0100

    Upgrade to Python 3.12 (#870)
    * Upgrade to Python 3.12
    
    * Increment supported Python versions in setup.py.
    * Update GitHub workflow testing matrix.
    
    * Upgrade eccodes and cfgrib versions
    
    * Add prominent information on Python versions
    
    ---------
    
    Co-authored-by: emanuel-schmid <schmide@ethz.ch>

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
         "Topic :: Scientific/Engineering :: Mathematics",
     ],
     keywords="climate adaptation",
-    python_requires=">=3.10,<3.12",
+    python_requires=">=3.10,<3.13",
     install_requires=[
         "bayesian-optimization<2.0",
         "bottleneck",
