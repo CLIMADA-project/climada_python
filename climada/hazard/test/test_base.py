38b2fc5f22e46e64e411c1a6281daafe94f0de69
luseverin <luca.severino@usys.ethz.ch>
2025-12-08 12:43:57 +0100

    Correct error in hazard_kwargs test base

--- climada/hazard/test/test_base.py
+++ climada/hazard/test/test_base.py
@@ -48,21 +48,21 @@ Fraction is empty. Format: HDF5.
 
 def hazard_kwargs():
     return {
-        haz_type: "TC",
-        intensity: sparse.csr_matrix(
+        "haz_type": "TC",
+        "intensity": sparse.csr_matrix(
             [[0.2, 0.3, 0.4], [0.1, 0.1, 0.01], [4.3, 2.1, 1.0], [5.3, 0.2, 0.0]]
         ),
-        fraction: sparse.csr_matrix(
+        "fraction": sparse.csr_matrix(
             [[0.02, 0.03, 0.04], [0.01, 0.01, 0.01], [0.3, 0.1, 0.0], [0.3, 0.2, 0.0]]
         ),
-        centroids: Centroids(lat=np.array([1, 3, 5]), lon=np.array([2, 4, 6])),
-        event_id: np.array([1, 2, 3, 4]),
-        event_name: ["ev1", "ev2", "ev3", "ev4"],
-        date: np.array([1, 2, 3, 4]),
-        orig: np.array([True, False, False, True]),
-        frequency: np.array([0.1, 0.5, 0.5, 0.2]),
-        frequency_unit: "1/week",
-        units: "m/s",
+        "centroids": Centroids(lat=np.array([1, 3, 5]), lon=np.array([2, 4, 6])),
+        "event_id": np.array([1, 2, 3, 4]),
+        "event_name": ["ev1", "ev2", "ev3", "ev4"],
+        "date": np.array([1, 2, 3, 4]),
+        "orig": np.array([True, False, False, True]),
+        "frequency": np.array([0.1, 0.5, 0.5, 0.2]),
+        "frequency_unit": "1/week",
+        "units": "m/s",
     }
 
 
