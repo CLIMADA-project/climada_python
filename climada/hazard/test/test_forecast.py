d04fecf7e569a110c5c2585589b7c296b8cebc40
Luca Severino <91593121+luseverin@users.noreply.github.com>
2025-12-08 15:49:10 +0100

    Hazard forecast concat test (#1178)
    * Add test for hazardForecast.concat
    * Skip HazardForecast.concat test
    
    ---------
    
    Co-authored-by: luseverin <luca.severino@usys.ethz.ch>
    Co-authored-by: Lukas Riedel <34276446+peanutfun@users.noreply.github.com>

--- climada/hazard/test/test_forecast.py
+++ climada/hazard/test/test_forecast.py
@@ -88,6 +88,18 @@ def test_from_hazard(lead_time, member, hazard, haz_kwargs):
     assert_hazard_kwargs(haz_fc_from_haz, **haz_kwargs)
 
 
+@pytest.mark.skip("Concat from base class does not work")
+def test_hazard_forecast_concat(haz_fc, lead_time, member):
+    haz_fc1 = haz_fc.select(event_id=[1, 2])
+    haz_fc2 = haz_fc.select(event_id=[3, 4])
+    haz_fc_concat = HazardForecast.concat([haz_fc1, haz_fc2])
+    assert isinstance(haz_fc_concat, HazardForecast)
+    npt.assert_array_equal(
+        haz_fc_concat.lead_time, np.concatenate([lead_time, lead_time])
+    )
+    npt.assert_array_equal(haz_fc_concat.member, np.concatenate([member, member]))
+
+
 def test_hazard_forecast_select(haz_fc, lead_time, member):
     """Check if Hazard.select works on the derived class"""
     haz_fc_select = haz_fc.select(event_id=[4, 1])
