3ab6354c2f739312c18e232ceb889ce5c5aecff1
Lukas Riedel <34276446+peanutfun@users.noreply.github.com>
2025-12-08 15:04:50 +0100

    Use correct command for skipping a test

--- climada/engine/test/test_impact_forecast.py
+++ climada/engine/test/test_impact_forecast.py
@@ -95,7 +95,7 @@ def test_impact_forecast_select(impact_forecast, lead_time, member):
     npt.assert_array_equal(impact_fc.lead_time, lead_time[np.array([0, 2])])
 
 
-@pytest.skip("Concat from base class does not work")
+@pytest.mark.skip("Concat from base class does not work")
 def test_impact_forecast_concat(impact_forecast, member):
     """Check if Impact.concat works on the derived class"""
     impact_fc = ImpactForecast.concat(
