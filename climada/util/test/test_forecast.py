16886415cf310280bd2b88487d3975fb0a53aafc
Evelyn Mühlhofer <42063186+Evelyn-M@users.noreply.github.com>
2025-12-08 11:45:08 +0100

    1127 - Forecast base class init (#1167)
    * define base class
    * add init test
    * Fix #1127
    
    ---------
    
    Co-authored-by: Valentin Gebhart <valentin.gebhart@usys.ethz.ch>
    Co-authored-by: Lukas Riedel <34276446+peanutfun@users.noreply.github.com>

--- climada/util/test/test_forecast.py
+++ climada/util/test/test_forecast.py
@@ -18,3 +18,35 @@ with CLIMADA. If not, see <https://www.gnu.org/licenses/>.
 
 Tests for Forecast base class.
 """
+
+import numpy as np
+import numpy.testing as npt
+
+from climada.util.forecast import Forecast
+
+
+def test_forecast_init():
+    """Test initialization of Forecast class."""
+    forecast = Forecast()
+    npt.assert_array_equal(forecast.lead_time, np.array([]))
+    npt.assert_array_equal(forecast.member, np.array([]))
+
+    forecast = Forecast(member=np.array([1, 2]))
+    npt.assert_array_equal(forecast.member, np.array([1, 2]), strict=True)
+
+    forecast = Forecast(lead_time=np.array([1, 2]))
+    npt.assert_array_equal(forecast.lead_time, np.array([1, 2]), strict=True)
+
+    forecast = Forecast(lead_time=np.array([1, 2]), member=[3, 4])
+    npt.assert_array_equal(forecast.lead_time, np.array([1, 2]), strict=True)
+    npt.assert_array_equal(forecast.member, np.array([3, 4]), strict=True)
+    assert isinstance(forecast.member, np.ndarray)
+
+    # Test with datetime64 including seconds
+    lead_times_seconds = np.array(
+        ["2024-01-01T00:00:00", "2024-01-01T00:01:00", "2024-01-01"],
+        dtype="datetime64[s]",
+    )
+    forecast = Forecast(lead_time=lead_times_seconds, member=[1, 2, 3])
+    npt.assert_array_equal(forecast.lead_time, lead_times_seconds, strict=True)
+    assert forecast.lead_time.dtype == np.dtype("datetime64[s]")
