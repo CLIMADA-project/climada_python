0b6e1e46c42b9af409336c713ed5c0422e399ae9
luseverin <luca.severino@usys.ethz.ch>
2025-12-10 10:59:09 +0100

    Add min max mean for hazard forecast

--- climada/hazard/forecast.py
+++ climada/hazard/forecast.py
@@ -104,3 +104,136 @@ class HazardForecast(Forecast, Hazard):
         num_entries = len(self.event_id)
         size(exp_len=num_entries, var=self.member, var_name="Forecast.member")
         size(exp_len=num_entries, var=self.lead_time, var_name="Forecast.lead_time")
+
+    def _reduce_attrs(self, reduce_method: str):
+        """
+        Reduce the attributes of a HazardForecast to a single value.
+
+        Attributes are modified as follows:
+        - lead_time: set to NaT
+        - member: set to -1
+        - event_id: set to 0
+        - event_name: set to reduce_method
+        - date: set to the minimum value
+        - frequency: set to 1
+
+        Parameters
+        ----------
+        reduce_method : str
+            The reduction method used to reduce the attributes.
+        """
+        reduced_attrs = {
+            "lead_time": np.array([np.timedelta64("NaT")]),
+            "member": np.array([-1]),
+            "event_id": np.array([0]),
+            "event_name": np.array([reduce_method]),
+            "date": np.array([self.date.min()]),
+            "frequency": np.array([1]),
+            "orig": np.array([True]),
+        }
+
+        return reduced_attrs
+
+    def min(self):
+        """
+        Reduce the impact matrix and at_event of a HazardForecast to the minimum
+        value.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        HazardForecast
+            A HazardForecast object with the min intensity and fraction.
+        """
+        red_intensity = sparse.csr_matrix(self.intensity.min(axis=0))
+        red_fraction = sparse.csr_matrix(self.fraction.sum())
+        reduced_attrs = self._reduce_attrs("min")
+        return HazardForecast(
+            lead_time=reduced_attrs["lead_time"],
+            member=reduced_attrs["member"],
+            haz_type=self.haz_type,
+            pool=self.pool,
+            units=self.units,
+            centroids=self.centroids,
+            event_id=reduced_attrs["event_id"],
+            frequency=reduced_attrs["frequency"],
+            frequency_unit=self.frequency_unit,
+            event_name=reduced_attrs["event_name"],
+            date=reduced_attrs["date"],
+            orig=hazard.orig,
+            intensity=red_intensity,
+            fraction=red_fraction,
+        )
+
+    def max(self):
+        """
+        Reduce the impact matrix and at_event of a HazardForecast to the maximum
+        value.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        HazardForecast
+            A HazardForecast object with the min intensity and fraction.
+        """
+        red_intensity = sparse.csr_matrix(self.intensity.min(axis=0))
+        red_fraction = sparse.csr_matrix(self.fraction.sum())
+        reduced_attrs = self._reduce_attrs("max")
+        return HazardForecast(
+            lead_time=reduced_attrs["lead_time"],
+            member=reduced_attrs["member"],
+            haz_type=self.haz_type,
+            pool=self.pool,
+            units=self.units,
+            centroids=self.centroids,
+            event_id=reduced_attrs["event_id"],
+            frequency=reduced_attrs["frequency"],
+            frequency_unit=self.frequency_unit,
+            event_name=reduced_attrs["event_name"],
+            date=reduced_attrs["date"],
+            orig=hazard.orig,
+            intensity=red_intensity,
+            fraction=red_fraction,
+        )
+
+    def mean(self):
+        """
+        Reduce the impact matrix and at_event of a HazardForecast to the mean value.
+
+        The mean value is computed by taking the mean of the impact matrix along the
+        exposure points axis (axis=1) and then taking the mean of the resulting array.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        HazardForecast
+            A HazardForecast object with the min intensity and fraction.
+        """
+        red_intensity = sparse.csr_matrix(self.intensity.min(axis=0))
+        red_fraction = sparse.csr_matrix(self.fraction.sum())
+        reduced_attrs = self._reduce_attrs("mean")
+        return HazardForecast(
+            lead_time=reduced_attrs["lead_time"],
+            member=reduced_attrs["member"],
+            haz_type=self.haz_type,
+            pool=self.pool,
+            units=self.units,
+            centroids=self.centroids,
+            event_id=reduced_attrs["event_id"],
+            frequency=reduced_attrs["frequency"],
+            frequency_unit=self.frequency_unit,
+            event_name=reduced_attrs["event_name"],
+            date=reduced_attrs["date"],
+            orig=hazard.orig,
+            intensity=red_intensity,
+            fraction=red_fraction,
+        )
