d11871fbd1cd5b0d0cb6c32e74ef1594075131d9
spjuhel <pro@sjuhel.org>
2025-12-18 11:09:34 +0100

    cherry pick __init__ (for real)

--- climada/trajectories/__init__.py
+++ climada/trajectories/__init__.py
@@ -21,15 +21,9 @@ possibly interpolation of risk metric over multiple dates.
 
 """
 
-from .interpolated_trajectory import InterpolatedRiskTrajectory
 from .interpolation import AllLinearStrategy, ExponentialExposureStrategy
-from .snapshot import Snapshot
-from .static_trajectory import StaticRiskTrajectory
 
 __all__ = [
-    "InterpolatedRiskTrajectory",
     "AllLinearStrategy",
     "ExponentialExposureStrategy",
-    "Snapshot",
-    "StaticRiskTrajectory",
 ]
